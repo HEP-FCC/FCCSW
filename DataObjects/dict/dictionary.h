5067a33cd73d70021167bafc19c7d35229400431
Colin <colin.bernet@cern.ch>
2014-12-09 13:44:00 +0100

    bug fix in dictionaries + adapted to new event data model.

--- DataObjects/dict/dictionary.h
+++ DataObjects/dict/dictionary.h
@@ -9,16 +9,25 @@
 #include "DataObjects/Particle.h"
 #include "DataObjects/ParticleHandle.h"
 
+#include "DataObjects/MCParticle.h"
+#include "DataObjects/MCParticleHandle.h"
+
 #include "DataObjects/JetParticle.h"
 #include "DataObjects/JetParticleHandle.h"
+
+#include "DataObjects/GenJet.h"
 
 #include "DataObjects/EventInfo.h"
 #include "DataObjects/EventInfoHandle.h"
 
 namespace DataModelDict {
 
-  std::vector<Jet> i1;
-  std::vector<EventInfo> i2;
-  std::vector<Particle> i3;
-  std::vector<JetParticle> i4;
+  std::vector<Jet> jet;
+  std::vector<GenJet> genjet;
+  std::vector<EventInfo> eventinfo;
+  std::vector<Particle> particle;
+  std::vector<MCParticle> mcparticle;
+  std::vector<JetParticle> jetparticle;
+  BareParticle bareparticle;
+  BareJet      barejet;
 }
--- Examples/src/AlbersOutput.cpp
+++ Examples/src/AlbersOutput.cpp
@@ -5,23 +5,23 @@
 DECLARE_COMPONENT(AlbersOutput)
 
 AlbersOutput::AlbersOutput(const std::string& name, ISvcLocator* svcLoc) :
-    GaudiAlgorithm(name, svcLoc),m_first(true)
+GaudiAlgorithm(name, svcLoc),m_first(true)
 {
   declareProperty("Filename", m_filename="output.root",
                   "Name of the file to create");
 }
 
 StatusCode AlbersOutput::initialize() {
-	if (GaudiAlgorithm::initialize().isFailure())
-		return StatusCode::FAILURE;
-        // check whether we have the AlbersEvtSvc active
-        m_albersDataSvc = dynamic_cast<AlbersDataSvc*>(evtSvc().get());
-        if (0==m_albersDataSvc) return StatusCode::FAILURE;
-	m_file = new TFile(m_filename.c_str(),"RECREATE","data file");
-	m_datatree  = new TTree("events","Events tree");
-	m_metadatatree = new TTree("metadata", "Metadata tree");
-        m_registry = m_albersDataSvc->getRegistry();
-	return StatusCode::SUCCESS;
+  if (GaudiAlgorithm::initialize().isFailure())
+    return StatusCode::FAILURE;
+  // check whether we have the AlbersEvtSvc active
+  m_albersDataSvc = dynamic_cast<AlbersDataSvc*>(evtSvc().get());
+  if (0==m_albersDataSvc) return StatusCode::FAILURE;
+  m_file = new TFile(m_filename.c_str(),"RECREATE","data file");
+  m_datatree  = new TTree("events","Events tree");
+  m_metadatatree = new TTree("metadata", "Metadata tree");
+  m_registry = m_albersDataSvc->getRegistry();
+  return StatusCode::SUCCESS;
 }
 
 StatusCode AlbersOutput::execute() {
@@ -36,25 +36,28 @@ StatusCode AlbersOutput::execute() {
       pos = name.find("Collection");
       name.erase(pos,pos+10); 
       std::string classname = "vector<"+name+">";
-      debug() << "Registering collection " << i.first.c_str() << " containing type " << name << endmsg;
+      debug() << "Registering collection " << classname << " " << i.first.c_str() << " containing type " << name << endmsg;
       m_datatree->Branch(i.first.c_str(), classname.c_str(), i.second->_getRawBuffer());
     }
     else {
+      // debug() << "set branch address " << i.first.c_str() << " " << endmsg;
       m_datatree->SetBranchAddress(i.first.c_str(),i.second->_getRawBuffer());
     }
     i.second->prepareForWrite(m_registry);  
   }
+  if(m_first)
+    m_datatree->Print();
   m_first = false;
   m_datatree->Fill();
   return StatusCode::SUCCESS;
 }
 
 StatusCode AlbersOutput::finalize() {
-	if (GaudiAlgorithm::finalize().isFailure())
-		return StatusCode::FAILURE;
-	m_metadatatree->Branch("Registry",m_registry);
-	m_metadatatree->Fill();
-	m_file->Write();
-	m_file->Close();
-	return StatusCode::SUCCESS;
+  if (GaudiAlgorithm::finalize().isFailure())
+    return StatusCode::FAILURE;
+  m_metadatatree->Branch("Registry",m_registry);
+  m_metadatatree->Fill();
+  m_file->Write();
+  m_file->Close();
+  return StatusCode::SUCCESS;
 }
