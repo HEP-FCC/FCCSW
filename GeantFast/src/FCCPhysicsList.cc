978a5263877c127aa11011459791d2cdeb08fd8c
Benedikt Hegner <hegner@cern.ch>
2015-03-30 22:00:44 +0200

    include all header files at the top

--- GeantFast/src/FCCPhysicsList.cc
+++ GeantFast/src/FCCPhysicsList.cc
@@ -1,31 +1,3 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
-// based on G4 examples/extended/parametrisations/Par01/src/Par01PhysicsList.cc
-//
-
 #include "FCCPhysicsList.hh"
 #include "globals.hh"
 #include "G4ParticleDefinition.hh"
@@ -39,6 +11,30 @@
 #include "G4SystemOfUnits.hh"
 #include <iomanip>
 #include "G4FastSimulationManagerProcess.hh"
+
+#include "G4Decay.hh"
+#include "G4LeptonConstructor.hh"
+#include "G4MesonConstructor.hh"
+#include "G4BaryonConstructor.hh"
+#include "G4IonConstructor.hh"
+
+#include "G4ComptonScattering.hh"
+#include "G4GammaConversion.hh"
+#include "G4PhotoElectricEffect.hh"
+
+#include "G4eMultipleScattering.hh"
+#include "G4MuMultipleScattering.hh"
+#include "G4hMultipleScattering.hh"
+
+#include "G4eIonisation.hh"
+#include "G4eBremsstrahlung.hh"
+#include "G4eplusAnnihilation.hh"
+
+#include "G4MuIonisation.hh"
+#include "G4MuBremsstrahlung.hh"
+#include "G4MuPairProduction.hh"
+
+#include "G4hIonisation.hh"
 
 FCCPhysicsList::FCCPhysicsList():  G4VUserPhysicsList()
 {
@@ -73,28 +69,24 @@ void FCCPhysicsList::ConstructBosons()
   G4OpticalPhoton::OpticalPhotonDefinition();
 }
 
-#include "G4LeptonConstructor.hh"
 void FCCPhysicsList::ConstructLeptons()
 {
   G4LeptonConstructor pConstructor;
   pConstructor.ConstructParticle();
 }
 
-#include "G4MesonConstructor.hh"
 void FCCPhysicsList::ConstructMesons()
 {
   G4MesonConstructor pConstructor;
   pConstructor.ConstructParticle();
 }
 
-#include "G4BaryonConstructor.hh"
 void FCCPhysicsList::ConstructBaryons()
 {
   G4BaryonConstructor  pConstructor;
   pConstructor.ConstructParticle();
 }
 
-#include "G4IonConstructor.hh"
 void FCCPhysicsList::ConstructIons()
 {
   G4IonConstructor pConstructor;
@@ -116,23 +108,6 @@ void FCCPhysicsList::AddTransportation()
    G4VUserPhysicsList::AddTransportation();
 }
 
-#include "G4ComptonScattering.hh"
-#include "G4GammaConversion.hh"
-#include "G4PhotoElectricEffect.hh"
-
-#include "G4eMultipleScattering.hh"
-#include "G4MuMultipleScattering.hh"
-#include "G4hMultipleScattering.hh"
-
-#include "G4eIonisation.hh"
-#include "G4eBremsstrahlung.hh"
-#include "G4eplusAnnihilation.hh"
-
-#include "G4MuIonisation.hh"
-#include "G4MuBremsstrahlung.hh"
-#include "G4MuPairProduction.hh"
-
-#include "G4hIonisation.hh"
 void FCCPhysicsList::ConstructEM()
 {
   theParticleIterator->reset();
@@ -231,7 +206,6 @@ void FCCPhysicsList::ConstructEM()
 }
 
 
-#include "G4Decay.hh"
 void FCCPhysicsList::ConstructGeneral()
 {
   G4Decay* theDecayProcess = new G4Decay();
