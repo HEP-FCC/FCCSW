c92693c60c844e285ef6f691156940eb78c7068c
clementhelsens <clement.helsens@cern.ch>
2015-10-07 13:39:18 +0200

    modify the config/simple_pythia.py accordingly

--- config/simple_pythia.py
+++ config/simple_pythia.py
@@ -8,10 +8,10 @@ from Gaudi.Configuration import *
 from Configurables import ApplicationMgr, FCCDataSvc
 
 #example of pythia configuration file to generate events
-pythiafile="main03.cmnd"
+pythiafile="Pythia_standard.cmnd"
 
 #example of pythia configuration file to read LH event file
-#pythiafile="main42.cmnd"
+#pythiafile="Pythia_LHEinput.cmnd"
 
 albersevent   = FCCDataSvc("EventDataSvc")
 
