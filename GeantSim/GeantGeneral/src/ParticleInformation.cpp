c57753759d89751034a44727f22420093c5eeca9
Anna <anna.zaborowska@cern.ch>
2015-11-18 10:41:05 +0100

    References added for handling EDM in particle information

--- GeantSim/GeantGeneral/src/ParticleInformation.cpp
+++ GeantSim/GeantGeneral/src/ParticleInformation.cpp
@@ -1,15 +1,15 @@
 #include "ParticleInformation.h"
 
-ParticleInformation::ParticleInformation(const MCParticleHandle aMCpart, ParticleHandle aPart): m_mcpart(aMCpart), m_part(aPart) {}
+ParticleInformation::ParticleInformation(const MCParticleHandle& aMCpart, ParticleHandle& aPart): m_mcpart(aMCpart), m_part(aPart) {}
 
 ParticleInformation::~ParticleInformation() {}
 
 void ParticleInformation::Print() const {}
 
-const MCParticleHandle ParticleInformation::GetMCParticleHandle() const {
+const MCParticleHandle& ParticleInformation::GetMCParticleHandle() const {
    return m_mcpart;
 }
 
-ParticleHandle ParticleInformation::GetParticleHandle() const {
+ParticleHandle& ParticleInformation::GetParticleHandle() {
    return m_part;
 }
