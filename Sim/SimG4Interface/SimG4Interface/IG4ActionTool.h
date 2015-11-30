32092b2b728486ae209f33ad5312cd0e4953287e
Benedikt Hegner <hegner@cern.ch>
2015-11-30 21:27:32 +0100

    start moving Geant code into new structure

---
+++ Sim/SimG4Interface/SimG4Interface/IG4ActionTool.h
@@ -0,0 +1,87 @@
+#ifndef SIM_IG4ACTIONTOOL_H
+#define SIM_IG4ACTIONTOOL_H 1 
+
+/// Include files from the Framework
+#include  "GaudiKernel/AlgTool.h"
+
+// Forward declarations from Geant4
+class G4VUserActionInitialization;
+
+
+/** @class IG4ActionTool IG4ActionTool.h G4Interface/IG4ActionTool.h
+ *
+ *  abstract interface to load G4UserActionInitializations 
+ *
+ *  @author Benedikt HEGNER
+ */
+
+class IG4ActionTool : virtual public AlgTool {
+public:
+  
+  /// Retrieve interface ID
+  static const InterfaceID& interfaceID() ;
+  
+  /**  initialize 
+   *   @return status code 
+   */
+  virtual StatusCode   initialize() = 0 ;
+  
+  /**  initialize 
+   *   @return status code 
+   */
+  virtual StatusCode   finalize() = 0 ;
+
+  /** get initilization hook
+   *  @return  pointer to G4VUserActionInitialization
+   */
+   virtual G4VUserActionInitialization* getUserActionInitialization() = 0 ;
+
+protected:
+  
+  /// virtual destructor
+  virtual ~IG4ActionTool();
+
+};
+
+// ============================================================================
+#endif 
+// ============================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
