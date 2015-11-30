8f6dbfabf041f2d201f9ab6a6224ec4c072ba1d2
Benedikt Hegner <hegner@cern.ch>
2015-11-30 20:02:47 +0100

    add missing pieces to init.sh

--- init.sh
+++ init.sh
@@ -12,7 +12,7 @@ export CMAKE_PREFIX_PATH=$FCCEDM:$ALBERS:$DELPHES_DIR:$CMAKE_PREFIX_PATH:$PYTHIA
 export PYTHIA8_XML=/afs/cern.ch/sw/lcg/releases/LCG_80/MCGenerators/pythia8/212/x86_64-slc6-gcc49-opt/xmldoc
 
 # add Geant4 data files
-#source /afs/cern.ch/sw/lcg/external/geant4/10.1/setup_g4datasets.sh
+source /afs/cern.ch/sw/lcg/external/geant4/10.1/setup_g4datasets.sh
 
 # add DD4hep
 export inithere=$PWD
@@ -21,3 +21,4 @@ source bin/thisdd4hep.sh
 cd $inithere
 
 export CMTPROJECTPATH=/afs/cern.ch/exp/fcc/sw/0.5/
+source /afs/cern.ch/sw/lcg/contrib/gcc/4.9.3/x86_64-slc6/setup.sh
