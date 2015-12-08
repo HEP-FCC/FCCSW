8d3aa3b9d05e22fbab7d48662f488a2e7bada5ac
Anna <anna.zaborowska@cern.ch>
2015-12-08 12:14:41 +0100

    namespace change in SDWrapper

--- DetectorDescription/Detectors/src/SDWrapper.cpp
+++ DetectorDescription/Detectors/src/SDWrapper.cpp
@@ -3,14 +3,13 @@
 
 #include "SimG4Full/GeantTrackerSD.h"
 
-namespace DD4hep { namespace Simulation {
-
-      // Factory method to create an instance of ExN04TrackerSD
-    static G4VSensitiveDetector* createTrackerSD(const std::string& name,  DD4hep::Geometry::LCDD& lcdd)  {
-       std::string trackerSDname = "/mydet/tracker/"+name;
-       std::cout<<" Creating /mydet/tracker SD in G4"<<std::endl;
-      return new GeantTrackerSD(trackerSDname);
-    }
-   }
+namespace sim {
+namespace detectors {
+// Factory method to create an instance of ExN04TrackerSD
+static G4VSensitiveDetector* createTrackerSD(const std::string& name,  DD4hep::Geometry::LCDD& lcdd) {
+  std::string trackerSDname = "/mydet/tracker/"+name;
+  return new GeantTrackerSD(trackerSDname);
 }
-DECLARE_EXTERNAL_GEANT4SENSITIVEDETECTOR(DD4hepGeantTrackerSD,DD4hep::Simulation::createTrackerSD)
+}
+}
+DECLARE_EXTERNAL_GEANT4SENSITIVEDETECTOR(DD4hepGeantTrackerSD,sim::detectors::createTrackerSD)
