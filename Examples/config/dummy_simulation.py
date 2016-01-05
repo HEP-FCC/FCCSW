80eb83e91de37864d6c0a12d4718e04b702fbe6c
Joschka Lingemann <joschka.lingemann@cern.ch>
2016-01-05 15:55:07 +0100

    Migration to DataOutputs and DataInputs.

--- Examples/config/dummy_simulation.py
+++ Examples/config/dummy_simulation.py
@@ -9,39 +9,39 @@ from Configurables import HepMCReader
 reader = HepMCReader("Reader", Filename="example_MyPythia.dat")
 # have a look at the source code of HepMCReader, in Generation/src/HepMCReader
 # In the following line,
-#   reader.Outputs.YYY.Path = "ZZZ"
+#   reader.DataOutputs.YYY.Path = "ZZZ"
 # YYY matches the string passed to declareOutput in the constructor of the algorithm
 # XXX declares a name for the product (the HepMC::GenEvent)
-reader.Outputs.hepmc.Path = "hepmc"
+reader.DataOutputs.hepmc.Path = "hepmc"
 
 # reads an HepMC::GenEvent from the data service and writes
 # a collection of EDM Particles
 from Configurables import HepMCConverter
 hepmc_converter = HepMCConverter("Converter")
 # the input product name matches the output product name of the previous module
-hepmc_converter.Inputs.hepmc.Path="hepmc"
-hepmc_converter.Outputs.genparticles.Path="all_genparticles"
+hepmc_converter.DataInputs.hepmc.Path="hepmc"
+hepmc_converter.DataOutputs.genparticles.Path="all_genparticles"
 hepmc_converter.DataOutputs.genvertices.Path="all_genvertices"
 
 from Configurables import GenParticleFilter
 genfilter = GenParticleFilter("StableParticles")
-genfilter.Inputs.genparticles.Path = "all_genparticles"
-genfilter.Outputs.genparticles.Path = "genparticles"
+genfilter.DataInputs.genparticles.Path = "all_genparticles"
+genfilter.DataOutputs.genparticles.Path = "genparticles"
 
 from Configurables import JetClustering_MCParticleCollection_GenJetCollection_GenJetParticleAssociationCollection_ as GenJetClustering
 genjet_clustering = GenJetClustering(
     "GenJetClustering",
     verbose=False
     )
-genjet_clustering.Inputs.particles.Path='genparticles'
+genjet_clustering.DataInputs.particles.Path='genparticles'
 # giving a meaningful name for the output product
-genjet_clustering.Outputs.jets.Path='genjets'
-genjet_clustering.Outputs.constituents.Path='genjets_particles'
+genjet_clustering.DataOutputs.jets.Path='genjets'
+genjet_clustering.DataOutputs.constituents.Path='genjets_particles'
 
 from Configurables import DummySimulation
 dummysimulation = DummySimulation("Simulation")
-dummysimulation.Inputs.genparticles.Path = "genparticles"
-dummysimulation.Outputs.particles.Path = "particles"
+dummysimulation.DataInputs.genparticles.Path = "genparticles"
+dummysimulation.DataOutputs.particles.Path = "particles"
 
 # reads EDM Particles and creates EDM jets
 from Configurables import JetClustering_ParticleCollection_JetCollection_JetParticleAssociationCollection_ as JetClustering
@@ -49,10 +49,10 @@ jet_clustering = JetClustering(
     "JetClustering",
     verbose=False
     )
-jet_clustering.Inputs.particles.Path='particles'
+jet_clustering.DataInputs.particles.Path='particles'
 # giving a meaningful name for the output product
-jet_clustering.Outputs.jets.Path='jets'
-jet_clustering.Outputs.constituents.Path='jets_particles'
+jet_clustering.DataOutputs.jets.Path='jets'
+jet_clustering.DataOutputs.constituents.Path='jets_particles'
 
 out = AlbersOutput("out",
                    OutputLevel=DEBUG)
