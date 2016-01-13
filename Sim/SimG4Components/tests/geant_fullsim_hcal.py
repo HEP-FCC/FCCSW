f968ab5dbf536605d9662dc01db88e178a5ab047
Anna <anna.zaborowska@cern.ch>
2016-01-13 16:18:01 +0100

    all main dir options as tests added

---
+++ Sim/SimG4Components/tests/geant_fullsim_hcal.py
@@ -0,0 +1,42 @@
+from Gaudi.Configuration import *
+from Configurables import ApplicationMgr, HepMCReader, HepMCDumper, FCCDataSvc
+
+albersevent = FCCDataSvc("EventDataSvc")
+
+reader = HepMCReader("Reader", Filename="../example_MyPythia.dat")
+reader.DataOutputs.hepmc.Path = "hepmc"
+
+from Configurables import HepMCConverter
+hepmc_converter = HepMCConverter("Converter")
+hepmc_converter.DataInputs.hepmc.Path="hepmc"
+hepmc_converter.DataOutputs.genparticles.Path="allGenParticles"
+hepmc_converter.DataOutputs.genvertices.Path="allGenVertices"
+
+from Configurables import G4SimSvc
+geantservice = G4SimSvc("G4SimSvc", detector="G4DD4hepDetector", physicslist="G4FtfpBert", actions="G4FullSimActions")
+
+from Configurables import GeoSvc
+geoservice = GeoSvc("GeoSvc", detector='file:../DetectorDescription/Detectors/compact/TestHCal.xml',
+                    OutputLevel = DEBUG)
+
+from Configurables import G4FullSimAlg, G4SaveTrackerHits
+savetrackertool = G4SaveTrackerHits("G4SaveTrackerHits")
+savetrackertool.DataOutputs.trackClusters.Path = "clusters"
+savetrackertool.DataOutputs.trackHits.Path = "hits"
+savetrackertool.DataOutputs.trackHitsClusters.Path = "hitClusterAssociation"
+geantsim = G4FullSimAlg("G4FullSimAlg", outputs= savetrackertool)
+geantsim.DataInputs.genParticles.Path="allGenParticles"
+geantsim.DataOutputs.caloClusters.Path = "caloClusters"
+geantsim.DataOutputs.caloHits.Path = "caloHits"
+
+from Configurables import AlbersWrite, AlbersOutput
+out = AlbersOutput("out",
+                   OutputLevel=DEBUG)
+out.outputCommands = ["keep *"]
+
+ApplicationMgr( TopAlg = [reader, hepmc_converter, geantsim, out],
+                EvtSel = 'NONE',
+                EvtMax   = 1,
+                ExtSvc = [albersevent, geoservice, geantservice], # order! geo needed by geant
+                OutputLevel=INFO
+ )
