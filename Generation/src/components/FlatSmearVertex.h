2e8e36844c80d8d7c45fe158fdac1605894c2b63
Valentin Volkl <valentin.volkl@cern.ch>
2016-01-25 13:56:55 +0100

    Make comments, whitespace conform with new style.

--- Generation/src/components/FlatSmearVertex.h
+++ Generation/src/components/FlatSmearVertex.h
@@ -1,9 +1,6 @@
-// $Id: FlatSmearVertex.h,v 1.1.1.1 2009-09-18 16:18:24 gcorti Exp $
-#ifndef PARTICLEGUNS_FLATSMEARVERTEX_H 
-#define PARTICLEGUNS_FLATSMEARVERTEX_H 1
+#ifndef GENERATION_FLATSMEARVERTEX_H
+#define GENERATION_FLATSMEARVERTEX_H 1
 
-// Include files
-// from Gaudi
 #include "GaudiAlg/GaudiTool.h"
 #include "GaudiKernel/RndmGenerators.h" 
 
@@ -19,7 +16,7 @@
  *  @date   2008-05-18
  */
 class FlatSmearVertex : public GaudiTool, virtual public IVertexSmearingTool {
- public:
+public:
   /// Standard constructor
   FlatSmearVertex( const std::string& type , const std::string& name,
                     const IInterface* parent ) ;
@@ -33,30 +30,31 @@ class FlatSmearVertex : public GaudiTool, virtual public IVertexSmearingTool {
    */
   virtual StatusCode smearVertex( HepMC::GenEvent * theEvent ) ;
   
- private:
+private:
   /// Minimum value for the x coordinate of the vertex (set by options)
   double m_xmin   ;
-
+  
   /// Minimum value for the y coordinate of the vertex (set by options)
   double m_ymin   ;
- 
+  
   /// Minimum value for the z coordinate of the vertex (set by options)
   double m_zmin   ;
   
   /// Maximum value for the x coordinate of the vertex (set by options)
   double m_xmax   ;
-
+  
   /// Maximum value for the y coordinate of the vertex (set by options)
   double m_ymax   ;
-
+  
   /// Maximum value for the z coordinate of the vertex (set by options)
   double m_zmax   ;
-
+  
   /// Direction of the beam to take into account TOF vs nominal IP8, can have
   /// only values -1 or 1, or 0 to switch off the TOF and set time of 
   /// interaction to zero (default = 1, as for beam 1)
   int m_zDir;
-
+  
   Rndm::Numbers m_flatDist ; ///< Flat random number generator
 };
-#endif // PARTICLEGUNS_FLATSMEARVERTEX_H
+
+#endif // GENERATION_FLATSMEARVERTEX_H
