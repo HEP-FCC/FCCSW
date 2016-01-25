8f56e7325a4bbbcf3334e945de967e747dcf5bb5
clementhelsens <clement.helsens@cern.ch>
2016-01-25 15:04:33 +0100

    adding det namespace

--- DetectorDescription/Detectors/src/SimpleCylinder_geo.cxx
+++ DetectorDescription/Detectors/src/SimpleCylinder_geo.cxx
@@ -8,44 +8,42 @@
 //====================================================================
 #include "DD4hep/DetFactoryHelper.h"
 
+namespace det {
 
-static  DD4hep::Geometry::Ref_t createSimpleCylinder(DD4hep::Geometry::LCDD& lcdd, 
-                                               xml_h e, 
-                                               DD4hep::Geometry::SensitiveDetector)  {
-
-
-  xml_det_t x_det = e;
-  std::string  name  = x_det.nameStr();
-  DD4hep::Geometry::DetElement cylinderDet( name, x_det.id() );
-  
-  DD4hep::Geometry::Volume experimentalHall =  lcdd.pickMotherVolume( cylinderDet ) ;
-  
-  xml_comp_t  cylinderDim ( x_det.child( _U(dimensions) ) );
-  
-  DD4hep::Geometry::Tube cylinder( cylinderDim.rmin(),cylinderDim.rmax(),cylinderDim.dz(), cylinderDim.phi0(),cylinderDim.deltaphi() ); 
-  
-  DD4hep::Geometry::Volume cylinderVol( x_det.nameStr()+ "_SimpleCylinder", cylinder, lcdd.material(cylinderDim.materialStr()) );
-  
-  DD4hep::Geometry::PlacedVolume cylinderPhys;
- 
-  double zoff = cylinderDim.z_offset();
-  if (fabs(zoff)>0.000000000001) {
-    DD4hep::Geometry::Position trans(0.,0.,zoff ) ;
-    cylinderPhys = experimentalHall.placeVolume( cylinderVol, DD4hep::Geometry::Transform3D( DD4hep::Geometry::RotationZ(0.) , trans ));
-  }  
-  else cylinderPhys = experimentalHall.placeVolume( cylinderVol );
-  
-
-  cylinderPhys.addPhysVolID( "system", x_det.id() ).addPhysVolID("side",0 )  ;
-
-  cylinderDet.setPlacement( cylinderPhys );
-
-  cylinderDet.setVisAttributes( lcdd, x_det.visStr(), cylinderVol );
+  static DD4hep::Geometry::Ref_t createSimpleCylinder(DD4hep::Geometry::LCDD& lcdd, 
+                                                      xml_h e, 
+                                                      DD4hep::Geometry::SensitiveDetector)  {
 
 
-
-  
-  return cylinderDet;
-}
-
-DECLARE_DETELEMENT( SimpleCylinder ,createSimpleCylinder )
+    xml_det_t x_det = e;
+    std::string  name  = x_det.nameStr();
+    DD4hep::Geometry::DetElement cylinderDet( name, x_det.id() );
+    
+    DD4hep::Geometry::Volume experimentalHall =  lcdd.pickMotherVolume( cylinderDet ) ;
+    
+    xml_comp_t  cylinderDim ( x_det.child( _U(dimensions) ) );
+    
+    DD4hep::Geometry::Tube cylinder( cylinderDim.rmin(),cylinderDim.rmax(),cylinderDim.dz(), cylinderDim.phi0(),cylinderDim.deltaphi() ); 
+    
+    DD4hep::Geometry::Volume cylinderVol( x_det.nameStr()+ "_SimpleCylinder", cylinder, lcdd.material(cylinderDim.materialStr()) );
+    
+    DD4hep::Geometry::PlacedVolume cylinderPhys;
+    
+    double zoff = cylinderDim.z_offset();
+    if (fabs(zoff)>0.000000000001) {
+      DD4hep::Geometry::Position trans(0.,0.,zoff ) ;
+      cylinderPhys = experimentalHall.placeVolume( cylinderVol, DD4hep::Geometry::Transform3D( DD4hep::Geometry::RotationZ(0.) , trans ));
+    }  
+    else cylinderPhys = experimentalHall.placeVolume( cylinderVol );
+    
+    
+    cylinderPhys.addPhysVolID( "system", x_det.id() ).addPhysVolID("side",0 )  ;
+    
+    cylinderDet.setPlacement( cylinderPhys );
+    
+    cylinderDet.setVisAttributes( lcdd, x_det.visStr(), cylinderVol );
+    
+    return cylinderDet;
+  }
+}  
+DECLARE_DETELEMENT( SimpleCylinder ,det::createSimpleCylinder )
