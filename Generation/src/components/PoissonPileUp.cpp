da6fa00d9fe1626fc9fb6358b9c9b6c00b20e2ca
Valentin Volkl <valentin.volkl@cern.ch>
2016-01-28 16:59:39 +0100

    Slightly more realistic implementation of a Pileup Tool, compared to just using a constant value for the pileup.

---
+++ Generation/src/components/PoissonPileUp.cpp
@@ -0,0 +1,47 @@
+#include "PoissonPileUp.h"
+
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "GaudiKernel/IRndmGenSvc.h" 
+
+DECLARE_TOOL_FACTORY( PoissonPileUp )
+
+PoissonPileUp::PoissonPileUp( const std::string& type,
+                              const std::string& name,
+                              const IInterface* parent )
+  : GaudiTool ( type, name , parent ) {
+  declareInterface< IPileUpTool >( this );
+  
+ declareProperty( "numPileUpEvents", m_meanPileUpEvents = 0 );
+ declareProperty( "Filename", m_filename = "");
+}
+
+
+PoissonPileUp::~PoissonPileUp( ) { ; }
+
+
+StatusCode PoissonPileUp::initialize( ) {
+  StatusCode sc = GaudiTool::initialize( );
+  if ( sc.isFailure() ) return sc ;
+  IRndmGenSvc * randSvc = svc< IRndmGenSvc >( "RndmGenSvc" , true ) ;
+  if ( m_numPileUpEvents < 0 ) return Error( "Number of Pileup events cannot be negative!" ) ;
+  sc = m_PoissonDist.initialize( randSvc , Rndm::Poisson( m_numPileUpEvents ) ) ;
+  if ( ! sc.isSuccess() ) 
+    return Error( "Could not initialize Poisson random number generator" ) ;
+
+  release( randSvc ) ;
+  return sc;
+}
+
+std::string PoissonPileUp::getFilename() {
+  return m_filename;
+}
+
+unsigned int PoissonPileUp::numberOfPileUp( ) {
+  m_currentNumPileUpEvents = m_PoissonDist();
+  printPileUpCounters();
+  return m_currentNumPileUpEvents;
+}
+
+void PoissonPileUp::printPileUpCounters () {
+  info() << "Adding " << m_numPileUpEvents << " pileup events." << endmsg;
+}
---
+++ Generation/src/components/PoissonPileUp.h
@@ -0,0 +1,51 @@
+#ifndef GENERATION_POISSONPILEUP_H 
+#define GENERATION_POISSONPILEUP_H 1
+
+#include "IPileUpTool.h"
+
+#include "GaudiAlg/GaudiTool.h"
+#include "GaudiKernel/RndmGenerators.h" 
+
+
+/** @class PoissonPileUp PoissonPileUp.h "ConstPileup.h"
+ *  
+ *  Tool to generate number of pile-up events to be mixed with signal event.
+ *  Concrete implementation of a IPileUpTool, returning a random variable
+ *   drawn from a Poisson distribution centered around the average number
+ *   of pileup events.
+ * 
+ *  @author Valentin Volkl
+ *  @date   2016-01-18
+ */
+
+class PoissonPileUp : public GaudiTool, virtual public IPileUpTool {
+ public:
+  /// Standard constructor
+  PoissonPileUp( const std::string& type , const std::string& name,
+                    const IInterface* parent ) ;
+  
+  virtual ~PoissonPileUp( ); ///< Destructor
+
+  /// Initialize method
+  virtual StatusCode initialize( ) ;
+  
+  /** Implements IPileUpTool::numberOfPileUp.
+   */
+  virtual unsigned int numberOfPileUp( ) ;
+  /** Implements IPileUpTool::printPileUpCounters.
+   */
+  virtual void printPileUpCounters( ) ;
+  
+  virtual std::string getFilename();
+  
+ private:
+  /// average number of min bias events to pile on signal event.
+  double m_meanPileUpEvents;
+  int m_currentNumPileUpEvents;
+  std::string m_filename;
+
+
+  Rndm::Numbers m_PoissonDist ; ///< Poisson random number generator
+};
+
+#endif // GENERATION_POISSONPILEUP_H
