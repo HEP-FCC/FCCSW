5f1a150ea139e86580dc450b4de7df62524e4e39
Zbynek Drasal <Zbynek.Drasal@cern.ch>
2016-02-02 14:35:33 +0100

    init file redefined to be used with new PODIO library

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -u
 source /afs/cern.ch/lhcb/software/releases/LBSCRIPTS/LBSCRIPTS_v8r4p3/InstallArea/scripts/LbLogin.sh --cmtconfig x86_64-slc6-gcc49-opt
 
-export FCCEDM=/afs/cern.ch/exp/fcc/sw/0.5/fcc-edm/0.1/x86_64-slc6-gcc49-opt/
-export ALBERS=/afs/cern.ch/exp/fcc/sw/0.5/albers-core/0.1/x86_64-slc6-gcc49-opt
+export FCCEDM=/afs/cern.ch/work/d/drasal/fcc/FCC-PODIO/fcc-edm/install #/afs/cern.ch/exp/fcc/sw/0.5/fcc-edm/0.1/x86_64-slc6-gcc49-opt/
+export ALBERS=/afs/cern.ch/work/d/drasal/fcc/FCC-PODIO/podio/install #afs/cern.ch/exp/fcc/sw/0.5/albers-core/0.1/x86_64-slc6-gcc49-opt
 export DELPHES_DIR=/afs/cern.ch/exp/fcc/sw/0.5/Delphes-3.3.1-newPCMs/x86_64-slc6-gcc49-opt
 export PYTHIA_DIR=/afs/cern.ch/sw/lcg/releases/LCG_80/MCGenerators/pythia8/212/x86_64-slc6-gcc49-opt/
 
