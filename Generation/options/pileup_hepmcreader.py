a9ba9776002d1218449f7939279d3af658f28056
Valentin Volkl <valentin.volkl@cern.ch>
2016-02-04 11:54:00 +0100

    Add test for HepMCReader.

---
+++ Generation/options/pileup_hepmcreader.py
@@ -0,0 +1,26 @@
+from Gaudi.Configuration import *
+
+from Configurables import ApplicationMgr, FCCDataSvc, AlbersWrite, AlbersOutput
+
+albersevent   = FCCDataSvc("EventDataSvc")
+# reads HepMC text file and write the HepMC::GenEvent to the data service
+from Configurables import HepMCReader, HepMCDumper, PoissonPileUp, HepMCFileReader
+genpileup = PoissonPileUp(name="Pileup", 
+    Filename="/afs/cern.ch/user/v/vavolkl/public/example_MyPythia.dat", 
+    numPileUpEvents=2)
+reader = HepMCReader("Reader", 
+    Filename="/afs/cern.ch/user/v/vavolkl/public/example_MyPythia.dat", 
+    PileUpTool=genpileup)
+reader.DataOutputs.hepmc.Path = "hepmc"
+
+# printout of the hepmc stuff
+dumper = HepMCDumper()
+dumper.DataInputs.hepmc.Path="hepmc"
+
+ApplicationMgr(
+    TopAlg = [reader, dumper],
+    EvtSel = 'NONE',
+    EvtMax   = 1,
+    ExtSvc = [albersevent],
+ )
+
