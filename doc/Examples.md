d03a6c9700a836354a1967e4607d9e6e00ebc3b1
zaborowska <azaborow@cern.ch>
2016-02-08 08:56:07 +0100

    commenting simple_workflow.py example

---
+++ doc/Examples.md
@@ -0,0 +1,25 @@
+List of examples
+====
+
+* options/simple_workflow.py
+  * reading events from a HepMC file
+  * converting HepMC::GenEvent to EDM
+  * using sample (jet clustering) algorithm
+  * writing the EDM output using PODIO algorithms and service
+* options/example_options.py
+* options/simple_pythia.py
+* options/geant_pgun_fullsim.py
+* options/geant_fullsim.py
+* options/PythiaDelphes_config.py
+* options/geant_fastsim.py
+
+
+* Sim/SimG4Components/tests/geant_fullsim_hcal.py
+* Sim/SimG4Components/tests/geant_fullsim_gdml.py
+* Sim/SimG4Components/tests/geant_fullsim_moreEvents.py
+
+* Generation/config/particleGun.py
+* Examples/config/delphes_simulation.py
+* Examples/config/dummy_simulation.py
+* Examples/config/compare_material.py
+* Reconstruction/config/genJetClustering.py
--- options/simple_workflow.py
+++ options/simple_workflow.py
@@ -1,7 +1,7 @@
 from Gaudi.Configuration import *
 
-from Configurables import ApplicationMgr, FCCDataSvc, PodioWrite, PodioOutput
-
+# data service
+from Configurables import FCCDataSvc
 podioevent   = FCCDataSvc("EventDataSvc")
 
 # reads HepMC text file and write the HepMC::GenEvent to the data service
@@ -9,17 +9,17 @@ from Configurables import HepMCReader
 reader = HepMCReader("Reader", Filename="example_MyPythia.dat")
 # have a look at the source code of HepMCReader, in Generation/src/HepMCReader
 # In the following line,
-#   reader.DataOutputs.YYY.Path = "ZZZ"
-# YYY matches the string passed to declareOutput in the constructor of the algorithm
-# XXX declares a name for the product (the HepMC::GenEvent)
-reader.DataOutputs.hepmc.Path = "hepmc"
+#   reader.DataOutputs.YYY.Path = "XXX"
+# YYY matches the string passed to declareOutput in the constructor of the algorithm (here "hepmc")
+# XXX declares a name for the product (here the product is HepMC::GenEvent: "hepmcevent")
+reader.DataOutputs.hepmc.Path = "hepmcevent"
 
-# reads an HepMC::GenEvent from the data service and writes
-# a collection of EDM Particles
+# reads an HepMC::GenEvent from the data service and writes a collection of EDM Particles
 from Configurables import HepMCConverter
 hepmc_converter = HepMCConverter("Converter")
 # the input product name matches the output product name of the previous module
-hepmc_converter.DataInputs.hepmc.Path="hepmc"
+hepmc_converter.DataInputs.hepmc.Path="hepmcevent"
+# giving a meaningful name for the output product
 hepmc_converter.DataOutputs.genparticles.Path="all_genparticles"
 hepmc_converter.DataOutputs.genvertices.Path="all_genvertices"
 
@@ -27,25 +27,32 @@ from Configurables import JetClustering_fcc__MCParticleCollection_fcc__GenJetCol
 genjet_clustering = JetClustering(
     "GenJetClustering",
     verbose = False
-    )
+)
+# the input product name matches the output product name of the previous module
 genjet_clustering.DataInputs.particles.Path='all_genparticles'
 # giving a meaningful name for the output product
 genjet_clustering.DataOutputs.jets.Path='genjets'
 genjet_clustering.DataOutputs.constituents.Path='genjets_particles'
 
+# PODIO algorithms
+from Configurables import PodioOutput
 out = PodioOutput("out",
-                   OutputLevel=DEBUG)
+                  OutputLevel=DEBUG)
 out.outputCommands = ["keep *"]
 
+# ApplicationMgr
+from Configurables import ApplicationMgr
 ApplicationMgr(
+    # all algorithms should be put here
     TopAlg = [reader,hepmc_converter,
               genjet_clustering,
               out
-              ],
+    ],
     EvtSel = 'NONE',
+    # number of events
     EvtMax   = 10,
+    # all services should be put here
     ExtSvc = [podioevent],
-    #                EventLoop = eventloopmgr,
-    #                OutputLevel=DEBUG
+    # OutputLevel=DEBUG
  )
 
