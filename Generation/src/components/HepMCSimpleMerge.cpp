19667f5f254b2516df24f055459f9e35a44b0415
Valentin Volkl <valentin.volkl@cern.ch>
2016-02-08 17:22:57 +0100

    Fix indentation, iterator style, comments.

--- Generation/src/components/HepMCSimpleMerge.cpp
+++ Generation/src/components/HepMCSimpleMerge.cpp
@@ -9,11 +9,11 @@
 
 DECLARE_TOOL_FACTORY(HepMCSimpleMerge)
 
-HepMCSimpleMerge::HepMCSimpleMerge( 
-    const std::string& type,
-    const std::string& name, 
-    const IInterface* parent)
-    : GaudiTool(type, name, parent) {
+HepMCSimpleMerge::HepMCSimpleMerge(
+  const std::string& type,
+  const std::string& name,
+  const IInterface* parent)
+  : GaudiTool(type, name, parent) {
   declareInterface< IHepMCMergeTool >( this );
 }
 
@@ -30,18 +30,18 @@ HepMC::GenEvent* HepMCSimpleMerge::merge(const std::vector<HepMC::GenEvent>& eve
   // initial event is assumed to be signal event -- copied in full
   HepMC::GenEvent* pileEvent = new HepMC::GenEvent(eventVector[0]);
   // iterate over vertices and add them to pileEvent
-  for (auto it=eventVector.begin()+1; 
-      it != eventVector.end();
-       ++it) {
+  for (auto it=eventVector.cbegin()+1, end = eventVector.cend(); it != end; ++it) {
     for (auto p = (*it).particles_begin();
           p != (*it).particles_end();
           ++p ) {
-        // simple check if final-state particle: 
+        // simple check if final-state particle:
         // has no end vertex and correct status code meaning no further decays
         if( !(*p)->end_vertex() && (*p)->status()==1 ) {
+          // ownership of the particle  (tmpParticle) is given to the vertex (tmpVertex)
           HepMC::GenParticle* tmpParticle = new HepMC::GenParticle(**p);
           // each pile up particle is associated to a new production vertex
           // the position information is preserved
+          // ownership of the vertex (tmpVertex) is given to the event (tmpEvent)
           HepMC::GenVertex* tmpVertex = new HepMC::GenVertex((*p)->production_vertex()->position());
           tmpVertex->add_particle_out(tmpParticle);
           pileEvent->add_vertex(tmpVertex);
