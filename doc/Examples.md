3b8c9ab8097028f9d534e5fb9ce6a65cf3036415
zaborowska <azaborow@cern.ch>
2016-02-08 17:12:23 +0100

    commenting simple_pythia.py example

--- doc/Examples.md
+++ doc/Examples.md
@@ -1,19 +1,31 @@
 List of examples
 ====
 
-* options/simple_workflow.py
-  * reading events from a HepMC file
-  * converting `HepMC::GenEvent` to EDM
-  * using sample (jet clustering) algorithm
-  * writing the EDM output using PODIO algorithm
 * options/example_options.py
   * reading events from a HepMC file
   * dumping `HepMC::GenEvent`
   * using dummy algorithm `CreateSampleJet` that creates EDM `fcc::JetCollection`
+  * writing the EDM output using PODIO
+
+* options/simple_workflow.py
+  * reading events from a HepMC file
+  * converting `HepMC::GenEvent` to EDM
+  * using sample (jet clustering) algorithm
+  * writing the EDM output using PODIO
+
 * options/simple_pythia.py
+  * genereting PYTHIA events and saving them to HepMC
+  * converting `HepMC::GenEvent` to EDM
+  * filtering MC particles
+  * using sample (jet clustering) algorithm
+  * writing the EDM output using PODIO
+
 * options/geant_pgun_fullsim.py
+
 * options/geant_fullsim.py
+
 * options/PythiaDelphes_config.py
+
 * options/geant_fastsim.py
 
 
--- options/simple_pythia.py
+++ options/simple_pythia.py
@@ -1,36 +1,27 @@
-"""
-to run example
-> export PYTHIA8_XML=/afs/cern.ch/sw/lcg/releases/LCG_68/MCGenerators/pythia8/186/x86_64-slc6-gcc48-opt/xmldoc
-> ./run gaudirun.py simple_pythia.py
-"""
 from Gaudi.Configuration import *
 
-from Configurables import ApplicationMgr, FCCDataSvc
-
-#example of pythia configuration file to generate events
+# Example of pythia configuration file to generate events
 pythiafile="options/Pythia_standard.cmd"
-
-#example of pythia configuration file to read LH event file
+# Example of pythia configuration file to read LH event file
 #pythiafile="options/Pythia_LHEinput.cmd"
 
-podioevent   = FCCDataSvc("EventDataSvc")
+# Data service
+from Configurables import FCCDataSvc
+podioevent = FCCDataSvc("EventDataSvc")
 
-from Configurables import PythiaInterface
-# PythiaInterface parameter
-#pythia8gen = PythiaInterface("Pythia8Interface",Filename="main03.cmnd")
-pythia8gen = PythiaInterface(Filename=pythiafile)
-# write the HepMC::GenEvent to the data service
-pythia8gen.DataOutputs.hepmc.Path = "hepmc"
+# PYTHIA algorithm
+from Configurablese import PythiaInterface
+pythia8gen = PythiaInterface("Pythia8Interface", Filename=pythiafile)
+pythia8gen.DataOutputs.hepmc.Path = "hepmcevent"
 
+# Reads an HepMC::GenEvent from the data service and writes a collection of EDM Particles
 from Configurables import HepMCConverter
-# reads an HepMC::GenEvent from the data service and writes
-# a collection of EDM Particles
 hepmc_converter = HepMCConverter("Converter")
-# the input product name matches the output product name of the previous module
-hepmc_converter.DataInputs.hepmc.Path="hepmc"
+hepmc_converter.DataInputs.hepmc.Path="hepmcevent"
 hepmc_converter.DataOutputs.genparticles.Path="all_genparticles"
 hepmc_converter.DataOutputs.genvertices.Path="all_genvertices"
 
+# Filters generated particles
 from Configurables import GenParticleFilter
 genfilter = GenParticleFilter("StableParticles")
 genfilter.DataInputs.genparticles.Path = "all_genparticles"
@@ -39,14 +30,16 @@ genfilter.DataOutputs.genparticles.Path = "genparticles"
 from Configurables import JetClustering_fcc__MCParticleCollection_fcc__GenJetCollection_fcc__GenJetParticleAssociationCollection_ as JetClustering
 genjet_clustering = JetClustering("GenJetClustering", verbose = False)
 genjet_clustering.DataInputs.particles.Path='genparticles'
-# giving a meaningful name for the output product
 genjet_clustering.DataOutputs.jets.Path='genjets'
 genjet_clustering.DataOutputs.constituents.Path='genjets_particles'
 
+# PODIO algorithm
 from Configurables import PodioOutput
 out = PodioOutput("out",OutputLevel=DEBUG)
 out.outputCommands = ["keep *"]
 
+# ApplicationMgr
+from Configurables import ApplicationMgr
 ApplicationMgr( TopAlg = [ pythia8gen, hepmc_converter, genfilter, genjet_clustering, out ],
                 EvtSel = 'NONE',
                 EvtMax = 100,
