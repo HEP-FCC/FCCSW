c5cc48ac10eb6d71ff0b3b16392b2a00146558f8
Valentin Volkl <valentin.volkl@cern.ch>
2016-02-08 09:22:01 +0100

    Revert to mm in FlatSmearVertex ( units made uniform in gaussino ).

--- Generation/options/pileup_hepmcreader.py
+++ Generation/options/pileup_hepmcreader.py
@@ -4,13 +4,25 @@ from Configurables import ApplicationMgr, FCCDataSvc
 
 albersevent   = FCCDataSvc("EventDataSvc")
 # reads HepMC text file and write the HepMC::GenEvent to the data service
-from Configurables import HepMCReader, HepMCDumper, PoissonPileUp, HepMCFileReader
+from Configurables import HepMCReader, HepMCDumper, PoissonPileUp, HepMCFileReader, FlatSmearVertex
+
+# assuming mm
+smeartool = FlatSmearVertex(
+    xVertexMin = -0.5,
+    xVertexMax = 0.5,
+    yVertexMin = -0.5,
+    yVertexMax = 0.5,
+    zVertexMin = -35,
+    zVertexMax = 35 )
+
+
 genpileup = PoissonPileUp(name="Pileup", 
     Filename="/afs/cern.ch/user/v/vavolkl/public/example_MyPythia.dat", 
     numPileUpEvents=2)
 reader = HepMCReader("Reader", 
     Filename="/afs/cern.ch/user/v/vavolkl/public/example_MyPythia.dat", 
-    PileUpTool=genpileup)
+    PileUpTool=genpileup,
+    VertexSmearingTool = smeartool)
 reader.DataOutputs.hepmc.Path = "hepmc"
 
 # printout of the hepmc stuff
