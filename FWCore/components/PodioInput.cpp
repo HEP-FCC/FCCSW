ff345edeaecd157fbcb07ef16941cdedc0b00f42
vvolkl <valentin.voelkl@student.uibk.ac.at>
2016-02-26 10:55:45 +0100

    Ongoing development.

--- FWCore/components/PodioInput.cpp
+++ FWCore/components/PodioInput.cpp
@@ -2,6 +2,7 @@
 #include "FWCore/PodioDataSvc.h"
 
 
+
 DECLARE_COMPONENT(PodioInput)
 
 PodioInput::PodioInput(const std::string& name, ISvcLocator* svcLoc) :
@@ -13,6 +14,9 @@ GaudiAlgorithm(name, svcLoc),m_first(true)
   defaultCommands.push_back("keep *");
   declareProperty("inputCommands", m_inputCommands=defaultCommands,
                   "A set of commands to declare which collections to keep or drop.");
+  declareOutput("trackClusters", m_trackClusters,"hits/trackerClusters");
+  //declareOutput("trackHits", m_trackHits,"hits/trackerHits");
+  declareOutput("trackHitsClusters", m_trackHitsClusters,"hits/trackerAssociations");
 }
 
 StatusCode PodioInput::initialize() {
@@ -22,10 +26,33 @@ StatusCode PodioInput::initialize() {
   m_podioDataSvc = dynamic_cast<PodioDataSvc*>(evtSvc().get());
   if (nullptr == m_podioDataSvc) return StatusCode::FAILURE;
   m_switch = KeepDropSwitch(m_inputCommands);
+  m_reader.openFile("input.root");
+  m_store.setReader(&m_reader);
   return StatusCode::SUCCESS;
 }
 
 StatusCode PodioInput::execute() {
+  fcc::TrackCluster cluster = 0;
+  const fcc::TrackClusterCollection* clusters = nullptr;
+  bool is_available = m_store.get("clusters",clusters);
+  debug() << "clusters available: " << is_available << endmsg;
+  if(is_available){
+    for (auto cluster = clusters->begin(); cluster != clusters->end(); ++cluster) {
+      debug() << "Cluster has an energy of " << (*cluster).Core().Energy << endmsg;
+    }
+      m_store.clear();
+      m_reader.endOfEvent();
+      debug() << "End of event" << endmsg;
+      fcc::TrackClusterCollection* new_clusters = new fcc::TrackClusterCollection;
+      *new_clusters = *clusters;
+      m_trackClusters.put(new_clusters); 
+  }
+  /*
+  is_available = m_store.get("hitClusterAssociation", clusterHitsAssociations);
+  if(is_available) {
+    m_trackHitsClusters.put(clusterHitsAssociations); 
+  }
+  */
   return StatusCode::SUCCESS;
 }
 
--- FWCore/components/PodioInput.h
+++ FWCore/components/PodioInput.h
@@ -3,9 +3,14 @@
 
 #include "GaudiAlg/GaudiAlgorithm.h"
 #include "podio/CollectionBase.h"
+#include "podio/EventStore.h"
+#include "podio/ROOTReader.h"
 #include "FWCore/KeepDropSwitch.h"
+#include "FWCore/DataHandle.h"
 
 #include "TTree.h"
+#include "datamodel/TrackClusterCollection.h"
+#include "datamodel/TrackClusterHitsAssociationCollection.h"
 
 #include <vector>
 
@@ -36,6 +41,15 @@ private:
   TTree* m_datatree;
   TTree* m_metadatatree;
   std::vector<podio::CollectionBase*> m_storedCollections;
+  podio::ROOTReader m_reader;
+  podio::EventStore m_store;
+  const fcc::TrackClusterHitsAssociationCollection* clusterHitsAssociations;
+  /// Handle for tracker clusters
+  DataHandle<fcc::TrackClusterCollection> m_trackClusters;
+  /// Handle for tracker hits
+  //DataHandle<fcc::TrackHitCollection> m_trackHits;
+  /// Handle for tracker hits-clusters associations
+  DataHandle<fcc::TrackClusterHitsAssociationCollection> m_trackHitsClusters;
 
 };
 
