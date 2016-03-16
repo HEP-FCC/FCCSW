8135d150b811eb28edaac461ae8d28541aeb4089
Valentin Volkl <valentin.volkl@cern.ch>
2016-03-16 17:35:51 +0100

    Fix typo, reduce complexity to fit requirements.

--- Generation/options/pileup_hepmcreader.py
+++ Generation/options/pileup_hepmcreader.py
@@ -1,17 +1,23 @@
 from Gaudi.Configuration import *
 
-from FCCPileupScenarios import FCCPhase1Pileup
 from Configurables import ApplicationMgr, FCCDataSvc
+from FCCPileupScenarios import FCCPhase1PileupConf as pileupconf
 
 albersevent   = FCCDataSvc("EventDataSvc")
 # reads HepMC text file and write the HepMC::GenEvent to the data service
 from Configurables import HepMCReader, HepMCDumper, PoissonPileUp, HepMCFileReader, FlatSmearVertex
 
-pileupscenario = FCCPhase1Pileup('PileupScenario')
 smeartool = FlatSmearVertex(
-     **pileupscenario.VertexSmearingConfig)
-genpileup = PoissonPileUp(name="Pileup", Filename="/afs/cern.ch/exp/fcc/sw/0.6/testsamples/example_MyPythia.dat",
-    **pileupscenario.PileupToolConfig) 
+     xVertexMin=pileupconf['xVertexMin'],
+     xVertexMax=pileupconf['xVertexMax'],
+     yVertexMin=pileupconf['yVertexMin'],
+     yVertexMax=pileupconf['yVertexMax'],
+     zVertexMin=pileupconf['zVertexMin'],
+     zVertexMax=pileupconf['zVertexMax'])
+
+genpileup = PoissonPileUp(name="Pileup", 
+    Filename="/afs/cern.ch/exp/fcc/sw/0.6/testsamples/example_MyPythia.dat",
+    numPileUpEvents=pileupconf['numPileUpEvents']) 
 
 reader = HepMCReader("Reader", 
     Filename="/afs/cern.ch/exp/fcc/sw/0.6/testsamples/example_MyPythia.dat",
--- Generation/python/FCCPileupScenarios.py
+++ Generation/python/FCCPileupScenarios.py
@@ -1,32 +1,26 @@
 
+"""Beam Parameters Common to all FCC scenarios. Taken from
+Michael Benedikt, Daniel Schulte, and Frank Zimmermann: 
+"Optimizing integrated luminosity of future hadron colliders." 
+Physical Review Special Topics-Accelerators and Beams 18.10 (2015): 101002.
+http://dx.doi.org/10.1103/PhysRevSTAB.18.101002
+"""
+
 from GaudiKernel import SystemOfUnits as units
 
 
-class FCCPileupScenarioBase(object):
-    """Beam Parameters Common to all FCC scenarios. Taken from
-    Benedikt, Michael, Daniel Schulte, and Frank Zimmermann: 
-    "Optimizing integrated luminosity of future hadron colliders." 
-    Physical Review Special Topics-Accelerators and Beams 18.10 (2015): 101002.
-    http://dx.doi.org/10.1103/PhysRevSTAB.18.101002
-    """
-    def __init__(self, name):
-        self._name = name
-        self.VertexSmearingConfig = {
+_CommonFCCPileupConf = {
           "xVertexMin": -0.5 * units.mm,
           "xVertexMax": 0.5 * units.mm,
           "yVertexMin": -0.5 * units.mm,
           "yVertexMax": 0.5 * units.mm,
           "zVertexMin": -35 * units.mm,
-          "zVertexMax": 35 * units.mm
-        }
-        self.PileupToolConfig = {}
+          "zVertexMax": 35 * units.mm }
 
-class FCCPhase1Pileup(FCCPileupScenarioBase):
-    def __init__(self, name):
-        FCCPileupScenarioBase.__init__(self, name)
-        self.PileupToolConfig['numPileUpEvents'] = 180
 
-class FCCPhase2Pileup(FCCPileupScenarioBase):
-    def __init__(self, name):
-        FCCPileupScenarioBase.__init__(self, name)
-        self.PileupToolConfig['numPileUpEvents'] = 940
+FCCPhase1PileupConf = _CommonFCCPileupConf.copy()
+FCCPhase1PileupConf['numPileUpEvents'] = 180
+    
+FCCPhase2PileupConf = _CommonFCCPileupConf.copy()
+FCCPhase2PileupConf['numPileUpEvents'] = 940
+
