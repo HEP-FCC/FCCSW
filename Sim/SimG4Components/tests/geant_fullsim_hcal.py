20c148dcfb1879f9907767dee29be32db1adfea7
Valentin Volkl <valentin.volkl@cern.ch>
2016-03-24 11:53:36 +0100

    Move overview table to comments in config files.

--- Sim/SimG4Components/tests/geant_fullsim_hcal.py
+++ Sim/SimG4Components/tests/geant_fullsim_hcal.py
@@ -1,35 +1,44 @@
+
+
+### \file
+### \ingroup SimulationTests
+### | **input (alg)**                 | other algorithms                   |                                                           |                          |                                    | **output (alg)**                                |
+### | ------------------------------- | ---------------------------------- | --------------------------------------------------------- | ------------------------ | ---------------------------------- | ----------------------------------------------- |
+### | read events from a HepMC file   | convert `HepMC::GenEvent` to EDM   | geometry taken from XML            | FTFP_BERT physics list   | save HCAL hits              | write the EDM output to ROOT file using PODIO   |
+
 from Gaudi.Configuration import *
-# Data service
+
 from Configurables import FCCDataSvc
+## Data service
 podioevent = FCCDataSvc("EventDataSvc")
 
-# reads HepMC text file and write the HepMC::GenEvent to the data service
 from Configurables import HepMCReader
+## reads HepMC text file and write the HepMC::GenEvent to the data service
 reader = HepMCReader("Reader", Filename="example_MyPythia.dat")
 reader.DataOutputs.hepmc.Path = "hepmc"
 
-# reads an HepMC::GenEvent from the data service and writes a collection of EDM Particles
 from Configurables import HepMCConverter
+## reads an HepMC::GenEvent from the data service and writes a collection of EDM Particles
 hepmc_converter = HepMCConverter("Converter")
 hepmc_converter.DataInputs.hepmc.Path="hepmc"
 hepmc_converter.DataOutputs.genparticles.Path="allGenParticles"
 hepmc_converter.DataOutputs.genvertices.Path="allGenVertices"
 
-# Geant4 service
-# Configures the Geant simulation: geometry, physics list and user actions
 from Configurables import G4SimSvc
+## Geant4 service
+# Configures the Geant simulation: geometry, physics list and user actions
 geantservice = G4SimSvc("G4SimSvc", detector="G4DD4hepDetector", physicslist="G4FtfpBert", actions="G4FullSimActions")
 
-# DD4hep geometry service
-# Parses the given xml file
 from Configurables import GeoSvc
+## DD4hep geometry service
+# Parses the given xml file
 geoservice = GeoSvc("GeoSvc", detectors=['file:DetectorDescription/Detectors/compact/FCChh_DectMaster.xml',
                                          'file:DetectorDescription/Detectors/compact/FCChh_HCalBarrel_TileCal.xml' ],
                     OutputLevel = DEBUG)
 
-# Geant4 algorithm
-# Translates EDM to G4Event, passes the event to G4, writes out outputs via tools
 from Configurables import G4SimAlg, G4SaveCalHits
+## Geant4 algorithm
+# Translates EDM to G4Event, passes the event to G4, writes out outputs via tools
 # first, create a tool that saves the calorimeter hits (of type "hcal")
 # Name of that tool in GAUDI is "XX/YY" where XX is the tool class name ("G4SaveTrackerHits")
 # and YY is the given name ("saveTrackerHits")
@@ -41,18 +50,15 @@ geantsim = G4SimAlg("G4SimAlg",
                         outputs = ["G4SaveCalHits/saveHCalHits"] )
 geantsim.DataInputs.genParticles.Path="allGenParticles"
 
-# PODIO algorithm
 from Configurables import PodioOutput
 out = PodioOutput("out",
                    OutputLevel=DEBUG)
 out.outputCommands = ["keep *"]
 
-# ApplicationMgr
 from Configurables import ApplicationMgr
-ApplicationMgr( TopAlg = [reader, hepmc_converter, geantsim, out],
-                EvtSel = 'NONE',
-                EvtMax   = 1,
+ApplicationMgr( TopAlg=[reader, hepmc_converter, geantsim, out],
+                EvtSel='NONE',
+                EvtMax=1,
                 # order is important, as GeoSvc is needed by G4SimSvc
-                ExtSvc = [podioevent, geoservice, geantservice],
-                OutputLevel=DEBUG
- )
+                ExtSvc=[podioevent, geoservice, geantservice],
+                OutputLevel=DEBUG)
