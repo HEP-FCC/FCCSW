876328b33828390ed18319487b7e05cd68b25906
zaborowska <azaborow@cern.ch>
2016-04-20 15:50:27 +0200

    Adapting tests to new input file

--- Sim/SimG4Components/tests/scripts/geant_fastsim_checkNoParticles.py
+++ Sim/SimG4Components/tests/scripts/geant_fastsim_checkNoParticles.py
@@ -4,11 +4,10 @@ from EventStore import EventStore
 gSystem.Load("libdatamodelDict")
 store = EventStore(["./out_fast_simple.root"])
 
-entries = [155, 126, 72, 66, 46, 46, 67, 52, 186, 41]
+entries = [82, 190, 276, 29, 24, 62, 115, 69, 2, 262]
 
 for iev, event in enumerate(store):
     if(iev<10):
         particles = event.get('smearedParticles')
-        print(entries[iev])
-        print(len(particles))
+        print('test=',entries[iev],len(particles))
         assert(len(particles) == entries[iev])
