4b601f4744f86decb12b190e3fa68904c45f58b0
Anna <anna.zaborowska@cern.ch>
2016-04-28 16:56:51 +0200

    Last py script for tests fixed

--- DetectorDescription/DetSensitive/tests/scripts/cmpTrackerSdHitsAndClusters.py
+++ DetectorDescription/DetSensitive/tests/scripts/cmpTrackerSdHitsAndClusters.py
@@ -1,3 +1,6 @@
+from ROOT import gSystem, gROOT, TCanvas, TH2D, gStyle
+from EventStore import EventStore
+
 bitfieldsize = 6
 cellNo = 51
 width = 1020
@@ -28,27 +31,26 @@ def pos2cell(pos):
 def z2cell(z):
     return int(floor((z-width/2.+0.5*cellSize)/cellSize))
 
-from ROOT import gSystem, gROOT, TCanvas, TH2D, gStyle
-from EventStore import EventStore
 
-gSystem.Load("libdatamodelDict")
-store = EventStore(["./out_simpleTrackerSD_2cm.root"])
-for iev, event in enumerate(store):
-    hitsClus = event.get('hitClusterAssociation')
-    print(len(hitsClus))
-    for assoc in hitsClus:
-        hit = assoc.Hit()
-        clu = assoc.Cluster()
-        cellId = hit.Core().Cellid
-        hxx = x(cellId)
-        hyy = y(cellId)
-        hzz = z(cellId)
-        hE = hit.Core().Energy
-        cxx = pos2cell(clu.Core().position.X)
-        cyy = pos2cell(clu.Core().position.Y)
-        czz = z2cell(clu.Core().position.Z)
-        cE = clu.Core().Energy
-        assert(hxx == cxx)
-        assert(hyy == cyy)
-        assert(hzz == czz)
-        assert(hE == cE)
+if __name__ == "__main__":
+    gSystem.Load("libdatamodelDict")
+    store = EventStore(["./out_simpleTrackerSD_2cm.root"])
+    for iev, event in enumerate(store):
+        hitsClus = event.get('hitClusterAssociation')
+        print(len(hitsClus))
+        for assoc in hitsClus:
+            hit = assoc.Hit()
+            clu = assoc.Cluster()
+            cellId = hit.Core().Cellid
+            hxx = x(cellId)
+            hyy = y(cellId)
+            hzz = z(cellId)
+            hE = hit.Core().Energy
+            cxx = pos2cell(clu.Core().position.X)
+            cyy = pos2cell(clu.Core().position.Y)
+            czz = z2cell(clu.Core().position.Z)
+            cE = clu.Core().Energy
+            assert(hxx == cxx)
+            assert(hyy == cyy)
+            assert(hzz == czz)
+            assert(hE == cE)
