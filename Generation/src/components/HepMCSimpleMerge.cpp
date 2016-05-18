9b315b293089e4c064e1016cbd5da99faa3bdd4c
Valentin Volkl <valentin.volkl@cern.ch>
2016-05-18 10:03:26 +0200

    Use mapping from input to output vertices also for SimpleMerge.
    This tool now still only keeps the final state particles, but keeps
    daughters of a production vertex attached to the same vertex in the
    merged event.

--- Generation/src/components/HepMCSimpleMerge.cpp
+++ Generation/src/components/HepMCSimpleMerge.cpp
@@ -31,20 +31,27 @@ HepMC::GenEvent* HepMCSimpleMerge::merge(const std::vector<HepMC::GenEvent>& eve
   HepMC::GenEvent* pileEvent = new HepMC::GenEvent(eventVector[0]);
   // iterate over vertices and add them to pileEvent
   for (auto it=eventVector.cbegin()+1, end = eventVector.cend(); it != end; ++it) {
+    std::map<const HepMC::GenVertex*, HepMC::GenVertex*> inputToMergedVertexMap;
+    for (auto v = (*it).vertices_begin(); v != (*it).vertices_end(); ++v ) {
+        HepMC::GenVertex* newVertex = new HepMC::GenVertex((*v)->position());
+        inputToMergedVertexMap[*v] = newVertex;
+      }
     for (auto p = (*it).particles_begin();
           p != (*it).particles_end();
           ++p ) {
         // simple check if final-state particle:
         // has no end vertex and correct status code meaning no further decays
-        if( !(*p)->end_vertex() && (*p)->status()==1 ) {
-          // ownership of the particle  (tmpParticle) is given to the vertex (tmpVertex)
-          HepMC::GenParticle* tmpParticle = new HepMC::GenParticle(**p);
+        if(!(*p)->end_vertex() && (*p)->status() == 1) {
+          // ownership of the particle  (newParticle) is then given to the vertex (newVertex)
+          HepMC::GenParticle* newParticle = new HepMC::GenParticle(**p);
           // each pile up particle is associated to a new production vertex
           // the position information is preserved
-          // ownership of the vertex (tmpVertex) is given to the event (tmpEvent)
-          HepMC::GenVertex* tmpVertex = new HepMC::GenVertex((*p)->production_vertex()->position());
-          tmpVertex->add_particle_out(tmpParticle);
-          pileEvent->add_vertex(tmpVertex);
+          // ownership of the vertex (newVertex) is given to the event (newEvent)
+          HepMC::GenVertex* newVertex = inputToMergedVertexMap[(*p)->production_vertex()];
+          newVertex->add_particle_out(newParticle);
+          if (newVertex->parent_event() != pileEvent) {
+            pileEvent->add_vertex(newVertex);
+          }
         }
     }
   }
