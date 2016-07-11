9a2b3db679d047a311310a4a3a600e447e89e79e
Valentin Volkl <valentin.volkl@cern.ch>
2016-07-11 15:12:50 +0200

    Correct constness.

--- Detector/DetCommon/DetCommon/DetUtils.h
+++ Detector/DetCommon/DetCommon/DetUtils.h
@@ -4,9 +4,9 @@
 #include "DD4hep/DetFactoryHelper.h"
 
 namespace det {
-DD4hep::XML::Component getNodeByStrAttr(DD4hep::XML::Handle_t mother, const char* nodeName, const char* attrName,
-                                        std::string attrValue);
+DD4hep::XML::Component getNodeByStrAttr(const DD4hep::XML::Handle_t mother, const char* nodeName, const char* attrName,
+                                        const std::string attrValue);
 
-double getDoubleWithDefault(DD4hep::XML::Component node, const char* attrName, double defaultValue);
+double getDoubleWithDefault(const DD4hep::XML::Component node, const char* attrName, const double defaultValue);
 }
 #endif
--- Detector/DetCommon/src/DetUtils.cpp
+++ Detector/DetCommon/src/DetUtils.cpp
@@ -5,18 +5,19 @@
 /// this method returns the first daughter of type nodeName whose attribute has a given value
 /// e.g. returns <layer name="2"/> when called with (detector, "layer", "name", "1")
 /// @todo: runtime checks that attribute is present
-DD4hep::XML::Component det::getNodeByStrAttr(DD4hep::XML::Handle_t mother, const char* nodeName, const char* attrName,
-                                             std::string attrValue) {
+DD4hep::XML::Component det::getNodeByStrAttr(const DD4hep::XML::Handle_t mother, const char* nodeName, const char* attrName,
+                                             const std::string attrValue) {
   for (DD4hep::XML::Collection_t xCompColl(mother, nodeName); nullptr != xCompColl; ++xCompColl) {
     if (xCompColl.attr<std::string>(attrName) == attrValue) {
       return static_cast<DD4hep::XML::Component>(xCompColl);
     }
   }
+  // in case there was no xml daughter with matching name
   return DD4hep::XML::Component(nullptr);
 }
 
 /// try to get attribute with double value, return defaultValue if attribute not found
-double det::getDoubleWithDefault(DD4hep::XML::Component node, const char* attrName, double defaultValue) {
+double det::getDoubleWithDefault(const DD4hep::XML::Component node, const char* attrName, const double defaultValue) {
   if (node.hasAttr(attrName)) {
     return node.attr<double>(attrName);
   } else {
