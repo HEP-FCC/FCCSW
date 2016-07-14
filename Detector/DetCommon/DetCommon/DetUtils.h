6a7410eb54e79071e790607817eeeabc3cd9926f
Valentin Volkl <valentin.volkl@cern.ch>
2016-07-14 16:03:36 +0200

    Move comment to header.

--- Detector/DetCommon/DetCommon/DetUtils.h
+++ Detector/DetCommon/DetCommon/DetUtils.h
@@ -3,10 +3,15 @@
 
 #include "DD4hep/DetFactoryHelper.h"
 
+/** Given a XML element with several daughters with the same name, e.g.
+ <detector> <layer name="1" /> <layer name="2"> </detector>
+ this method returns the first daughter of type nodeName whose attribute has a given value
+ e.g. returns <layer name="2"/> when called with (detector, "layer", "name", "1") */
 namespace det {
 DD4hep::XML::Component getNodeByStrAttr(const DD4hep::XML::Handle_t& mother, const std::string& nodeName, const std::string& attrName,
                                         const std::string& attrValue);
 
+/// try to get attribute with double value, return defaultValue if attribute not found
 double getDoubleWithDefault(const DD4hep::XML::Component& node, const std::string& attrName, const double& defaultValue);
 }
 #endif
--- Detector/DetCommon/src/DetUtils.cpp
+++ Detector/DetCommon/src/DetUtils.cpp
@@ -1,10 +1,5 @@
 #include "DetCommon/DetUtils.h"
 
-/// Given a XML element with several daughters with the same name, e.g.
-/// <detector> <layer name="1" /> <layer name="2"> </detector>
-/// this method returns the first daughter of type nodeName whose attribute has a given value
-/// e.g. returns <layer name="2"/> when called with (detector, "layer", "name", "1")
-/// @todo: runtime checks that attribute is present
 DD4hep::XML::Component det::getNodeByStrAttr(const DD4hep::XML::Handle_t& mother, const std::string& nodeName, const std::string& attrName,
                                              const std::string& attrValue) {
   for (DD4hep::XML::Collection_t xCompColl(mother, nodeName.c_str()); nullptr != xCompColl; ++xCompColl) {
@@ -16,7 +11,6 @@ DD4hep::XML::Component det::getNodeByStrAttr(const DD4hep::XML::Handle_t& mother
   return DD4hep::XML::Component(nullptr);
 }
 
-/// try to get attribute with double value, return defaultValue if attribute not found
 double det::getDoubleWithDefault(const DD4hep::XML::Component& node, const std::string& attrName, const double& defaultValue) {
   if (node.hasAttr(attrName.c_str())) {
     return node.attr<double>(attrName.c_str());
