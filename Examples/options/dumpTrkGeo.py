fedf2fbf2967b52596293bc5ce48d502edc348f7
Valentin Volkl <valentin.volkl@cern.ch>
2016-07-21 11:21:25 +0200

    Usage example for tracking geometry dump service.

---
+++ Examples/options/dumpTrkGeo.py
@@ -0,0 +1,37 @@
+import os
+from Gaudi.Configuration import *
+
+from Configurables import GeoSvc
+geoservice = GeoSvc("GeoSvc", detectors=['file:Detector/DetFCChhBaseline1/compact/FCChh_DectEmptyMaster.xml',
+  'file:Detector/DetFCChhTrackerSimple/compact/Tracker.xml'],
+                    OutputLevel = DEBUG)
+
+from Configurables import TrackingGeoSvc
+trkgeoservice = TrackingGeoSvc("TrackingGeometryService") 
+
+from Configurables import SimG4Svc
+geantservice = SimG4Svc("SimG4Svc",
+                        detector='SimG4DD4hepDetector',
+                        physicslist="SimG4FtfpBert",
+                        actions="SimG4FullSimActions")
+
+export_fname = "TestBox.gdml"
+# check if file exists and delete it:
+if os.path.isfile(export_fname):
+    os.remove(export_fname)
+
+
+from Configurables import GeoToGdmlDumpSvc
+geodumpservice = GeoToGdmlDumpSvc("GeoDump", gdml=export_fname)
+
+from Configurables import TrkGeoToGdmlDumpSvc
+trkgeodumpservice = TrkGeoToGdmlDumpSvc("TrkGeoDump", gdml="TrkGeoDump.gdml")
+
+from Configurables import ApplicationMgr
+ApplicationMgr( TopAlg = [],
+                EvtSel = 'NONE',
+                EvtMax   = 1,
+                # order is important, as GeoSvc is needed by SimG4Svc
+                ExtSvc = [geoservice, geantservice, trkgeoservice, geodumpservice, trkgeodumpservice],
+                OutputLevel=DEBUG
+ )
