dd01f4a61d7c1b516612fef56ad606b42b546edf
Joschka Lingemann <joschka.lingemann@cern.ch>
2016-08-02 14:34:42 +0200

    Add tests for podio reader

--- FWCore/tests/scripts/check_coll_after_read.py
+++ FWCore/tests/scripts/check_coll_after_read.py
@@ -2,11 +2,19 @@ from ROOT import gSystem
 from EventStore import EventStore
 
 gSystem.Load("libdatamodelDict")
-store = EventStore(["./output.root"])
-store_after = EventStore(["./out2.root"])
+store = EventStore(["./pythia_test.root"])
+store_after = EventStore(["./test.root"])
 
-for iev, event in enumerate(store):
+nevts = min(len(store_after), len(store))
+for iev in range(nevts):
     event_after = store_after[iev]
-    hits_before = event.get("hits")
-    hits_after = event_after.get('hits')
-    assert(len(hits_before) == len(hits_after))
+    event = store[iev]
+
+    particles_before = event.get("allGenParticles")
+    particles_after = event_after.get('allGenParticles')
+    assert(len(particles_before) == len(particles_after))
+
+    for before, after in zip(particles_before, particles_after):
+        assert(before.Core().P4.Px == after.Core().P4.Px)
+        assert(before.Core().P4.Py == after.Core().P4.Py)
+        assert(before.Core().P4.Pz == after.Core().P4.Pz)
