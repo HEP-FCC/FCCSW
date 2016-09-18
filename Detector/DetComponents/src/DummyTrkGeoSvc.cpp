2f53322bb02cd33825f4f70ca59d5ee45cb0d228
Valentin Volkl <valentin.volkl@cern.ch>
2016-09-18 19:43:39 +0200

    Make ACTS Detector Example accessible in FCCSW.

---
+++ Detector/DetComponents/src/DummyTrkGeoSvc.cpp
@@ -0,0 +1,24 @@
+#include "DummyTrkGeoSvc.h"
+
+// ACTS
+#include "ACTS/Detector/TrackingGeometry.hpp"
+#include "ACTS/Examples/BuildGenericDetector.hpp""
+
+
+DECLARE_SERVICE_FACTORY(DummyTrkGeoSvc)
+
+DummyTrkGeoSvc::DummyTrkGeoSvc(const std::string& name, ISvcLocator* svc)
+    : base_class(name, svc), m_trackingGeo(nullptr) {}
+
+DummyTrkGeoSvc::~DummyTrkGeoSvc() {}
+
+StatusCode DummyTrkGeoSvc::initialize() {
+  if (Service::initialize().isFailure()) {
+    error() << "Unable to initialize Service()" << endmsg;
+    return StatusCode::FAILURE;
+  }
+  m_trackingGeo = Acts::trackingGeometry();
+  return StatusCode::SUCCESS;
+}
+
+StatusCode DummyTrkGeoSvc::finalize() { return StatusCode::SUCCESS; }
