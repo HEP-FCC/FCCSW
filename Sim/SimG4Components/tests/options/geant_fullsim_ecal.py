e95ebe0ba1e94760cce498572f49282a68d71cb8
Jana <jana.faltova@cern.ch>
2016-09-20 22:29:30 +0200

    ECAL Readout name fixed

--- Sim/SimG4Components/tests/options/geant_fullsim_ecal.py
+++ Sim/SimG4Components/tests/options/geant_fullsim_ecal.py
@@ -31,7 +31,7 @@ from Configurables import SimG4Alg, SimG4SaveCalHits, SimG4PrimariesFromEdmTool
 # first, create a tool that saves the calorimeter hits (of type "ecal")
 # Name of that tool in GAUDI is "XX/YY" where XX is the tool class name ("SimG4SaveTrackerHits")
 # and YY is the given name ("saveTrackerHits")
-saveecaltool = SimG4SaveCalHits("saveECalHits", readoutNames = ["BarECal_Readout"])
+saveecaltool = SimG4SaveCalHits("saveECalHits", readoutNames = ["ECalHitsPhiEta"])
 saveecaltool.DataOutputs.caloClusters.Path = "caloClusters"
 saveecaltool.DataOutputs.caloHits.Path = "caloHits"
 # next, create the G4 algorithm, giving the list of names of tools ("XX/YY")
