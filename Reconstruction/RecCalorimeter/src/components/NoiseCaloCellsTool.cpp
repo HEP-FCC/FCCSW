1399fb40df0b4dc1a955a39dfc24cfc095816001
Anna <anna.zaborowska@cern.ch>
2016-11-29 10:42:46 +0100

    changing units in noise tool to GeV

--- Reconstruction/RecCalorimeter/src/components/NoiseCaloCellsTool.cpp
+++ Reconstruction/RecCalorimeter/src/components/NoiseCaloCellsTool.cpp
@@ -2,13 +2,14 @@
 
 DECLARE_TOOL_FACTORY(NoiseCaloCellsTool)
 
-NoiseCaloCellsTool::NoiseCaloCellsTool(const std::string& type,const std::string& name, const IInterface* parent) 
+NoiseCaloCellsTool::NoiseCaloCellsTool(const std::string& type,const std::string& name, const IInterface* parent)
   : GaudiTool(type, name, parent)
 {
   declareInterface<INoiseCaloCellsTool>(this);
-  declareProperty("cellNoise", m_cellNoise=50.0);
+  //uniform noise per cell in GeV
+  declareProperty("cellNoise", m_cellNoise=0.003);
   //remove cells with energy bellow filterThreshold (threshold is multiplied by a cell noise sigma)
-  declareProperty("filterNoiseThreshold", m_filterThreshold=3);
+  declareProperty("filterNoiseThreshold", m_filterThreshold=0.003); // in GeV
 }
 
 NoiseCaloCellsTool::~NoiseCaloCellsTool()
@@ -26,7 +27,7 @@ StatusCode NoiseCaloCellsTool::initialize() {
   }
   m_gauss.initialize(m_randSvc, Rndm::Gauss(0,m_cellNoise));
 
-  info() << "Sigma of the cell noise: " <<  m_cellNoise << " MeV" << endmsg;
+  info() << "Sigma of the cell noise: " <<  m_cellNoise * 1.e3 << " MeV" << endmsg;
   info() << "Filter noise threshold: " <<  m_filterThreshold << "*sigma" << endmsg;
   return sc;
 }
