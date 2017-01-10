de03c496c564c35dfde8fd5a605425255e118e14
nalipour <nalipour@cern.ch>
2017-01-10 09:51:12 +0100

    DetFCCee: use existing drivers in DD4hep instead of the ones described in lcgeo.

---
+++ Examples/options/geant_fullsim_FCCee.py
@@ -0,0 +1,71 @@
+
+### \file
+### \ingroup SimulationExamples
+### | **input (alg)**               | other algorithms                 |                                                         |                        |                                  | **output (alg)**                              |
+### |-------------------------------|----------------------------------|---------------------------------------------------------|------------------------|----------------------------------|-----------------------------------------------|
+### | read events from a HepMC file | convert `HepMC::GenEvent` to EDM | geometry taken from GDML file (no sensitive detectors!) | FTFP_BERT physics list | empty action initialisation list | write the EDM output to ROOT file using PODIO |
+
+
+from Gaudi.Configuration import *
+
+# Data service
+from Configurables import FCCDataSvc
+podioevent = FCCDataSvc("EventDataSvc")
+
+# reads HepMC text file and write the HepMC::GenEvent to the data service
+from Configurables import HepMCReader
+reader = HepMCReader("Reader", Filename="/afs/cern.ch/exp/fcc/sw/0.7/testsamples/FCC_minbias_100TeV.dat")
+reader.DataOutputs.hepmc.Path = "hepmc"
+
+# reads an HepMC::GenEvent from the data service and writes a collection of EDM Particles
+from Configurables import HepMCConverter
+hepmc_converter = HepMCConverter("Converter")
+hepmc_converter.DataInputs.hepmc.Path="hepmc"
+hepmc_converter.DataOutputs.genparticles.Path="allGenParticles"
+hepmc_converter.DataOutputs.genvertices.Path="allGenVertices"
+
+# DD4hep geometry service
+# Parses the given xml file
+from Configurables import GeoSvc
+"""geoservice = GeoSvc("GeoSvc", detectors=['file:Detector/DetFCChhBaseline1/compact/FCChh_DectEmptyMaster.xml',
+  'file:Detector/DetFCChhTrackerTkLayout/compact/Tracker.xml'])"""
+geoservice = GeoSvc("GeoSvc", detectors=['file:Detector/DetFCCeeBaseline1/compact/FCCee_o3_v03.xml'])
+
+
+# Geant4 service
+# Configures the Geant simulation: geometry, physics list and user actions
+from Configurables import SimG4Svc
+# giving the names of tools will initialize the tools of that type
+geantservice = SimG4Svc("SimG4Svc", detector='SimG4DD4hepDetector', physicslist="SimG4FtfpBert", actions="SimG4FullSimActions")
+
+# Geant4 algorithm
+# Translates EDM to G4Event, passes the event to G4, writes out outputs via tools
+from Configurables import SimG4Alg, SimG4SaveTrackerHits, SimG4PrimariesFromEdmTool
+# first, create a tool that saves the tracker hits
+# Name of that tool in GAUDI is "XX/YY" where XX is the tool class name ("SimG4SaveTrackerHits")
+# and YY is the given name ("saveTrackerHits")
+savetrackertool = SimG4SaveTrackerHits("saveTrackerHits", readoutNames = ["VertexBarrelCollection", "VertexEndcapCollection"])
+savetrackertool.DataOutputs.positionedTrackHits.Path = "positionedHits"
+savetrackertool.DataOutputs.trackHits.Path = "hits"
+# next, create the G4 algorithm, giving the list of names of tools ("XX/YY")
+particle_converter = SimG4PrimariesFromEdmTool("EdmConverter")
+particle_converter.DataInputs.genParticles.Path = "allGenParticles"
+geantsim = SimG4Alg("SimG4Alg",
+                    outputs = ["SimG4SaveTrackerHits/saveTrackerHits"],
+                    eventProvider=particle_converter)
+
+# PODIO algorithm
+from Configurables import PodioOutput
+out = PodioOutput("out",
+                   OutputLevel=DEBUG)
+out.outputCommands = ["keep *"]
+
+# ApplicationMgr
+from Configurables import ApplicationMgr
+ApplicationMgr( TopAlg = [reader, hepmc_converter, geantsim, out],
+                EvtSel = 'NONE',
+                EvtMax   = 1,
+                # order is important, as GeoSvc is needed by SimG4Svc
+                ExtSvc = [podioevent, geoservice, geantservice],
+                OutputLevel=DEBUG
+ )
---
+++ Examples/options/geant_fullsim_pgun_geantino_FCCee.py
@@ -0,0 +1,51 @@
+from Gaudi.Configuration import *
+
+# Data service
+from Configurables import FCCDataSvc
+podioevent = FCCDataSvc("EventDataSvc")
+
+# DD4hep geometry service
+# Parses the given xml file
+from Configurables import GeoSvc, SimG4SingleParticleGeneratorTool
+
+geoservice = GeoSvc("GeoSvc", detectors=['file:Detector/DetFCCeeBaseline1/compact/FCCee_o3_v03.xml'],                                                 
+                    OutputLevel = DEBUG)
+
+# Geant4 service
+# Configures the Geant simulation: geometry, physics list and user actions
+from Configurables import SimG4Svc
+# giving the names of tools will initialize the tools of that type
+geantservice = SimG4Svc("SimG4Svc", detector='SimG4DD4hepDetector', physicslist="SimG4FtfpBert", actions="SimG4FullSimActions")
+
+#geantservice.G4commands += ["/tracking/verbose 1"]
+
+# Geant4 algorithm
+# Translates EDM to G4Event, passes the event to G4, writes out outputs via tools
+from Configurables import SimG4Alg, SimG4SaveTrackerHits
+# first, create a tool that saves the tracker hits
+# Name of that tool in GAUDI is "XX/YY" where XX is the tool class name ("SimG4SaveTrackerHits")
+# and YY is the given name ("saveTrackerHits")
+savetrackertool = SimG4SaveTrackerHits("saveTrackerHits", readoutNames = ["VertexBarrelCollection", "VertexEndcapCollection"])
+savetrackertool.DataOutputs.positionedTrackHits.Path = "positionedHits"
+savetrackertool.DataOutputs.trackHits.Path = "hits"
+# next, create the G4 algorithm, giving the list of names of tools ("XX/YY")
+pgun = SimG4SingleParticleGeneratorTool("GeantinoGun", etaMin=-0.1, etaMax=0.1, particleName="mu-", energyMin=10, energyMax=10)
+geantsim = SimG4Alg("SimG4Alg",
+                    outputs= ["SimG4SaveTrackerHits/saveTrackerHits" ],
+                    eventProvider=pgun)
+
+# PODIO algorithm
+from Configurables import PodioOutput
+out = PodioOutput("naliout.root",
+                   OutputLevel=DEBUG)
+out.outputCommands = ["keep *"]
+
+# ApplicationMgr
+from Configurables import ApplicationMgr
+ApplicationMgr( TopAlg = [geantsim, out],
+                EvtSel = 'NONE',
+                EvtMax   = 1000,
+                # order is important, as GeoSvc is needed by SimG4Svc
+                ExtSvc = [podioevent, geoservice, geantservice],
+                OutputLevel=DEBUG
+ )
