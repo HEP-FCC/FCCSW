789d24e8bd9561c7a924a712e4cecb1455bb1fe6
Jana <jana.faltova@cern.ch>
2017-01-11 10:20:57 +0100

    ecal configuration simplified

--- Reconstruction/RecCalorimeter/tests/options/runEcalReconstruction.py
+++ Reconstruction/RecCalorimeter/tests/options/runEcalReconstruction.py
@@ -13,18 +13,31 @@ geoservice = GeoSvc("GeoSvc", detectors=[  'file:Detector/DetFCChhBaseline1/comp
                                            'file:Detector/DetFCChhECalSimple/compact/FCChh_ECalBarrel_Mockup.xml'],
                     OutputLevel = INFO)
 
-#from ecalCommonSetup import *
+# common ECAL specific information
+# readout name
+ecalReadoutName = "ECalHitsPhiEta"
+# active material identifier name
+ecalIdentifierName = "active_layer"
+# active material volume name
+ecalVolumeName = "LAr_sensitive"
+# number of active layers to be merged to create cells
+ecalNumberOfLayersToMerge = [19,71,9]
+# number of ECAL layers
+ecalNumberOfLayers = len(ecalNumberOfLayersToMerge)
+# ECAL bitfield names & values
+ecalFieldNames=["system","ECAL_Cryo","bath","EM_barrel"]
+ecalFieldValues=[5,1,1,1]
 
 from Configurables import MergeLayers
 mergelayers = MergeLayers("MergeLayers",
                    # take the bitfield description from the geometry service
-                   readout = "ECalHitsPhiEta",
+                   readout = ecalReadoutName,
                    # cells in which field should be merged
-                   identifier = "active_layer",
-                   volumeName = "LAr_sensitive",
+                   identifier = ecalIdentifierName,
+                   volumeName = ecalVolumeName,
                    # how many cells to merge
                    # merge first 19 into new cell (id=0), next 71 into second cell (id=1), ...
-                   merge = [19,71,9],
+                   merge = ecalNumberOfLayersToMerge,
                    OutputLevel = INFO)
 mergelayers.DataInputs.inhits.Path = "ECalHits"
 mergelayers.DataOutputs.outhits.Path = "mergedECalHits"
@@ -42,16 +55,16 @@ createcells = CreateCaloCells("CreateCaloCells",
                               noiseTool=noisefile,
                               addCellNoise=True, filterCellNoise=False,
                               useVolumeIdOnly=False,
-                              readoutName="ECalHitsPhiEta",
-                              fieldNames=["system","ECAL_Cryo","bath","EM_barrel"],
-                              fieldValues=[5,1,1,1],
-                              activeVolumesNumber=3,
+                              readoutName=ecalReadoutName,
+                              fieldNames=ecalFieldNames,
+                              fieldValues=ecalFieldValues,
+                              activeVolumesNumber=ecalNumberOfLayers,
                               OutputLevel=INFO)
 createcells.DataInputs.hits.Path="mergedECalHits"
 createcells.DataOutputs.cells.Path="caloCells"
 
 from Configurables import CreatePositionedHit
-positionhit = CreatePositionedHit("CreatePositionedHit", readoutName = "ECalHitsPhiEta",activeFieldName = "active_layer",activeVolumeName="LAr_sensitive")
+positionhit = CreatePositionedHit("CreatePositionedHit", readoutName = ecalReadoutName,activeFieldName = ecalIdentifierName,activeVolumeName=ecalVolumeName)
 positionhit.DataInputs.caloCells.Path="caloCells"
 positionhit.DataOutputs.caloPositionedHits.Path="caloCellsPositions"
 
@@ -59,9 +72,9 @@ positionhit.DataOutputs.caloPositionedHits.Path="caloCellsPositions"
 from Configurables import CreateCaloClustersSlidingWindow
 from GaudiKernel.PhysicalConstants import pi
 createclusters = CreateCaloClustersSlidingWindow("CreateCaloClusters",
-                                                 readoutName = "ECalHitsPhiEta",
-                                                 fieldNames = ["system","ECAL_Cryo","bath","EM_barrel"],
-                                                 fieldValues = [5,1,1,1],
+                                                 readoutName = ecalReadoutName,
+                                                 fieldNames = ecalFieldNames,
+                                                 fieldValues = ecalFieldValues,
                                                  deltaEtaTower = 0.01, deltaPhiTower = 2*pi/629.,
                                                  nEtaWindow = 5, nPhiWindow = 15,
                                                  nEtaPosition = 3, nPhiPosition = 3,
