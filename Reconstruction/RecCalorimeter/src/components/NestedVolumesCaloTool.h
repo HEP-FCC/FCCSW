f9d49214a3db8e26f15686b668472b5a10e1fbc9
Anna Zaborowska <anna.zaborowska@cern.ch>
2017-01-17 12:38:50 +0100

    Creating the digitisation for HCal

--- Reconstruction/RecCalorimeter/src/components/NestedVolumesCaloTool.h
+++ Reconstruction/RecCalorimeter/src/components/NestedVolumesCaloTool.h
@@ -1,5 +1,5 @@
-#ifndef RECCALORIMETER_NOSEGMENTATIONCALOTOOL_H
-#define RECCALORIMETER_NOSEGMENTATIONCALOTOOL_H
+#ifndef RECCALORIMETER_NESTEDVOLUMESCALOTOOL_H
+#define RECCALORIMETER_NESTEDVOLUMESCALOTOOL_H
 
 // from Gaudi
 #include "GaudiAlg/GaudiTool.h"
@@ -9,11 +9,11 @@
 
 class IGeoSvc;
 
-class NoSegmentationCaloTool : public GaudiTool, virtual public ICalorimeterTool
+class NestedVolumesCaloTool : public GaudiTool, virtual public ICalorimeterTool
 {
 public:
-  NoSegmentationCaloTool(const std::string& type,const std::string& name, const IInterface* parent);
-  virtual ~NoSegmentationCaloTool();
+  NestedVolumesCaloTool(const std::string& type,const std::string& name, const IInterface* parent);
+  virtual ~NestedVolumesCaloTool();
   virtual StatusCode initialize() final;
   virtual StatusCode finalize() final;
   virtual StatusCode prepareEmptyCells(std::unordered_map<uint64_t, double>& aCells) final;
@@ -24,15 +24,13 @@ private:
   /// Name of the detector readout
   std::string m_readoutName;
   /// Name of active volumes (if different than all)
-  std::string m_activeVolumeName;
+  std::vector<std::string> m_activeVolumeName;
   /// Name of active layers for sampling calorimeter
-  std::string m_activeFieldName;
+  std::vector<std::string> m_activeFieldName;
   /// Name of the fields describing the segmented volume
   std::vector<std::string> m_fieldNames;
   /// Values of the fields describing the segmented volume
   std::vector<int> m_fieldValues;
-  /// Temporary: for use with MergeLayer tool
-  unsigned int m_activeVolumesNumber;
 };
 
-#endif /* RECCALORIMETER_NOSEGMENTATIONCALOTOOL_H */
+#endif /* RECCALORIMETER_NESTEDVOLUMESCALOTOOL_H */
