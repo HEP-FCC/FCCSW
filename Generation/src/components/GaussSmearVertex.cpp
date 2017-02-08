a347fe9f79edfd422eff64e84316229745a395df
Valentin Volkl <valentin.volkl@cern.ch>
2017-02-08 11:09:12 +0100

    Add normal distribution for vertex smearing.

---
+++ Generation/src/components/GaussSmearVertex.cpp
@@ -0,0 +1,90 @@
+#include "GaussSmearVertex.h"
+
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "GaudiKernel/IRndmGenSvc.h"
+#include "GaudiKernel/PhysicalConstants.h"
+#include "GaudiKernel/Vector4DTypes.h"
+
+/// Declaration of the Tool Factory
+DECLARE_TOOL_FACTORY( GaussSmearVertex )
+
+/// Standard constructor, initializes variables
+GaussSmearVertex::GaussSmearVertex(
+  const std::string& type,
+  const std::string& name,
+  const IInterface* parent )
+  : GaudiTool ( type, name , parent ) {
+  declareInterface< IVertexSmearingTool >( this ) ;
+
+  declareProperty( "xVertexSigma" , m_xsig = 0.0 * Gaudi::Units::mm ) ;
+  declareProperty( "yVertexSigma" , m_ysig = 0.0 * Gaudi::Units::mm ) ;
+  declareProperty( "zVertexSigma" , m_zsig = 0.0 * Gaudi::Units::mm ) ;
+  declareProperty( "BeamDirection", m_zDir = 0 );
+}
+
+/// Destructor
+GaussSmearVertex::~GaussSmearVertex( ) { ; }
+
+//=============================================================================
+// Initialize
+//=============================================================================
+StatusCode GaussSmearVertex::initialize( ) {
+  StatusCode sc = GaudiTool::initialize( ) ;
+  if ( sc.isFailure() ) return sc ;
+
+  IRndmGenSvc * randSvc = svc< IRndmGenSvc >( "RndmGenSvc" , true ) ;
+
+  sc = m_gaussDistX.initialize( randSvc , Rndm::Gauss( 0. , m_xsig ) ) ;
+  if ( sc.isFailure() ) return sc ;
+  sc = m_gaussDistY.initialize( randSvc , Rndm::Gauss( 0. , m_ysig ) ) ;
+  if ( sc.isFailure() ) return sc ;
+  sc = m_gaussDistZ.initialize( randSvc , Rndm::Gauss( 0. , m_zsig ) ) ;
+
+  std::string infoMsg = " applying TOF of interaction with ";
+  if ( m_zDir == -1 ) {
+    infoMsg = infoMsg + "negative beam direction";
+  } else if ( m_zDir == 1 ) {
+    infoMsg = infoMsg + "positive beam direction";
+  } else if ( m_zDir == 0 ) {
+    infoMsg = " with TOF of interaction equal to zero ";
+  } else {
+    return Error("BeamDirection can only be set to -1 or 1, or 0 to switch off TOF");
+  }
+
+  info() << "Smearing of interaction point with normal distribution "
+         << " in x, y and z " << endmsg;
+  info() << infoMsg << endmsg;
+  info() << " with " << m_xsig / Gaudi::Units::mm
+         << " mm  standard deviation in x "
+         << m_ysig / Gaudi::Units::mm << " mm in y and "
+         << m_zmin / Gaudi::Units::mm << " mm in z." << endmsg;
+
+  if ( ! sc.isSuccess() )
+    return Error( "Could not initialize normal random number generator" ) ;
+
+  release( randSvc ) ;
+  return sc ;
+}
+
+/// Smearing function
+StatusCode GaussSmearVertex::smearVertex( HepMC::GenEvent& theEvent ) {
+  double dx , dy , dz , dt ;
+
+  dx = m_xmin + m_gaussDistX( );
+  dy = m_ymin + m_gaussDistY( );
+  dz = m_zmin + m_gaussDistZ( );
+  dt = m_zDir * dz/Gaudi::Units::c_light ;
+
+  Gaudi::LorentzVector dpos( dx , dy , dz , dt ) ;
+
+  debug() << "Smearing vertices by " << dpos << endmsg;
+
+  for (auto vit = theEvent.vertices_begin(); vit != theEvent.vertices_end(); ++vit ) {
+    Gaudi::LorentzVector pos ( (*vit) -> position() ) ;
+    pos += dpos ;
+    (*vit)->set_position( HepMC::FourVector(pos.x(), pos.y(), pos.z(), pos.t() ) ) ;
+  }
+
+  return StatusCode::SUCCESS ;
+}
+
---
+++ Generation/src/components/GaussSmearVertex.h
@@ -0,0 +1,54 @@
+#ifndef GENERATION_GAUSSSMEARVERTEX_H
+#define GENERATION_GAUSSSMEARVERTEX_H
+
+#include "GaudiAlg/GaudiTool.h"
+#include "GaudiKernel/RndmGenerators.h"
+
+#include "Generation/IVertexSmearingTool.h"
+
+/** @class FlatSmearVertex FlatSmearVertex.h "FlatSmearVertex.h"
+ *
+ *  Tool to smear vertex with flat smearing along the x- y- and z-axis.
+ *  Concrete implementation of a IVertexSmearingTool.
+ *
+ *  @author Patrick Robbe
+ *  @author Daniel Funke
+ *  @date   2008-05-18
+ */
+class FlatSmearVertex : public GaudiTool, virtual public IVertexSmearingTool {
+public:
+  /// Standard constructor
+  FlatSmearVertex( const std::string& type , const std::string& name,
+                    const IInterface* parent ) ;
+
+  virtual ~FlatSmearVertex( ); ///< Destructor
+
+  /// Initialize method
+  virtual StatusCode initialize( ) ;
+
+  /** Implements IVertexSmearingTool::smearVertex.
+   */
+  virtual StatusCode smearVertex( HepMC::GenEvent& theEvent ) ;
+
+private:
+  /// Minimum value for the x coordinate of the vertex (set by options)
+  double m_xsig   ;
+
+  /// Minimum value for the y coordinate of the vertex (set by options)
+  double m_ysig   ;
+
+  /// Minimum value for the z coordinate of the vertex (set by options)
+  double m_zsig   ;
+
+
+  /// Direction of the beam to take into account TOF vs nominal IP8, can have
+  /// only values -1 or 1, or 0 to switch off the TOF and set time of
+  /// interaction to zero (default = 1, as for beam 1)
+  int m_zDir;
+
+  Rndm::Numbers m_gaussDistX ;
+  Rndm::Numbers m_gaussDistY ; 
+  Rndm::Numbers m_gaussDistZ ;
+};
+
+#endif // GENERATION_GAUSSSMEARVERTEX_H
