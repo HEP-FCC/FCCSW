f16e3ce3efadeba7c33363d5e2f97b2b95530b4c
CoralieNeubueser <coralie.neubuser@cern.ch>
2017-03-10 13:59:52 +0100

    cleaned

--- Detector/DetSensitive/DetSensitive/SimpleCalorimeterSD.h
+++ Detector/DetSensitive/DetSensitive/SimpleCalorimeterSD.h
@@ -21,17 +21,16 @@
  */
 
 namespace det {
-class SimpleCalorimeterSD : public G4VSensitiveDetector
-{
-  public:
+class SimpleCalorimeterSD : public G4VSensitiveDetector {
+public:
   /** Constructor.
    *  @param aDetectorName Name of the detector
    *  @param aReadoutName Name of the readout (used to name the collection)
    *  @param aSeg Segmentation of the detector (used to retrieve the cell ID)
    */
   SimpleCalorimeterSD(const std::string& aDetectorName,
-		      const std::string& aReadoutName,
-		      const DD4hep::Geometry::Segmentation& aSeg);
+                      const std::string& aReadoutName,
+                      const DD4hep::Geometry::Segmentation& aSeg);
   /// Destructor
   virtual ~SimpleCalorimeterSD();
   /** Initialization.
