e8178722acdfab4d7dd4c353bcb62ac6a882c7a5
Joschka Lingemann <joschka.lingemann@cern.ch>
2017-03-28 10:41:50 +0200

    Remove unneeded using statements

--- Detector/DetSegmentation/src/plugins/SegmentationFactories.cpp
+++ Detector/DetSegmentation/src/plugins/SegmentationFactories.cpp
@@ -1,9 +1,6 @@
 #include "DD4hep/objects/SegmentationsInterna.h"
 #include "DD4hep/Factories.h"
 
-using namespace DD4hep::Geometry;
-using namespace DD4hep::DDSegmentation;
-
 namespace {
   template<typename T> DD4hep::Geometry::SegmentationObject*
   create_segmentation(DD4hep::Geometry::BitField64* decoder)  {
