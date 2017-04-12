decc067c324090368280206a017c7e4f2bf69433
Anna Zaborowska <anna.zaborowska@cern.ch>
2017-04-12 18:18:38 +0200

    Using know positions to check ECal centre of volumes

--- Detector/DetComponents/tests/scripts/check_inclinedEcal_positions.py
+++ Detector/DetComponents/tests/scripts/check_inclinedEcal_positions.py
@@ -1,5 +1,10 @@
 from ROOT import gSystem
 from EventStore import EventStore
+from numpy import testing
+
+centre_x = [1968.99, -675.552, 2142.64, -2023.78, -2229.37]
+centre_y = [-0.8434, 2081.93, 685.885, 658.538, 6.85499]
+centre_z = [0, 0, 0, 0, 0]
 
 if __name__ == "__main__":
     gSystem.Load("libdatamodelDict")
@@ -11,4 +16,6 @@ if __name__ == "__main__":
             x = pos.position().x
             y = pos.position().y
             z = pos.position().z
-            assert( not (x == 0 and y == 0 and z == 0))
+            testing.assert_allclose(x , centre_x[iev], 1e-3, 1e-3)
+            testing.assert_allclose(y , centre_y[iev], 1e-3, 1e-3)
+            testing.assert_allclose(z , centre_z[iev], 1e-3, 1e-3)
