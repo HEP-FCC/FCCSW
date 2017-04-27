4e6065e47848a3734fd38802a0c870b83b4deddd
CoralieNeubueser <coralie.neubuser@cern.ch>
2017-04-27 15:58:13 +0200

    fixed reco file

--- Reconstruction/RecCalorimeter/tests/options/geant_fullsim_combinedCalo_singleparticles.py
+++ Reconstruction/RecCalorimeter/tests/options/geant_fullsim_combinedCalo_singleparticles.py
@@ -27,7 +27,7 @@ podioevent   = FCCDataSvc("EventDataSvc")
 from Configurables import GeoSvc
 geoservice = GeoSvc("GeoSvc", detectors=[  'file:Detector/DetFCChhBaseline1/compact/FCChh_DectEmptyMaster.xml',
                                            'file:Detector/DetFCChhECalInclined/compact/FCChh_ECalBarrel_withCryostat.xml',
-                                          'file:Detector/DetFCChhHCalRealTile/compact/FCChh_HCalBarrel_RealTileCal.xml'],
+                                           'file:Detector/DetFCChhHCalTile/compact/FCChh_HCalBarrel_TileCal.xml'],
                     OutputLevel = INFO)
 # Geant4 service                                                                                                                                                                         
 # Configures the Geant simulation: geometry, physics list and user actions
