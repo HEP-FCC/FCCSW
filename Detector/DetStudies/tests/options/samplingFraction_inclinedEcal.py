74f91334817c8d46c8cc60f36442ee30b621e87f
Valentin Volkl <valentin.volkl@cern.ch>
2017-05-12 16:27:01 +0200

    Propagate changed "G4command" of geantservice to options files

--- Detector/DetStudies/tests/options/samplingFraction_inclinedEcal.py
+++ Detector/DetStudies/tests/options/samplingFraction_inclinedEcal.py
@@ -15,7 +15,7 @@ geoservice = GeoSvc("GeoSvc", detectors=[ 'file:Detector/DetFCChhBaseline1/compa
 # Configures the Geant simulation: geometry, physics list and user actions
 from Configurables import SimG4Svc
 geantservice = SimG4Svc("SimG4Svc", detector='SimG4DD4hepDetector', physicslist="SimG4FtfpBert", actions="SimG4FullSimActions")
-geantservice.G4commands += ["/run/setCut 0.1 mm"]
+geantservice.g4PostInitCommands += ["/run/setCut 0.1 mm"]
 
 # Geant4 algorithm
 # Translates EDM to G4Event, passes the event to G4, writes out outputs via tools
