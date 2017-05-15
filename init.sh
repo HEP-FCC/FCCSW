81718337a25f4df30302bd40c83bcf2c4a76855c
Valentin Volkl <valentin.volkl@cern.ch>
2017-05-15 13:29:54 +0200

    Temporarily use old acts installation

--- init.sh
+++ init.sh
@@ -3,3 +3,6 @@
 export FCCSWBASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source /cvmfs/fcc.cern.ch/sw/0.8.1/init_fcc_stack.sh $1
 
+# TODO: temporary, until new ACTS tag available & tests work with gcc6.2
+export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/cvmfs/fcc.cern.ch/sw/0.8pre/acts/0.3.0/x86_64-slc6-gcc49-opt/share/cmake/ACTS/
+
