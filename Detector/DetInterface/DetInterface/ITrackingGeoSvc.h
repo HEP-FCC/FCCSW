b58f38b2ec0918982baae0a42eb252c587ddeeb4
Valentin Volkl <valentin.volkl@cern.ch>
2017-06-08 15:38:53 +0200

    Add comment

--- Detector/DetInterface/DetInterface/ITrackingGeoSvc.h
+++ Detector/DetInterface/DetInterface/ITrackingGeoSvc.h
@@ -7,7 +7,12 @@ namespace Acts {
 class TrackingGeometry;
 }
 
-/// @author: Julia Hrdinka
+/** @author: Julia Hrdinka
+ *  Service for the tracking geometry ( concrete implementations
+ *  may be using the DD4hepPlugin or constructing it explicitly )
+ *  Returns the top level geometry object.
+ *
+ * /
 class ITrackingGeoSvc : virtual public IService {
 public:
   // InterfaceID
