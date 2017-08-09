da28ae251c180ecdd4b4e442dbdb14c1f85de7dc
CoralieNeubueser <coralie.neubuser@cern.ch>
2017-08-09 17:14:21 +0200

    ...

--- Reconstruction/RecCalorimeter/src/components/SingleCaloTowerTool.cpp
+++ Reconstruction/RecCalorimeter/src/components/SingleCaloTowerTool.cpp
@@ -4,8 +4,8 @@
 #include "DetInterface/IGeoSvc.h"
 
 // datamodel
-#include "datamodel/CaloHitCollection.h"
 #include "datamodel/CaloHit.h"
+#include "datamodel/CaloHitCollection.h"
 
 // DD4hep
 #include "DD4hep/LCDD.h"
@@ -15,17 +15,8 @@ DECLARE_TOOL_FACTORY(SingleCaloTowerTool)
 
 SingleCaloTowerTool::SingleCaloTowerTool(const std::string& type, const std::string& name, const IInterface* parent)
     : GaudiTool(type, name, parent) {
+  declareProperty("cells", m_cells, "Cells to create towers from (input)");
   declareInterface<ITowerTool>(this);
-  declareInput("cells", m_cells, "calo/cells");
-  declareProperty("readoutName", m_readoutName);
-  // the default value to calculate the position of clusters
-  declareProperty("radiusForPosition", m_radius = 1.);
-  declareProperty("etaMax", m_etaMax = 0.);
-  declareProperty("deltaEtaTower", m_deltaEtaTower = 0.01);
-  declareProperty("deltaPhiTower", m_deltaPhiTower = 0.01);
-  // needed for AlgTool wit output/input until it appears in Gaudi AlgTool constructor
-  declareProperty("DataInputs", inputDataObjects());
-  declareProperty("DataOutputs", outputDataObjects());
 }
 
 StatusCode SingleCaloTowerTool::initialize() {
@@ -35,7 +26,9 @@ StatusCode SingleCaloTowerTool::initialize() {
   m_geoSvc = service("GeoSvc");
   if (!m_geoSvc) {
     error() << "Unable to locate Geometry Service. "
-            << "Make sure you have GeoSvc and SimSvc in the right order in the configuration." << endmsg;
+            << "Make sure you have GeoSvc and SimSvc in the right order in the "
+               "configuration."
+            << endmsg;
     return StatusCode::FAILURE;
   }
   // check if readouts exist
@@ -50,98 +43,141 @@ StatusCode SingleCaloTowerTool::initialize() {
     error() << "There is no phi-eta segmentation." << endmsg;
     return StatusCode::FAILURE;
   }
-  if( ! m_etaMax) {
-    warning() << "Undefined detector size in eta. In each event the cell collection will be searched for maximum eta." << endmsg;
-  }
   return StatusCode::SUCCESS;
 }
 
 StatusCode SingleCaloTowerTool::finalize() { return GaudiTool::finalize(); }
 
 tower SingleCaloTowerTool::towersNumber() {
+  // maximum eta of the detector (== eta offset + half of the cell size)
+  m_etaMax = fabs(m_segmentation->offsetEta()) + m_segmentation->gridSizeEta() * 0.5;
+  m_phiMax = fabs(m_segmentation->offsetPhi()) + M_PI / (double)m_segmentation->phiBins();
+
   // number of phi bins
-  m_nPhiTower = idPhi(2 * M_PI);
+  float epsilon = 0.0001;
+  m_nPhiTower = ceil(2 * (m_phiMax - epsilon) / m_deltaPhiTower);
   // number of eta bins (if eta maximum is defined)
-  if( m_etaMax) {
-    m_nEtaTower = 2 * idEta(m_etaMax - m_deltaEtaTower / 2.) + 1;
-  } else {
-    m_nEtaTower = 0;
-  }
+  m_nEtaTower = ceil(2 * (m_etaMax - epsilon) / m_deltaEtaTower);
+  debug() << "etaMax " << m_etaMax << ", deltaEtaTower " << m_deltaEtaTower << ", nEtaTower " << m_nEtaTower << endmsg;
+  debug() << "phiMax " << m_phiMax << ", deltaPhiTower " << m_deltaPhiTower << ", nPhiTower " << m_nPhiTower << endmsg;
+
   tower total;
   total.eta = m_nEtaTower;
   total.phi = m_nPhiTower;
   return total;
 }
 
-int SingleCaloTowerTool::etaTowersNumber() {
-  // recalculate number of eta bins for each event
-  const fcc::CaloHitCollection* cells = m_cells.get();
-  float etaCell = 0;
-  for (const auto& cell : *cells) {
-    etaCell = fabs(m_segmentation->eta(cell.core().cellId));
-    if( etaCell > m_etaMax ) {
-      m_etaMax = etaCell;
-    }
-  }
-  // eta from cell collection is middle of cell
-  m_nEtaTower = 2 * m_etaMax / m_deltaEtaTower+ 1;
-  return m_nEtaTower;
-}
-
-
 uint SingleCaloTowerTool::buildTowers(std::vector<std::vector<float>>& aTowers) {
-  // Get the input collection with cells from simulation + digitisation (after calibration and with noise)
+  // Get the input collection with cells from simulation + digitisation (after
+  // calibration and with noise)
   const fcc::CaloHitCollection* cells = m_cells.get();
-  debug() << "Input Hit collection size: " << cells->size() << endmsg;
+  debug() << "Input cell collection size: " << cells->size() << endmsg;
   // Loop over a collection of calorimeter cells and build calo towers
-  int iPhi = 0, iEta = 0;
-  float etaCell = 0;
+  // borders of the cell in eta/phi
+  float etaCellMin = 0, etaCellMax = 0;
+  float phiCellMin = 0, phiCellMax = 0;
+  // tower index of the borders of the cell
+  int iPhiMin = 0, iPhiMax = 0;
+  int iEtaMin = 0, iEtaMax = 0;
+  // fraction of cell area in eta/phi belonging to towers
+  // Min - first tower, Max - last tower, Middle - middle tower(s)
+  // If cell size <= tower size => first == last == middle tower, all fractions = 1
+  // cell size > tower size => Sum of fractions = 1
+  float ratioEta = 1.0, ratioPhi = 1.0;
+  float fracEtaMin = 1.0, fracEtaMax = 1.0, fracEtaMiddle = 1.0;
+  float fracPhiMin = 1.0, fracPhiMax = 1.0, fracPhiMiddle = 1.0;
+  float epsilon = 0.0001;
   for (const auto& cell : *cells) {
-    // find to which tower the cell belongs
-    etaCell = m_segmentation->eta(cell.core().cellId);
-    iEta = idEta(etaCell);
-    iPhi = idPhi(m_segmentation->phi(cell.core().cellId));
-    // save transverse energy
-    aTowers[iEta][iPhi] += cell.core().energy / cosh(etaCell);
+    // find to which tower(s) the cell belongs
+    etaCellMin = m_segmentation->eta(cell.core().cellId) - m_segmentation->gridSizeEta() * 0.5;
+    etaCellMax = m_segmentation->eta(cell.core().cellId) + m_segmentation->gridSizeEta() * 0.5;
+    phiCellMin = m_segmentation->phi(cell.core().cellId) - M_PI / (double)m_segmentation->phiBins();
+    phiCellMax = m_segmentation->phi(cell.core().cellId) + M_PI / (double)m_segmentation->phiBins();
+    iEtaMin = idEta(etaCellMin + epsilon);
+    iPhiMin = idPhi(phiCellMin + epsilon);
+    iEtaMax = idEta(etaCellMax - epsilon);
+    iPhiMax = idPhi(phiCellMax - epsilon);
+    // if a cell is larger than a tower in eta/phi, calculate the fraction of
+    // the cell area belonging to the first/last/middle towers
+    fracEtaMin = 1.0;
+    fracEtaMax = 1.0;
+    fracEtaMiddle = 1.0;
+    if (iEtaMin != iEtaMax) {
+      fracEtaMin = fabs(eta(iEtaMin) + 0.5 * m_deltaEtaTower - etaCellMin) / m_segmentation->gridSizeEta();
+      fracEtaMax = fabs(etaCellMax - eta(iEtaMax) + 0.5 * m_deltaEtaTower) / m_segmentation->gridSizeEta();
+      if ((iEtaMax - iEtaMin - 1) != 0) {
+        fracEtaMiddle = (1 - fracEtaMin - fracEtaMax) / float(iEtaMax - iEtaMin - 1);
+      } else {
+        fracEtaMiddle = 0.0;
+      }
+    }
+    fracPhiMin = 1.0;
+    fracPhiMax = 1.0;
+    fracPhiMiddle = 1.0;
+    if (iPhiMin != iPhiMax) {
+      fracPhiMin =
+          fabs(phi(iPhiMin) + 0.5 * m_deltaPhiTower - phiCellMin) / (2 * M_PI / (double)m_segmentation->phiBins());
+      fracPhiMax =
+          fabs(phiCellMax - phi(iPhiMax) + 0.5 * m_deltaPhiTower) / (2 * M_PI / (double)m_segmentation->phiBins());
+      if ((iPhiMax - iPhiMin - 1) != 0) {
+        fracPhiMiddle = (1 - fracPhiMin - fracPhiMax) / float(iPhiMax - iPhiMin - 1);
+      } else {
+        fracPhiMiddle = 0.0;
+      }
+    }
+
+    // Loop through the appropriate towers and add transverse energy
+    for (auto iEta = iEtaMin; iEta <= iEtaMax; iEta++) {
+      if (iEta == iEtaMin) {
+        ratioEta = fracEtaMin;
+      } else if (iEta == iEtaMax) {
+        ratioEta = fracEtaMax;
+      } else {
+        ratioEta = fracEtaMiddle;
+      }
+      for (auto iPhi = iPhiMin; iPhi <= iPhiMax; iPhi++) {
+        if (iPhi == iPhiMin) {
+          ratioPhi = fracPhiMin;
+        } else if (iPhi == iPhiMax) {
+          ratioPhi = fracPhiMax;
+        } else {
+          ratioPhi = fracPhiMiddle;
+        }
+        aTowers[iEta][phiNeighbour(iPhi)] +=
+            cell.core().energy / cosh(m_segmentation->eta(cell.core().cellId)) * ratioEta * ratioPhi;
+      }
+    }
   }
   return cells->size();
 }
 
 uint SingleCaloTowerTool::idEta(float aEta) const {
-  // shift Ids so they start at 0 (segmentation returns IDs that may be from -N to N)
-  // for segmentation in eta the middle cell has its centre at eta=0 (segmentation offset = 0)
-  return floor((aEta + m_deltaEtaTower / 2.) / m_deltaEtaTower) + floor(m_nEtaTower / 2);
+  uint id = floor((aEta + m_etaMax) / m_deltaEtaTower);
+  return id;
 }
 
 uint SingleCaloTowerTool::idPhi(float aPhi) const {
-  // shift Ids so they start at 0 (segmentation returns IDs that may be from -N to N)
-  // for segmentation in phi the middle cell has its centre at phi=0 (segmentation offset = 0)
-  return floor((aPhi + m_deltaPhiTower / 2.) / m_deltaPhiTower) + floor(m_nPhiTower / 2);
+  uint id = floor((aPhi + m_phiMax) / m_deltaPhiTower);
+  return id;
 }
 
 float SingleCaloTowerTool::eta(int aIdEta) const {
   // middle of the tower
-  return (aIdEta - (m_nEtaTower - 1) / 2) * m_deltaEtaTower;
+  return ((aIdEta + 0.5) * m_deltaEtaTower - m_etaMax);
 }
 
 float SingleCaloTowerTool::phi(int aIdPhi) const {
   // middle of the tower
-  return (aIdPhi - (m_nPhiTower - 1) / 2) * m_deltaPhiTower;
+  return ((aIdPhi + 0.5) * m_deltaPhiTower - m_phiMax);
 }
 
-void SingleCaloTowerTool::matchCells(float eta, float phi, uint halfEtaFin, uint halfPhiFin, fcc::CaloCluster& aEdmCluster) {
-  const fcc::CaloHitCollection* cells = m_cells.get();
-  for (const auto& cell : *cells) {
-    float etaCell = m_segmentation->eta(cell.core().cellId);
-    float phiCell = m_segmentation->phi(cell.core().cellId);
-    if ((abs(idEta(etaCell) - idEta(eta)) <= halfEtaFin) &&
-        (abs(idPhi(phiCell) - idPhi(phi)) <= halfPhiFin)) {
-       aEdmCluster.addhits(cell);
-    }
+uint SingleCaloTowerTool::phiNeighbour(int aIPhi) const {
+  if (aIPhi < 0) {
+    return m_nPhiTower + aIPhi;
+  } else if (aIPhi >= m_nPhiTower) {
+    return aIPhi % m_nPhiTower;
   }
-  return;
+  return aIPhi;
 }
 
-float SingleCaloTowerTool::radiusForPosition() const {
-  return m_radius;
-}
+float SingleCaloTowerTool::radiusForPosition() const { return m_radius; }
--- Reconstruction/RecCalorimeter/src/components/SingleCaloTowerTool.h
+++ Reconstruction/RecCalorimeter/src/components/SingleCaloTowerTool.h
@@ -5,9 +5,9 @@
 #include "GaudiAlg/GaudiTool.h"
 
 // FCCSW
+#include "DetSegmentation/GridPhiEta.h"
 #include "FWCore/DataHandle.h"
 #include "RecInterface/ITowerTool.h"
-#include "DetSegmentation/GridPhiEta.h"
 class IGeoSvc;
 
 // datamodel
@@ -21,17 +21,23 @@ class Segmentation;
 }
 }
 
-/** @class SingleCaloTowerTool Reconstruction/RecCalorimeter/src/components/SingleCaloTowerTool.h SingleCaloTowerTool.h
+/** @class SingleCaloTowerTool
+ * Reconstruction/RecCalorimeter/src/components/SingleCaloTowerTool.h
+ * SingleCaloTowerTool.h
  *
  *  Tool building the calorimeter towers for the sliding window algorithm.
  *  Towers are built of cells in eta-phi, summed over all radial layers.
- *  A tower contains all cells within certain eta and phi (tower size: '\b deltaEtaTower', '\b deltaPhiTower').
- *  Distance in r plays no role, however `\b radiusForPosition` needs to be defined
- *  (e.g. to inner radius of the detector) for the cluster position calculation. By default the radius is equal to 1.
+ *  A tower contains all cells within certain eta and phi (tower size: '\b
+ * deltaEtaTower', '\b deltaPhiTower').
+ *  Distance in r plays no role, however `\b radiusForPosition` needs to be
+ *  defined (e.g. to inner radius of the detector) for the cluster position 
+ *  calculation. By default the radius is equal to 1.
  *
- *  This tool creates towers from a single cell collection (from one calorimeter).
+ *  This tool creates towers from a single cell collection (from one
+ * calorimeter).
  *
- *  For more explanation please [see reconstruction documentation](@ref md_reconstruction_doc_reccalorimeter).
+ *  For more explanation please [see reconstruction documentation](@ref
+ * md_reconstruction_doc_reccalorimeter).
  *
  *  @author Anna Zaborowska
  *  @author Jana Faltova
@@ -50,35 +56,25 @@ public:
    */
   virtual StatusCode finalize() final;
   /**  Find number of calorimeter towers.
-   *   Number of towers in phi is calculated from full azimuthal angle (2 pi) and the size of tower in phi ('\b deltaPhiTower').
-   *   Number of towers in eta is calculated from maximum detector eta ('\b etaMax`) and the size of tower in eta ('\b deltaEtaTower').
-   *   If max eta is undefined, 0 is returned. In this case number of eta towers should be calculated for each event separately (etaTowersNumber()).
+   *   Number of towers in phi is calculated from full azimuthal angle (2 pi)
+   * and the size of tower in phi ('\b deltaPhiTower').
+   *   Number of towers in eta is calculated from maximum detector eta ('\b
+   * etaMax`) and the size of tower in eta ('\b deltaEtaTower').
    *   @return Struct containing number of towers in eta and phi.
    */
   virtual tower towersNumber() final;
-  /**  Find number of calorimeter towers in eta for current event.
-   *   Cell collection is searched for a highest eta (absolute value).
-   *   @return Number of towers in eta for current event.
-   */
-  virtual int etaTowersNumber() final;
   /**  Build calorimeter towers.
-   *   Tower is segmented in eta and phi, with the energy from all layers (no r segmentation).
-   *   Currently the size of tower needs to be a multiple of a cell size (so each cell belongs to only one tower).
+   *   Tower is segmented in eta and phi, with the energy from all layers 
+   *   (no segmentation).
    *   @param[out] aTowers Calorimeter towers.
    *   @return Size of the cell collection.
    */
-  virtual uint buildTowers(std::vector<std::vector<float>> & aTowers) final;
-  /**  Find cells belonging to a cluster.
-   *   @param[in] aEta Position of the middle tower of a cluster in eta
-   *   @param[in] aPhi Position of the middle tower of a cluster in phi
-   *   @param[in] aHalfEtaFinal Half size of cluster in eta (in units of tower size). Cluster size is 2*aHalfEtaFinal+1
-   *   @param[in] aHalfPhiFinal Half size of cluster in phi (in units of tower size). Cluster size is 2*aHalfPhiFinal+1
-   *   @param[out] aEdmCluster Cluster where cells are attached to
-   */
-  virtual void matchCells(float aEta, float aPhi, uint aHalfEtaFinal, uint aHalfPhiFinal, fcc::CaloCluster& aEdmCluster) final;
+  virtual uint buildTowers(std::vector<std::vector<float>>& aTowers) final;
   /**  Get the radius (in mm) for the position calculation.
-   *   Reconstructed cluster has eta and phi position, without the radial coordinate. The cluster in EDM contains Cartesian position, so the radial position (e.g. the inner radius of the calorimeter)
-   *   needs to be specified. By default it is equal to 1.
+   *   Reconstructed cluster has eta and phi position, without the radial
+   * coordinate. The cluster in EDM contains
+   * Cartesian position, so the radial position (e.g. the inner radius of the
+   * calorimeter) needs to be specified. By default it is equal to 1.
    *   @return Radius
    */
   virtual float radiusForPosition() const final;
@@ -88,13 +84,16 @@ public:
    */
   virtual uint idEta(float aEta) const final;
   /**  Get the tower IDs in phi.
-   *   Tower IDs are shifted so they start at 0 (middle of cell with ID=0 is phi=0, phi is defined form -pi to pi). No segmentation offset is taken into account.
+   *   Tower IDs are shifted so they start at 0 (middle of cell with ID=0 is
+   * phi=0, phi is defined form -pi to pi). No
+   * segmentation offset is taken into account.
    *   @param[in] aPhi Position of the calorimeter cell in phi
    *   @return ID (phi) of a tower
    */
   virtual uint idPhi(float aPhi) const final;
   /**  Get the eta position of the centre of the tower.
-   *   Tower IDs are shifted so they start at 0 (middle of cell with ID=0 is eta=0). No segmentation offset is taken into account.
+   *   Tower IDs are shifted so they start at 0 (middle of cell with ID=0 is
+   * eta=0). No segmentation offset is taken into account.
    *   @param[in] aIdEta ID (eta) of a tower
    *   @return Position of the centre of the tower
    */
@@ -104,27 +103,42 @@ public:
    *   @return Position of the centre of the tower
    */
   virtual float phi(int aIdPhi) const final;
+  /**  Correct way to access the neighbour of the phi tower, taking into account
+   * the full coverage in phi.
+   *   Full coverage means that first tower in phi, with ID = 0 is a direct
+   * neighbour of the last tower in phi with ID = m_nPhiTower - 1).
+   *   @param[in] aIPhi requested ID of a phi tower, 
+   *   may be < 0 or >=m_nPhiTower
+   *   @return  ID of a tower - shifted and corrected 
+   * (in [0, m_nPhiTower) range)
+   */
+  uint phiNeighbour(int aIPhi) const;
+
 private:
   /// Handle for calo cells (input collection)
-  DataHandle<fcc::CaloHitCollection> m_cells;
+  DataHandle<fcc::CaloHitCollection> m_cells{"calo/cells", Gaudi::DataHandle::Reader, this};
   /// Pointer to the geometry service
   SmartIF<IGeoSvc> m_geoSvc;
   /// Name of the detector readout
-  std::string m_readoutName;
+  Gaudi::Property<std::string> m_readoutName{this, "readoutName", "", "Name of the detector readout"};
   /// PhiEta segmentation (owned by DD4hep)
   DD4hep::DDSegmentation::GridPhiEta* m_segmentation;
   /// Radius used to calculate cluster position from eta and phi (in mm)
-  double m_radius;
-  /// Maximum eta of detector. If undefined, it is calculated for each event from the cell collection.
+  Gaudi::Property<double> m_radius{this, "radiusForPosition", 1.0,
+                                   "Radius used to calculate cluster position from eta and phi (in mm)"};
+  /// Maximum eta of the detector
   float m_etaMax;
+  /// Maximum phi of the detector
+  float m_phiMax;
+
   /// Size of the tower in eta
-  float m_deltaEtaTower;
+  Gaudi::Property<float> m_deltaEtaTower{this, "deltaEtaTower", 0.01, "Size of the tower in eta"};
   /// Size of the tower in phi
-  float m_deltaPhiTower;
+  Gaudi::Property<float> m_deltaPhiTower{this, "deltaPhiTower", 0.01, "Size of the tower in phi"};
   /// number of towers in eta (calculated from m_deltaEtaTower and m_etaMax)
-  int m_nEtaTower;
+  uint m_nEtaTower;
   /// Number of towers in phi (calculated from m_deltaPhiTower)
-  int m_nPhiTower;
+  uint m_nPhiTower;
 };
 
 #endif /* RECCALORIMETER_SINGLECALOTOWERTOOL_H */
