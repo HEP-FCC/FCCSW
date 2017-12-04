fa916dc599a08aca3b905b2cd3cdfb3539643b54
Anna Zaborowska <anna.zaborowska@cern.ch>
2017-12-04 18:43:22 +0100

    checkformat

--- Detector/DetFCChhCalDiscs/src/CaloEndcapDiscs_geo.cpp
+++ Detector/DetFCChhCalDiscs/src/CaloEndcapDiscs_geo.cpp
@@ -7,7 +7,7 @@
 
 namespace det {
 void buildOneSide(MsgStream& lLog, DD4hep::Geometry::LCDD& aLcdd, DD4hep::Geometry::SensitiveDetector& aSensDet,
-                    DD4hep::Geometry::Volume& aEnvelope, DD4hep::XML::Handle_t& aXmlElement, int sign) {
+                  DD4hep::Geometry::Volume& aEnvelope, DD4hep::XML::Handle_t& aXmlElement, int sign) {
 
   DD4hep::XML::Dimension dim(aXmlElement.child(_Unicode(dimensions)));
 
@@ -54,7 +54,7 @@ void buildOneSide(MsgStream& lLog, DD4hep::Geometry::LCDD& aLcdd, DD4hep::Geomet
        << "\nThickness of absorber discs (cm) = " << passiveThickness
        << "\nThickness of readout disc placed in between absorber plates (cm) = " << readoutThickness
        << "\nNumber of absorber/readout discs: " << numDiscs
-    // + 1 to add the first active layer in between the first readout disc and the first absorber
+       // + 1 to add the first active layer in between the first readout disc and the first absorber
        << "\nNumber of active layers: " << numDiscs + 1
        << "\nMargin outside first readout disc and last absorber disc, filled with non-sensitive active medium (cm) = "
        << marginOutside << endmsg;
@@ -188,8 +188,8 @@ void buildOneSide(MsgStream& lLog, DD4hep::Geometry::LCDD& aLcdd, DD4hep::Geomet
 }
 
 static DD4hep::Geometry::Ref_t createCaloDiscs(DD4hep::Geometry::LCDD& aLcdd,
-                                                     DD4hep::XML::Handle_t aXmlElement,
-                                                     DD4hep::Geometry::SensitiveDetector aSensDet) {
+                                               DD4hep::XML::Handle_t aXmlElement,
+                                               DD4hep::Geometry::SensitiveDetector aSensDet) {
   ServiceHandle<IMessageSvc> msgSvc("MessageSvc", "CalDiscsConstruction");
   MsgStream lLog(&(*msgSvc), "CalDiscsConstruction");
 
