21ac805f563c50a13ed4de943d5a9a9ee7fed603
vvolkl <valentin.volkl@cern.ch>
2017-12-20 16:10:04 +0100

    Setup correct init

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@
 export FCCSWBASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 weekday=`date +%a`
 source /cvmfs/fcc.cern.ch/testing/sw/views/${weekday}/x86_64-slc6-gcc62-opt/setup.sh
-source /cvmfs/fcc.cern.ch/sw/0.8.2/init_fcc_stack.sh $1
+#source /cvmfs/fcc.cern.ch/sw/0.8.2/init_fcc_stack.sh $1
 
