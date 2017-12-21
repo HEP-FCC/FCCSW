3e07e29cc67d1474db9d3fcd8466fd5bda6ed67a
vvolkl <valentin.volkl@cern.ch>
2017-12-21 11:19:12 +0100

    Update init.sh to use the 0.8.3 setup script

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -u
 # set FCCSWBASEDIR to the directory containing this script
 export FCCSWBASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source /cvmfs/fcc.cern.ch/sw/0.8.2/init_fcc_stack.sh $1
+source /cvmfs/fcc.cern.ch/sw/0.8.3/init_fcc_stack.sh $1
 
-export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/cvmfs/fcc.cern.ch/sw/0.8.2/acts/0.4.0/x86_64-slc6-gcc62-opt/share/cmake/ACTS/
+export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/cvmfs/fcc.cern.ch/sw/0.8.3/acts/0.4.0/x86_64-slc6-gcc62-opt/share/cmake/ACTS/
 
