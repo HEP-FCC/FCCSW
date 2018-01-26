00efc91df04a3db6185e817be870d4e4b92371b0
CoralieNeubueser <coralie.neubuser@cern.ch>
2018-01-26 09:26:43 +0100

    noise tool for constants and reading root file

---
+++ Reconstruction/RecCalorimeter/src/components/ConstNoiseTool.cpp
@@ -0,0 +1,75 @@
+#include "ConstNoiseTool.h"
+
+// FCCSW
+#include "DetCommon/DetUtils.h"
+#include "DetInterface/IGeoSvc.h"
+
+// DD4hep
+#include "DD4hep/LCDD.h"
+
+// Root
+#include "TFile.h"
+#include "TH1F.h"
+#include "TMath.h"
+
+DECLARE_TOOL_FACTORY(ConstNoiseTool)
+
+ConstNoiseTool::ConstNoiseTool(const std::string& type, const std::string& name, const IInterface* parent)
+    : GaudiTool(type, name, parent) {
+  declareInterface<INoiseConstTool>(this);
+}
+
+StatusCode ConstNoiseTool::initialize() {
+  /// Noise estimates from Atlas
+  // set for ECalBarrel system:5
+  // set for HCalBarrel system:6
+  // set for HCalExtBarrel system:7
+  m_systemNoiseConstMap.emplace(5, 7.5/4.);
+  m_systemNoiseConstMap.emplace(6, 11.5/4.);
+  m_systemNoiseConstMap.emplace(7, 11.5/4.);
+
+  // Get GeoSvc
+  m_geoSvc = service("GeoSvc");
+  if (!m_geoSvc) {
+    error() << "Unable to locate Geometry Service. "
+            << "Make sure you have GeoSvc and SimSvc in the right order in the configuration." << endmsg;
+    return StatusCode::FAILURE;
+  }
+  // Get PhiEta segmentation
+  m_segmentation = dynamic_cast<DD4hep::DDSegmentation::GridPhiEta*>(
+      m_geoSvc->lcdd()->readout(m_readoutName).segmentation().segmentation());
+  if (m_segmentation == nullptr) {
+    error() << "There is no phi-eta segmentation." << endmsg;
+    return StatusCode::FAILURE;
+  }
+  // Take readout bitfield decoder from GeoSvc
+  m_decoder = std::shared_ptr<DD4hep::DDSegmentation::BitField64>(
+								  m_geoSvc->lcdd()->readout(m_readoutName).segmentation().segmentation()->decoder());
+  StatusCode sc = GaudiTool::initialize();
+  if (sc.isFailure()) return sc;
+
+  return sc;
+}
+
+StatusCode ConstNoiseTool::finalize() {
+  StatusCode sc = GaudiTool::finalize();
+  return sc;
+}
+
+double ConstNoiseTool::getNoiseConstantPerCell(int64_t aCellId) {
+
+  double Noise = 0.;
+  
+  // Get cells global coordinate "system"
+  m_decoder->setValue(aCellId);
+  unsigned cellSystem = (*m_decoder)[m_systemFieldName];
+  //for (auto& aSystemId: m_systemNoiseConstMap){
+  //if (aSystemId.first == cellSystem){
+  // cell noise in system 
+  if (m_systemNoiseConstMap[cellSystem])
+    Noise = m_systemNoiseConstMap[cellSystem];
+  //  }
+  else 
+    warning() << "No noise constants set for this subsystem! Noise of cell set to 0. " << endmsg;
+  return Noise;
+}
---
+++ Reconstruction/RecCalorimeter/src/components/ConstNoiseTool.h
@@ -0,0 +1,53 @@
+#ifndef RECCALORIMETER_CONSTNOISETOOL_H
+#define RECCALORIMETER_CONSTNOISETOOL_H
+
+// from Gaudi
+#include "GaudiAlg/GaudiTool.h"
+class IRndmGenSvc;
+
+// FCCSW
+#include "DetSegmentation/GridPhiEta.h"
+#include "RecInterface/INoiseConstTool.h"
+class IGeoSvc;
+
+#include <map>
+
+/** @class ConstNoiseTool
+ *
+ *  Tool to set noise constant for all cells in a calorimeters
+ *
+ *  @author Coralie Neubueser
+ *  @date   2018-01
+ *
+ */
+
+class ConstNoiseTool : public GaudiTool, virtual public INoiseConstTool {
+public:
+  ConstNoiseTool(const std::string& type, const std::string& name, const IInterface* parent);
+  virtual ~ConstNoiseTool() = default;
+
+  virtual StatusCode initialize() final;
+
+  virtual StatusCode finalize() final;
+
+  /// Find the appropriate noise constant from the histogram
+  double getNoiseConstantPerCell(int64_t aCellID);
+
+private:
+  /// Name of the detector readout
+  Gaudi::Property<std::string> m_readoutName{this, "readoutName", "ECalHitsPhiEta", "Name of the detector readout"};
+  /// Name of active layers for sampling calorimeter
+  Gaudi::Property<std::string> m_systemFieldName{this, "systemFieldName", "system",
+      "Name of field describing detector system"};
+  std::map<uint,double> m_systemNoiseConstMap;
+  
+  /// Pointer to the geometry service
+  SmartIF<IGeoSvc> m_geoSvc;
+  /// PhiEta segmentation
+  DD4hep::DDSegmentation::GridPhiEta* m_segmentation;
+  // Decoder
+  std::shared_ptr<DD4hep::DDSegmentation::BitField64> m_decoder;
+
+};
+
+#endif /* RECCALORIMETER_CONSTNOISETOOL_H */
