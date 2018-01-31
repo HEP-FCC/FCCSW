931a965c113cd1e23bfc271ecd029109b276d56d
CoralieNeubueser <coralie.neubuser@cern.ch>
2018-01-31 11:55:21 +0100

    re-write bitfield without calling segmentation

--- Detector/DetComponents/src/RewriteBitfield.cpp
+++ Detector/DetComponents/src/RewriteBitfield.cpp
@@ -38,7 +38,7 @@ StatusCode RewriteBitfield::initialize() {
   }
   // Take readout, bitfield from GeoSvc
   m_oldDecoder = std::shared_ptr<DD4hep::DDSegmentation::BitField64>(
-      m_geoSvc->lcdd()->readout(m_oldReadoutName).idSpec().decoder());
+								     m_geoSvc->lcdd()->readout(m_oldReadoutName).idSpec().decoder());
   // segmentation identifiers to be overwritten
   if (m_oldIdentifiers.size() == 0) {
     // it is not an error, maybe no segmentation was used previously
@@ -52,24 +52,23 @@ StatusCode RewriteBitfield::initialize() {
       m_detectorIdentifiers.push_back(field);
     }
   }
-  // Take new segmentation from geometry service
-  m_segmentation = m_geoSvc->lcdd()->readout(m_newReadoutName).segmentation().segmentation();
-  // check if detector identifiers (old and new) agree
   std::vector<std::string> newFields;
-  for (uint itField = 0; itField < m_segmentation->decoder()->size(); itField++) {
-    newFields.push_back((*m_segmentation->decoder())[itField].name());
+  m_newDecoder = std::shared_ptr<DD4hep::DDSegmentation::BitField64>(
+								     m_geoSvc->lcdd()->readout(m_newReadoutName).idSpec().decoder());
+  for (uint itField = 0; itField < m_newDecoder->size(); itField++) {
+    newFields.push_back((*m_newDecoder)[itField].name());
   }
   for (const auto& detectorField : m_detectorIdentifiers) {
     auto iter = std::find(newFields.begin(), newFields.end(), detectorField);
     if (iter == newFields.end()) {
       error() << "New readout does not contain field <<" << detectorField << ">> that describes the detector ID."
-              << endmsg;
+	      << endmsg;
       return StatusCode::FAILURE;
     }
   }
   info() << "Rewritting the readout bitfield." << endmsg;
   info() << "Old bitfield:\t" << m_oldDecoder->fieldDescription() << endmsg;
-  info() << "New bitfield:\t" << m_segmentation->decoder()->fieldDescription() << endmsg;
+  info() << "New bitfield:\t" << m_newDecoder->fieldDescription() << endmsg;
 
   return StatusCode::SUCCESS;
 }
@@ -92,11 +91,11 @@ StatusCode RewriteBitfield::execute() {
     // now rewrite all fields except for those to be removed
     for (const auto& detectorField : m_detectorIdentifiers) {
       oldid = (*m_oldDecoder)[detectorField];
-      (*m_segmentation->decoder())[detectorField] = oldid;
+      (*m_newDecoder)[detectorField] = oldid;
     }
-    newHit.cellId(m_segmentation->decoder()->getValue());
+    newHit.cellId(m_newDecoder->getValue());
     if (debugIter < m_debugPrint) {
-      debug() << "NEW: " << m_segmentation->decoder()->valueString() << endmsg;
+      debug() << "NEW: " << m_newDecoder->valueString() << endmsg;
       debugIter++;
     }
   }
