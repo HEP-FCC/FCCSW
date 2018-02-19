f4c4b4d467ee573d8b1ec3eabfe8624bb464738d
Valentin Volkl <valentin.volkl@cern.ch>
2018-02-19 13:46:49 +0100

    Add more useful particle gun

---
+++ Generation/src/components/ConstPtParticleGun.cpp
@@ -0,0 +1,129 @@
+#include "ConstPtParticleGun.h"
+
+#include <cmath>
+
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "GaudiKernel/IRndmGenSvc.h"
+#include "GaudiKernel/ParticleProperty.h"
+#include "GaudiKernel/PhysicalConstants.h"
+#include "GaudiKernel/SystemOfUnits.h"
+
+#include "HepMC/GenEvent.h"
+#include "HepPDT/ParticleID.hh"
+#include "Pythia8/ParticleData.h"
+
+DECLARE_TOOL_FACTORY(ConstPtParticleGun)
+
+/// Constructor
+ConstPtParticleGun::ConstPtParticleGun(const std::string& type,
+                                                   const std::string& name,
+                                                   const IInterface* parent)
+    : GaudiTool(type, name, parent) {
+  declareInterface<IParticleGunTool>(this);
+}
+
+/// Destructor
+ConstPtParticleGun::~ConstPtParticleGun() {}
+
+/// Initialize Particle Gun parameters
+StatusCode ConstPtParticleGun::initialize() {
+  StatusCode sc = GaudiTool::initialize();
+  if (!sc.isSuccess()) return sc;
+
+  IRndmGenSvc* randSvc = svc<IRndmGenSvc>("RndmGenSvc", true);
+  sc = m_flatGenerator.initialize(randSvc, Rndm::Flat(0., 1.));
+  if (!sc.isSuccess()) return Error("Cannot initialize flat generator");
+
+  // Get the mass of the particle to be generated
+  //
+
+  // check momentum and angles
+  if ((m_minEta > m_maxEta) || (m_minPhi > m_maxPhi))
+    return Error("Incorrect values for theta or phi!");
+
+  m_deltaPhi = m_maxPhi - m_minPhi;
+  m_deltaEta = m_maxEta - m_minEta;
+
+  // setup particle information
+  m_masses.clear();
+  auto pd = Pythia8::ParticleData();
+
+  info() << "Particle type chosen randomly from :";
+  PIDs::iterator icode;
+  for (icode = m_pdgCodes.begin(); icode != m_pdgCodes.end(); ++icode) {
+    info() << " " << *icode;
+    m_masses.push_back(pd.m0(*icode));
+  }
+
+  info() << endmsg;
+
+  info() << "Eta range: " << m_minEta << "  <-> " << m_maxEta 
+         << endmsg;
+  info() << "Phi range: " << m_minPhi / Gaudi::Units::rad << " rad <-> " << m_maxPhi / Gaudi::Units::rad << " rad"
+         << endmsg;
+
+
+  return sc;
+}
+
+/// Generate the particles
+void ConstPtParticleGun::generateParticle(Gaudi::LorentzVector& momentum,
+                                                Gaudi::LorentzVector& origin,
+                                                int& pdgId) {
+
+  origin.SetCoordinates(0., 0., 0., 0.);
+  double px(0.), py(0.), pz(0.);
+
+  // Generate values for energy, theta and phi
+  //
+  double phi = m_minPhi + m_flatGenerator() * (m_deltaPhi);
+  double eta = m_minEta + m_flatGenerator() * (m_deltaEta);
+
+  // Transform to x,y,z coordinates
+  int randIndex = m_flatGenerator() * m_ptList.size();
+  double pt = m_ptList[randIndex];
+  px = pt * cos(phi);
+  py = pt * sin(phi);
+  pz = pt * sinh(eta);
+
+  // randomly choose a particle type
+  unsigned int currentType = (unsigned int)(m_pdgCodes.size() * m_flatGenerator());
+  // protect against funnies
+  if (currentType >= m_pdgCodes.size()) currentType = 0;
+
+   momentum.SetPx(px);
+  momentum.SetPy(py);
+  momentum.SetPz(pz);
+  momentum.SetE(std::sqrt(m_masses[currentType] * m_masses[currentType] + momentum.P2()));
+
+  pdgId = m_pdgCodes[currentType];
+
+  debug() << " -> " << pdgId << endmsg << "   P   = " << momentum << endmsg;
+}
+
+StatusCode ConstPtParticleGun::getNextEvent(HepMC::GenEvent& theEvent) {
+  Gaudi::LorentzVector theFourMomentum;
+  Gaudi::LorentzVector origin;
+  // note: pgdid is set in function generateParticle
+  int thePdgId;
+  generateParticle(theFourMomentum, origin, thePdgId);
+
+  // create HepMC Vertex --
+  // by calling add_vertex(), the hepmc event is given ownership
+  //  of the vertex
+  HepMC::GenVertex* v = new HepMC::GenVertex(HepMC::FourVector(origin.X(), origin.Y(), origin.Z(), origin.T()));
+  // create HepMC particle --
+  // by calling add_particle_out(), the hepmc vertex is given ownership
+  // of the particle
+  HepMC::GenParticle* p = new HepMC::GenParticle(
+      HepMC::FourVector(theFourMomentum.Px(), theFourMomentum.Py(), theFourMomentum.Pz(), theFourMomentum.E()),
+      thePdgId,
+      1);  // hepmc status code for final state particle
+
+  v->add_particle_out(p);
+
+  theEvent.add_vertex(v);
+  theEvent.set_signal_process_vertex(v);
+
+  return StatusCode::SUCCESS;
+}
---
+++ Generation/src/components/ConstPtParticleGun.h
@@ -0,0 +1,66 @@
+#ifndef GENERATION_CONSTPTPARTICLEGUN_H
+#define GENERATION_CONSTPTPARTICLEGUN_H
+
+#include "GaudiAlg/GaudiTool.h"
+#include "GaudiKernel/PhysicalConstants.h"
+#include "GaudiKernel/RndmGenerators.h"
+#include "GaudiKernel/SystemOfUnits.h"
+
+#include "Generation/IParticleGunTool.h"
+
+/** @class ConstPtParticleGun ConstPtParticleGun.h "ConstPtParticleGun.h"
+ *
+ */
+class ConstPtParticleGun : public GaudiTool, virtual public IParticleGunTool {
+
+public:
+  /// Constructor
+  ConstPtParticleGun(const std::string& type, const std::string& name, const IInterface* parent);
+
+  /// Destructor
+  virtual ~ConstPtParticleGun();
+
+  /// Initialize particle gun parameters
+  virtual StatusCode initialize();
+
+  /// Generation of particles
+  virtual void generateParticle(Gaudi::LorentzVector& momentum, Gaudi::LorentzVector& origin, int& pdgId);
+
+  /// Print counters
+  virtual void printCounters() { ; };
+  virtual StatusCode getNextEvent(HepMC::GenEvent&);
+
+private:
+  /// Minimum momentum (Set by options)
+  Gaudi::Property<std::vector<double>> m_ptList{this, "PtList", {100.0 * Gaudi::Units::GeV}, "List of transverse momenta to generate"};
+  /// Minimum theta angle (Set by options)
+  Gaudi::Property<double> m_minEta{this, "EtaMin", -3.5 , "Minimal eta"};
+  /// Minimum phi angle (Set by options)
+  Gaudi::Property<double> m_minPhi{this, "PhiMin", 0. * Gaudi::Units::rad, "Minimal phi"};
+
+  /// Maximum theta angle (Set by options)
+  Gaudi::Property<double> m_maxEta{this, "EtaMax", 3.5, "Maximal eta"};
+  /// Maximum phi angle (Set by options)
+  Gaudi::Property<double> m_maxPhi{this, "PhiMax", Gaudi::Units::twopi* Gaudi::Units::rad, "Maximal phi"};
+
+  /// Momentum range
+  double m_deltaMom;
+  /// Eta range
+  double m_deltaEta;
+  /// Phi range
+  double m_deltaPhi;
+
+  /// Pdg Codes of particles to generate (Set by options)
+  Gaudi::Property<std::vector<int>> m_pdgCodes{this, "PdgCodes", {-211}, "list of pdg codes to produce"};
+
+  /// Masses of particles to generate (derived from PDG codes)
+  std::vector<double> m_masses;
+
+  /// Names of particles to generate (derived from PDG codes)
+  std::vector<std::string> m_names;
+
+  /// Flat random number generator
+  Rndm::Numbers m_flatGenerator;
+};
+
+#endif  // GENERATION_CONSTPTPARTICLEGUN_H
