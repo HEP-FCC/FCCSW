1d7adb13b8a21cf1d38b80d8e0defd47cb96b1c7
CoralieNeubueser <coralie.neubuser@cern.ch>
2018-02-21 16:14:39 +0100

    interface to read noise map in topo-cluster algorithm

---
+++ Reconstruction/RecInterface/RecInterface/ICaloReadCellNoiseMap.h
@@ -0,0 +1,20 @@
+#ifndef RECINTERFACE_ICALOREADCELLNOISEMAP_H
+#define RECINTERFACE_ICALOREADCELLNOISEMAP_H
+
+// Gaudi
+#include "GaudiKernel/IAlgTool.h"
+
+/** @class ICaloReadCellNoiseMap RecInterface/RecInterface/ICaloReadCellNoiseMap.h ICaloReadCellNoiseMap.h
+ *
+ *  Interface to the service creating a map for the calorimetry.
+ *
+ *  @author Coralie Neubueser
+ */
+
+class ICaloReadCellNoiseMap : virtual public IAlgTool {
+public:
+  DeclareInterfaceID(ICaloReadCellNoiseMap, 1, 0);
+
+  virtual double noise(uint64_t aCellId) = 0;
+};
+#endif /* RECINTERFACE_ICALOREADNEIGHBOURSMAP_H */
