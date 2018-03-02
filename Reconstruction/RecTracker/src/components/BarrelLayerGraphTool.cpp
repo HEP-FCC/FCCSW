58213e5d1bf66f74259aa11d19bcc92598e05500
Valentin Volkl <valentin.volkl@cern.ch>
2018-03-03 00:27:21 +0100

    Add layers!

--- Reconstruction/RecTracker/src/components/BarrelLayerGraphTool.cpp
+++ Reconstruction/RecTracker/src/components/BarrelLayerGraphTool.cpp
@@ -31,69 +31,92 @@ tricktrack::CMGraph BarrelLayerGraphTool::getGraph() {
   auto g = tricktrack::CMGraph();
 
   constexpr unsigned int numberOfHits = 1000; // used to resize vector "isOuterHitOfCell""
-  auto l1 = tricktrack::CMLayer("innerLayer", numberOfHits);
-  auto l2 = tricktrack::CMLayer("middleLayer", numberOfHits);
-  auto l3 = tricktrack::CMLayer("outerLayer", numberOfHits);
-  auto l4 = tricktrack::CMLayer("outermostLayer", numberOfHits);
-  auto l5 = tricktrack::CMLayer("firstEndcap", numberOfHits);
-  auto l6 = tricktrack::CMLayer("secondEndcap", numberOfHits);
-  auto l7 = tricktrack::CMLayer("thirdEndcap", numberOfHits);
-  auto l8 = tricktrack::CMLayer("fourthEndcap", numberOfHits);
+  auto lb0 = tricktrack::CMLayer("innerLayer", numberOfHits);
+  auto lb1 = tricktrack::CMLayer("middleLayer", numberOfHits);
+  auto lb2 = tricktrack::CMLayer("outerLayer", numberOfHits);
+  auto lb3 = tricktrack::CMLayer("outermostLayer", numberOfHits);
+  auto le4 = tricktrack::CMLayer("firstEndcap", numberOfHits);
+  auto le5 = tricktrack::CMLayer("secondEndcap", numberOfHits);
+  auto le6 = tricktrack::CMLayer("thirdEndcap", numberOfHits);
+  auto le7 = tricktrack::CMLayer("fourthEndcap", numberOfHits);
 
-  auto lp1 = tricktrack::CMLayerPair(0, 1);
-  auto lp2 = tricktrack::CMLayerPair(1, 2);
-  auto lp3 = tricktrack::CMLayerPair(2, 3);
-  auto lp4 = tricktrack::CMLayerPair(2, 3);
-  auto lp5 = tricktrack::CMLayerPair(2, 4);
-  auto lp6 = tricktrack::CMLayerPair(1, 4);
-  auto lp7 = tricktrack::CMLayerPair(0, 4);
-  auto lp8 = tricktrack::CMLayerPair(4, 5);
-  auto lp9 = tricktrack::CMLayerPair(5, 6);
-  auto lp10 = tricktrack::CMLayerPair(6, 7);
-  l1.theOuterLayers.push_back(1);
-  l2.theInnerLayers.push_back(0);
-  l2.theOuterLayers.push_back(2);
-  l3.theInnerLayers.push_back(1);
-  l3.theOuterLayers.push_back(3);
-  l4.theInnerLayers.push_back(2);
-  l5.theInnerLayers.push_back(0);
-  l5.theInnerLayers.push_back(1);
-  l5.theInnerLayers.push_back(2);
-  l5.theOuterLayers.push_back(5);
-  l6.theInnerLayers.push_back(4);
-  l6.theOuterLayers.push_back(6);
-  l7.theInnerLayers.push_back(5);
-  l7.theOuterLayers.push_back(7);
-  l8.theInnerLayers.push_back(6);
-  l1.theOuterLayerPairs.push_back(0);
-  l1.theOuterLayerPairs.push_back(6);
-  l2.theInnerLayerPairs.push_back(0);
-  l2.theOuterLayerPairs.push_back(5);
-  l2.theOuterLayerPairs.push_back(1);
-  l3.theInnerLayerPairs.push_back(4);
-  l3.theOuterLayerPairs.push_back(2);
-  l3.theOuterLayerPairs.push_back(2);
-  l4.theInnerLayerPairs.push_back(2);
+  // barrel - barrel
+  auto lbb0 = tricktrack::CMLayerPair(0, 1);
+  auto lbb1 = tricktrack::CMLayerPair(1, 2);
+  auto lbb2 = tricktrack::CMLayerPair(2, 3);
+  // barrel - endcap
+  auto lbe3 = tricktrack::CMLayerPair(0, 4);
+  auto lbe4 = tricktrack::CMLayerPair(1, 4);
+  auto lbe5 = tricktrack::CMLayerPair(2, 4);
+  auto lbe6 = tricktrack::CMLayerPair(3, 4);
+  // endcap - endcap
+  auto lee7 = tricktrack::CMLayerPair(4, 5);
+  auto lee8 = tricktrack::CMLayerPair(5, 6);
+  auto lee9 = tricktrack::CMLayerPair(6, 7);
+
+  // barrel - barrel
+  lb0.theOuterLayers.push_back(1);
+  lb1.theInnerLayers.push_back(0);
+  lb1.theOuterLayers.push_back(2);
+  lb2.theInnerLayers.push_back(1);
+  lb2.theOuterLayers.push_back(3);
+  lb3.theInnerLayers.push_back(2);
+  // barrel - endcap
+  le4.theInnerLayers.push_back(0);
+  lb0.theOuterLayers.push_back(4);
+  le4.theInnerLayers.push_back(1);
+  lb1.theOuterLayers.push_back(4);
+  le4.theInnerLayers.push_back(2);
+  lb2.theOuterLayers.push_back(4);
+  le4.theInnerLayers.push_back(3);
+  lb3.theOuterLayers.push_back(4);
+  // endcap - endcap
+  le4.theOuterLayers.push_back(5);
+  le5.theInnerLayers.push_back(4);
+  le5.theOuterLayers.push_back(6);
+  le6.theInnerLayers.push_back(5);
+  le6.theOuterLayers.push_back(7);
+  le7.theInnerLayers.push_back(6);
+
+  // barrel - barrel
+  lb0.theOuterLayerPairs.push_back(0);
+  lb1.theInnerLayerPairs.push_back(0);
+  lb1.theOuterLayerPairs.push_back(1);
+  lb2.theInnerLayerPairs.push_back(1);
+  lb2.theOuterLayerPairs.push_back(2);
+  lb3.theInnerLayerPairs.push_back(2);
+  // barrel - endcap
+  lb0.theOuterLayerPairs.push_back(3);
+  le4.theInnerLayerPairs.push_back(3);
+  lb1.theOuterLayerPairs.push_back(4);
+  le4.theInnerLayerPairs.push_back(4);
+  lb2.theOuterLayerPairs.push_back(5);
+  le4.theInnerLayerPairs.push_back(5);
+  lb3.theOuterLayerPairs.push_back(6);
+  le4.theInnerLayerPairs.push_back(6);
+
+
+  //endcap - endcap
 
   auto theLayerGraph = tricktrack::CMGraph();
-  g.theLayers.push_back(l1);
-  g.theLayers.push_back(l2);
-  g.theLayers.push_back(l3);
-  g.theLayers.push_back(l4);
-  g.theLayers.push_back(l5);
-  g.theLayers.push_back(l6);
-  g.theLayers.push_back(l7);
-  g.theLayers.push_back(l8);
-  g.theLayerPairs.push_back(lp1);
-  g.theLayerPairs.push_back(lp2);
-  g.theLayerPairs.push_back(lp3);
-  g.theLayerPairs.push_back(lp4);
-  g.theLayerPairs.push_back(lp5);
-  g.theLayerPairs.push_back(lp6);
-  g.theLayerPairs.push_back(lp7);
-  g.theLayerPairs.push_back(lp8);
-  g.theLayerPairs.push_back(lp9);
-  g.theLayerPairs.push_back(lp10);
+  g.theLayers.push_back(lb0);
+  g.theLayers.push_back(lb1);
+  g.theLayers.push_back(lb2);
+  g.theLayers.push_back(lb3);
+  g.theLayers.push_back(le4);
+  g.theLayers.push_back(le5);
+  g.theLayers.push_back(le6);
+  g.theLayers.push_back(le7);
+  g.theLayerPairs.push_back(lbb0);
+  g.theLayerPairs.push_back(lbb1);
+  g.theLayerPairs.push_back(lbb2);
+  g.theLayerPairs.push_back(lbe3);
+  g.theLayerPairs.push_back(lbe4);
+  g.theLayerPairs.push_back(lbe5);
+  g.theLayerPairs.push_back(lbe6);
+  g.theLayerPairs.push_back(lee7);
+  g.theLayerPairs.push_back(lee8);
+  g.theLayerPairs.push_back(lee9);
   g.theRootLayers.push_back(0);
 
   return g;
