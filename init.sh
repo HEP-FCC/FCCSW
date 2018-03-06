412544eb97c3b1a355313a2f2d28fc155d5b109e
Valentin Volkl <valentin.volkl@cern.ch>
2018-03-06 15:19:10 +0100

    Use new installation

--- init.sh
+++ init.sh
@@ -2,8 +2,6 @@
 #!/bin/sh -u
 # set FCCSWBASEDIR to the directory containing this script
 export FCCSWBASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source /cvmfs/fcc.cern.ch/sw/views/releases/0.9.0/x86_64-slc6-gcc62-opt/setup.sh
-export CMAKE_PREFIX_PATH=/afs/cern.ch/work/v/vavolkl/public/fcc.cern.ch/sw/0.9.0/tricktrack/install:$CMAKE_PREFIX_PATH
-export CMAKE_PREFIX_PATH=/afs/cern.ch/work/v/vavolkl/public/fcc.cern.ch/sw/0.9.0/fcc-edm/0.5.1/x86_64-slc6-gcc62-opt:$CMAKE_PREFIX_PATH
+source /cvmfs/fcc.cern.ch/sw/views/nightlies/Fri/x86_64-slc6-gcc62-opt/setup.sh
 
 
