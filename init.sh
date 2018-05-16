5d92423fcb5462e01876e2435c7dbd49ddd21f23
Javier Cervantes Villanueva <javier.cervantes.villanueva@cern.ch>
2018-05-16 11:49:19 +0200

    Switch to a stable external version

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@
 export FCCSWBASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 weekday=`date +%a`
 
-source /cvmfs/fcc.cern.ch/sw/views/nightlies/Wed/x86_64-slc6-gcc62-opt/setup.sh
+source /cvmfs/fcc.cern.ch/sw/views/releases/externals/93.0.0/x86_64-slc6-gcc62-opt/setup.sh
