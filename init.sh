675a7d8b0325808c1d17b54423cddc33e421357f
Anna Zaborowska <anna.zaborowska@cern.ch>
2018-05-25 19:07:03 +0200

    Temporarily add path to analysis

--- init.sh
+++ init.sh
@@ -4,3 +4,4 @@
 export FCCSWBASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 weekday=`date +%a`
 source /cvmfs/fcc.cern.ch/sw/views/releases/0.9.1/x86_64-slc6-gcc62-opt/setup.sh
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/afs/cern.ch/work/c/cneubuse/public/FCC_calo_analysis_cpp/install/lib/
