23f299da5674421800582cfe4c46ba7cf63d0926
jhrdinka <julia.hrdinka@cern.ch>
2018-10-12 13:37:17 +0200

    Consistent naming of trackIDs and change to int instead of unsigned for root output

--- Reconstruction/RecTracker/RecTracker/FCCDigitizationCell.h
+++ Reconstruction/RecTracker/RecTracker/FCCDigitizationCell.h
@@ -17,31 +17,34 @@ namespace sim {
 /// @struct FCCDigitizationCell
 /// @brief wrapper of Acts::DigitizationCell to store hit information from fcc
 struct FCCDigitizationCell : Acts::DigitizationCell {
-  /// connection to FCC track hits
-  std::vector<unsigned> trackHits;
+  /// track IDs of this cell
+  std::vector<int> trackIDs;
+  /// time of this cell
   float time = 0;
+  /// count how often this cell was hit in the same event
   size_t mergedCellsCount = 0;
   /// constructor
   /// @param the FCC track hits of the digitization cell
   /// @param ch0 first channel number
   /// @param ch1 second channel number
   /// @param d data of cell (energy for analogue readout, 1 for digital readout)
-  FCCDigitizationCell(std::vector<unsigned> th, float t, size_t ch0, size_t ch1, float d = 0.)
-      : DigitizationCell(ch0, ch1, d), trackHits(th), time(t) {}
+  FCCDigitizationCell(std::vector<int> TrackIDs, float Time, size_t Channel0, size_t Channel1, float Data = 0.)
+      : DigitizationCell(Channel0, Channel1, Data), trackIDs(TrackIDs), time(Time) {}
   // copy constructor
   FCCDigitizationCell(const FCCDigitizationCell& dc)
-      : DigitizationCell(dc), trackHits(dc.trackHits), time(dc.time), mergedCellsCount(dc.mergedCellsCount) {}
+      : DigitizationCell(dc), trackIDs(dc.trackIDs), time(dc.time), mergedCellsCount(dc.mergedCellsCount) {}
   // to merge cells in case they are at the same position
   void addCell(const FCCDigitizationCell& dc) {
-    for (auto& th : dc.trackHits)
-      trackHits.push_back(th);
+    for (auto& tID : dc.trackIDs)
+      trackIDs.push_back(tID);
     data += dc.data;
     time += dc.time;
     mergedCellsCount++;
   }
   // @todo remove readout boolean in acts
+  /// total deposited energy in this cell
   float depositedEnergy() const { return data; }
-
+  /// averaged time of this cell
   float averagedTime() const { return ((mergedCellsCount > 1) ? (time / mergedCellsCount) : time); }
 };
 
@@ -55,8 +58,8 @@ struct FCCPlanarCluster : Acts::Measurement_t<Acts::ParDef::eLOC_0, Acts::ParDef
   float energy;
   /// the time information of the cluster
   float time;
-  /// the tracks contributing to this cluster
-  std::vector<unsigned> tracks;
+  /// the tracks IDs contributing to this cluster
+  std::vector<int> trackIDs;
 
   /// constructor
   /// @param en the energy deposited in the cluster
@@ -67,14 +70,14 @@ struct FCCPlanarCluster : Acts::Measurement_t<Acts::ParDef::eLOC_0, Acts::ParDef
   /// @param loc0 the local position of the cluster - first coordinate
   /// @param loc1 the local position of the cluster - second coordinate
   /// @param dCells the cells contributing to the cluster
-  FCCPlanarCluster(float Energy, float Time, const std::vector<unsigned>& Tracks, const Acts::Surface& mSurface,
+  FCCPlanarCluster(float Energy, float Time, const std::vector<int>& TrackIDs, const Acts::Surface& mSurface,
                    const Identifier& cIdentifier, Acts::ActsSymMatrixD<2> cov, double loc0, double loc1,
                    std::vector<FCCDigitizationCell> dCells)
       : Acts::Measurement_t<Acts::ParDef::eLOC_0, Acts::ParDef::eLOC_1>(mSurface, cIdentifier, std::move(cov), loc0,
                                                                         loc1),
         energy(Energy),
         time(Time),
-        tracks(Tracks),
+        trackIDs(TrackIDs),
         m_digitizationCells(dCells) {}
 
   /// access to the digitization cells
