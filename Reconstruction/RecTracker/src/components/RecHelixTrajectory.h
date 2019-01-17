d05c23f2015c24c631bcb69699d9ca148810b510
Valentin Volkl <valentin.volkl@cern.ch>
2019-01-17 14:29:18 +0100

    Clean up RecHelixtrajectory

--- Reconstruction/RecTracker/src/components/RecHelixTrajectory.h
+++ Reconstruction/RecTracker/src/components/RecHelixTrajectory.h
@@ -3,15 +3,10 @@
 
 // GAUDI
 #include "GaudiAlg/GaudiAlgorithm.h"
-#include "GaudiKernel/RndmGenerators.h"
 #include "GaudiKernel/ToolHandle.h"
 
 // FCCSW
 #include "FWCore/DataHandle.h"
-
-class IGeoSvc;
-class ITrackSeedingTool;
-class ITrackFittingTool;
 
 namespace fcc {
 class TrackHitCollection;
@@ -21,7 +16,8 @@ class PositionedTrackHitCollection;
 }
 
 /** @class RecHelixtrajectory
- *  Calculate points on the helix from the track state.
+ *  Returns a list of equidistant points along the ideal helix a trackstate
+ *  represents, for debugging and visualizations.
  */
 class RecHelixTrajectory : public GaudiAlgorithm {
 public:
@@ -36,17 +32,21 @@ public:
   StatusCode finalize() override final;
 
 private:
-  /// TrackHits as Input to the track seeding
-  DataHandle<fcc::PositionedTrackHitCollection> m_recHelixPoints{"RecHelixPoints", Gaudi::DataHandle::Writer,
-                                                                      this};
-  DataHandle<fcc::PositionedTrackHitCollection> m_recExtrapolationPoint{"RecExtrapolationPoint", Gaudi::DataHandle::Writer,
-                                                                      this};
-  DataHandle<fcc::TrackCollection> m_tracks{"Tracks", Gaudi::DataHandle::Reader, this};
+  /// Output: Points along all the helices of the input tracks
+  DataHandle<fcc::PositionedTrackHitCollection> m_recHelixPoints{"RecHelixPoints", Gaudi::DataHandle::Writer, this};
+  /// Input: TrackStates for which to calculate helix
   DataHandle<fcc::TrackStateCollection> m_trackStates{"TrackStates", Gaudi::DataHandle::Reader, this};
-  Gaudi::Property<double> m_stepSize {this, "stepSize", 0.0001};
-  Gaudi::Property<double> m_maxPathLength {this, "maxPathLength", 2000000.};
-  Gaudi::Property<double> m_maxR {this, "maxR", 1600. };
-  Gaudi::Property<double> m_maxZ {this, "maxZ", 15000. };
+  ///
+  Gaudi::Property<double> m_stepSize{this, "stepSize", 0.0001 * Gaudi::Units::mm};
+  /// hard path length limit for the generated helix,
+  // if it is longer that maxPathLength, stop adding points
+  Gaudi::Property<double> m_maxPathLength{this, "maxPathLength", 2000000. * Gaudi::Units::mm};
+  /// hard geometric limit for the generated helix,
+  // stop adding points when the distance to the beamline is more than maxR
+  Gaudi::Property<double> m_maxR{this, "maxR", 1600. * Gaudi::Units::mm};
+  /// hard geometric limit for the generated helix,
+  // stop adding points when the longitudinal distance from the origin is greater than maxZ
+  Gaudi::Property<double> m_maxZ{this, "maxZ", 15000. * Gaudi::Units::mm};
 };
 
 #endif /* RECTRACKER_RECHELIXTRAJECTORY_H */
