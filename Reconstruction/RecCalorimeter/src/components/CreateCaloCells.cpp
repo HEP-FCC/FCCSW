fd304aa730e2b3c5bc9c77a85cf095929c5f200c
Anna Zaborowska <anna.zaborowska@cern.ch>
2019-02-21 09:34:19 +0100

    fix parameters in the name

--- Reconstruction/RecCalorimeter/src/components/CreateCaloCells.cpp
+++ Reconstruction/RecCalorimeter/src/components/CreateCaloCells.cpp
@@ -13,7 +13,7 @@
 
 DECLARE_ALGORITHM_FACTORY(CreateCaloCells)
 
-CreateCaloCells::CreateCaloCells(const std::string& name, ISvcLocator* svcLoc) : GaudiAlgorithm(name, svcLoc) {
+CreateCaloCells::CreateCaloCells(const std::string& aName, ISvcLocator* aSvcLoc) : GaudiAlgorithm(aName, aSvcLoc) {
   declareProperty("hits", m_hits, "Hits from which to create cells (input)");
   declareProperty("cells", m_cells, "The created calorimeter cells (output)");
 
