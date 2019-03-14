be5d0461fd5d6eccf68d9f35f9513320d5b1cf66
Valentin Volkl <valentin.volkl@cern.ch>
2019-03-14 09:43:50 +0100

    Change CaloHitClass in remaining SDs

--- Detector/DetSensitive/src/SimpleCalorimeterSD.cpp
+++ Detector/DetSensitive/src/SimpleCalorimeterSD.cpp
@@ -41,11 +41,11 @@ bool SimpleCalorimeterSD::ProcessHits(G4Step* aStep, G4TouchableHistory*) {
 
   // as in dd4hep::sim::Geant4GenericSD<Calorimeter>
   const G4Track* track = aStep->GetTrack();
-  CLHEP::Hep3Vector prePos = aStep->GetPreStepPoint()->GetPosition();
   auto hit = new fcc::Geant4CaloHit(
       track->GetTrackID(), track->GetDefinition()->GetPDGEncoding(), edep, track->GetGlobalTime());
   hit->cellID = utils::cellID(m_seg, *aStep);
   hit->energyDeposit = edep;
+  CLHEP::Hep3Vector prePos = aStep->GetPreStepPoint()->GetPosition();
   hit->position = prePos;
   m_calorimeterCollection->insert(hit);
   return true;
