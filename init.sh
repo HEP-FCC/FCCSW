d2bf6eb0c9a3288f45f68a103388dad10d714e7f
Gerardo GANIS <gerardo.ganis@cern.ch>
2019-06-26 10:56:47 +0200

    Add some automation to init.sh
    Detect the platform. Allow to specify the version.

--- init.sh
+++ init.sh
@@ -12,5 +12,39 @@ if [ ! -L $FCCSWBASEDIR/.git/hooks/pre-commit ]; then
   ln -s $FCCSWBASEDIR/git-hooks/pre-commit.py $FCCSWBASEDIR/.git/hooks/pre-commit
 fi
 
+# Version
+versiontag="94.2.0"
+if ! test "x$1" = "x" ; then
+   versiontag="$1"
+fi
+
+# Guess OS
+ostag="x86_64-slc6"
+if test -f "/etc/redhat-release"; then
+   six=`grep "release 7" /etc/redhat-release`
+   if test ! "x$six" = "x" ; then
+      ostag="x86_64-centos7"
+   fi
+fi
+
+# Compiler
+gcctag="gcc62"
+
+# Platform
+platform="$ostag-$gcctag-opt"
+
+# Setup script
+setuppath="/cvmfs/fcc.cern.ch/sw/views/releases/externals/$versiontag/$platform/setup.sh"
+
 weekday=`date +%a`
-source /cvmfs/fcc.cern.ch/sw/views/releases/externals/94.2.0/x86_64-slc6-gcc62-opt/setup.sh
+if test -f $setuppath ; then
+   source $setuppath
+else
+   echo "Setup script not found: $setuppath! "
+   echo "Platforms available for this version:"
+   ls -1 /cvmfs/fcc.cern.ch/sw/views/releases/externals/$versiontag
+   echo "Versions available for this platform:"
+   ls -1 /cvmfs/fcc.cern.ch/sw/views/releases/externals/*/$platform/setup.sh
+fi
+
+#source /cvmfs/fcc.cern.ch/sw/views/releases/externals/94.2.0/x86_64-slc6-gcc62-opt/setup.sh
