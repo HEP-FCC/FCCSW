fab08fb81976611e66eb826a76cbeb700aad2e55
Valentin Volkl <valentin.volkl@cern.ch>
2020-06-02 16:07:37 +0200

    [SimG4ConstantMagneticField] add property for minStep

--- Sim/SimG4Components/src/SimG4ConstantMagneticFieldTool.h
+++ Sim/SimG4Components/src/SimG4ConstantMagneticFieldTool.h
@@ -68,6 +68,8 @@ private:
   Gaudi::Property<double> m_deltaOneStep{this, "DeltaOneStep", 0, "Delta(one-step)"};
   /// Upper limit of the step size, see G4 doc for more details. Set with property MaximumStep
   Gaudi::Property<double> m_maxStep{this, "MaximumStep", 1. * m, "Maximum step length in field (see G4 documentation)"};
+  /// Lower limit of the step size, see G4 doc for more details. Set with property MaximumStep
+  Gaudi::Property<double> m_minxStep{this, "MinimumStep", 0.01 * mm, "Maximum step length in field (see G4 documentation)"};
   /// Name of the integration stepper, defaults to NystromRK4.
   Gaudi::Property<std::string> m_integratorStepper{this, "IntegratorStepper", "NystromRK4", "Integrator stepper name"};
 
