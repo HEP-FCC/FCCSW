18c6042f9161c15f18f2623c86da6b37a1bebf86
Valentin Volkl <valentin.volkl@cern.ch>
2020-06-09 14:07:47 +0200

    [ci] cleanup

--- .github/scripts/gitlabmirror.sh
+++ .github/scripts/gitlabmirror.sh
@@ -9,25 +9,12 @@ echo ${GITHUB_REF}
 # leave ancient history undisturbed
 git fetch --shallow-since=01/01/2020
 
-#github_ref
-#git checkout "${GITHUB_REF}"
-
-#branch=${GITHUB_REPOSITORY}/$(git symbolic-ref --short HEAD)
 shortbranch=$(git symbolic-ref --short HEAD)
-
-#git config --global credential.username $GITLAB_USERNAME
-#git config --global core.askPass .github/scripts/cred-helper.sh
-#git config --global credential.helper cache
-#git remote add mirror $GITLAB_URL
-#echo pushing to $shortbranch branch at $(git remote get-url --push mirror)
-#git push mirror $shortbranch
-
 
 # need a token with api acces
 curl --silent   --header "PRIVATE-TOKEN: ${GITLAB_PASSWORD2}" -X https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/mirror/pull
 
 sleep $POLL_TIMEOUT
-
 
 pipeline_id=$(curl --silent "https://${GITLAB_HOSTNAME}/api/v4/projects/${GITLAB_PROJECT_ID}/repository/commits/${shortbranch}" | jq '.last_pipeline.id')
 
