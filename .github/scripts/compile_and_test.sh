c7b862d4b8cd7bda316fd4a91a890b675afed73f
Valentin Volkl <valentin.volkl@cern.ch>
2020-06-09 14:07:47 +0200

    [ci] use github actions

---
+++ .github/scripts/compile_and_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /Package
+source init.sh
+mkdir build install
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17  .. && \
+make  -j `getconf _NPROCESSORS_ONLN` && \
+make install && \
+ls -alhR $G4LEDATA && \
+ctest -j `getconf _NPROCESSORS_ONLN` --output-on-failure
