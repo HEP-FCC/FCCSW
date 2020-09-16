9eb7c23ad6990940e0e94cb3978b2610cb9c99bb
Valentin Volkl <valentin.volkl@cern.ch>
2020-09-16 14:13:00 +0200

    [ci] gaudi xenv workarounds

--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@ LCGPREFIX=/cvmfs/sft.cern.ch/lcg
 LCGPATH=/cvmfs/sft.cern.ch/lcg/views/LCG_97a_FCC_2/x86_64-centos7-gcc8-opt
 export BINARY_TAG=x86_64-centos7-gcc8-opt
 source $LCGPATH/bin/thisdd4hep_only.sh
+export Gaudi_DIR=/cvmfs/sft.cern.ch/lcg/releases/Gaudi/v33r1-2775b/x86_64-centos7-gcc8-opt/
 
 source /cvmfs/sft.cern.ch/lcg/views/LCG_97a_FCC_2/x86_64-centos7-gcc8-opt/setup.sh
