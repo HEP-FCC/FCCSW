0baf6672bec0cdcbeddcef6d6e1ca9a6162a60a0
Valentin Volkl <valentin.volkl@cern.ch>
2020-09-16 13:31:53 +0200

    use LCG_97a_FCC_2; add missing setup vars

--- init.sh
+++ init.sh
@@ -1,1 +1,6 @@
+LCGPREFIX=/cvmfs/sft.cern.ch/lcg
+LCGPATH=/cvmfs/sft.cern.ch/lcg/views/LCG_97a_FCC_2/x86_64-centos7-gcc8-opt
+export BINARY_TAG=x86_64-centos7-gcc8-opt
+source $LCGPATH/bin/thisdd4hep_only.sh
+
 source /cvmfs/sft.cern.ch/lcg/views/LCG_97a_FCC_2/x86_64-centos7-gcc8-opt/setup.sh
