08e1d1ede50d106c10504c4bf474a2fce3560ac8
Valentin Volkl <valentin.volkl@cern.ch>
2020-09-19 18:09:27 +0200

    fix tests

--- .github/scripts/compile_and_test.sh
+++ .github/scripts/compile_and_test.sh
@@ -4,7 +4,7 @@ cd /Package
 source init.sh
 mkdir build install
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17 -DHOST_BINARY_TAG=x86_64-linux-gcc9-opt  .. && \
+cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_CXX_STANDARD=17   .. && \
 make  -j `getconf _NPROCESSORS_ONLN` && \
 make install && \
 ls -alhR $G4LEDATA && \
