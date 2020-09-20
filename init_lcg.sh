473bbaa25005e9aba0abf4cf37f78717f5324ebe
Valentin Volkl <valentin.volkl@cern.ch>
2020-09-20 13:43:09 +0200

    improve ci

---
+++ init_lcg.sh
@@ -0,0 +1,8 @@
+LCGPREFIX=/cvmfs/sft.cern.ch/lcg
+LCGPATH=/cvmfs/sft.cern.ch/lcg/views/LCG_97a_FCC_2/x86_64-centos7-gcc8-opt
+export BINARY_TAG=x86_64-centos7-gcc8-opt
+source $LCGPATH/bin/thisdd4hep_only.sh
+
+export Gaudi_DIR=/cvmfs/sft.cern.ch/lcg/releases/Gaudi/v33r1-2775b/x86_64-centos7-gcc8-opt
+export CMAKE_PREFIX_PATH=$Gaudi_DIR:$CMAKE_PREFIX_PATH
+source /cvmfs/sft.cern.ch/lcg/views/LCG_97a_FCC_2/x86_64-centos7-gcc8-opt/setup.sh
