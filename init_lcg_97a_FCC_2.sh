6c2e49639ebf6b666a525469fe86798666cc5115
Valentin Volkl <valentin.volkl@cern.ch>
2020-10-11 12:12:29 +0200

    add lcg 98 and corresponding fixes

---
+++ init_lcg_97a_FCC_2.sh
@@ -0,0 +1,9 @@
+LCGPREFIX=/cvmfs/sft.cern.ch/lcg
+export BINARY_TAG=x86_64-centos7-gcc8-opt
+LCGPATH=$LCGPREFIX/views/LCG_97a_FCC_2/$BINARY_TAG
+source $LCGPATH/bin/thisdd4hep_only.sh
+
+
+export Gaudi_DIR=$(dirname $(readlink -f "$(which gaudirun.py)"))/../
+export CMAKE_PREFIX_PATH=$Gaudi_DIR:$CMAKE_PREFIX_PATH
+source $LCGPATH/setup.sh
