658139d53e8e6a4e4b82a6d683e70b541d31c43e
vvolkl <valentin.volkl@cern.ch>
2020-10-12 13:44:25 +0200

    Fixes for LCG98 (#409)
    * add lcg 98 and corresponding fixes
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- test_lcg.sh
+++ test_lcg.sh
@@ -6,3 +6,10 @@ source $LCGPATH/bin/thisdd4hep_only.sh
 export Gaudi_DIR=/cvmfs/sft.cern.ch/lcg/releases/Gaudi/v33r1-2775b/x86_64-centos7-gcc8-opt
 export CMAKE_PREFIX_PATH=$Gaudi_DIR:$CMAKE_PREFIX_PATH
 source /cvmfs/sft.cern.ch/lcg/views/LCG_97a_FCC_2/x86_64-centos7-gcc8-opt/setup.sh
+
+export FCCSWBASEDIR=/cvmfs/sft.cern.ch/lcg/releases/fccsw/0.13-5b877/x86_64-centos7-gcc8-opt/
+export FCCSWSHAREDIR=$FCCSWBASEDIR/share/FCCSW
+export FCC_DETECTORS=$FCCSWSHAREDIR
+export FCC_PYTHIACARDS=$FCCSWSHAREDIR
+export FCCSW=/cvmfs/sft.cern.ch/lcg/releases/fccsw/0.13-5b877/x86_64-centos7-gcc8-opt/share/FCCSW
+
