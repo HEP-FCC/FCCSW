5fcf957fc58e45646bc112da26c25aedc8cf803b
Valentin Volkl <valentin.volkl@cern.ch>
2020-10-28 12:33:57 +0100

    fix geosvc for spack

--- Detector/DetComponents/src/GeoSvc.cpp
+++ Detector/DetComponents/src/GeoSvc.cpp
@@ -1,10 +1,3 @@
-//
-//  GeoSvc.cxx
-//
-//
-//  Created by Julia Hrdinka on 30/03/15.
-//
-//
 
 #include "GeoSvc.h"
 #include "GaudiKernel/Service.h"
@@ -18,14 +11,11 @@ using namespace Gaudi;
 DECLARE_COMPONENT(GeoSvc)
 
 GeoSvc::GeoSvc(const std::string& name, ISvcLocator* svc)
-: base_class(name, svc), m_incidentSvc("IncidentSvc", "GeoSvc"), m_dd4hepgeo(0), m_geant4geo(0), m_log(msgSvc(), name), m_failureFlag(false) {}
+: base_class(name, svc), m_dd4hepgeo(0), m_geant4geo(0) {}
 
 GeoSvc::~GeoSvc() {
   if (m_dd4hepgeo){
-    try{
-      m_dd4hepgeo->destroyInstance();
-      m_dd4hepgeo = 0;
-    } catch(...) {} 
+      //m_dd4hepgeo->destroyInstance();
   }
 }
 
@@ -38,19 +28,16 @@ StatusCode GeoSvc::initialize() {
   }
   uint printoutLevel = msgLevel();
   dd4hep::setPrintLevel(dd4hep::PrintLevel(printoutLevel));
-  m_incidentSvc->addListener(this, "GeometryFailure");
   if (buildDD4HepGeo().isFailure())
-    m_log << MSG::ERROR << "Could not build DD4Hep geometry" << endmsg;
+    error() << "Could not build DD4Hep geometry" << endmsg;
   else
-    m_log << MSG::INFO << "DD4Hep geometry SUCCESSFULLY built" << endmsg;
+    info() <<  "DD4Hep geometry SUCCESSFULLY built" << endmsg;
 
   if (buildGeant4Geo().isFailure())
-    m_log << MSG::ERROR << "Could not build Geant4 geometry" << endmsg;
+    error() << "Could not build Geant4 geometry" << endmsg;
   else
-    m_log << MSG::INFO << "Geant4 geometry SUCCESSFULLY built" << endmsg;
-  if (m_failureFlag) {
-    return StatusCode::FAILURE;
-  }
+    info() << "Geant4 geometry SUCCESSFULLY built" << endmsg;
+  // TODO: return failure
   return StatusCode::SUCCESS;
 }
 
@@ -63,7 +50,7 @@ StatusCode GeoSvc::buildDD4HepGeo() {
 
   // load geometry
   for (auto& filename : m_xmlFileNames) {
-    m_log << MSG::INFO << "loading geometry from file:  '" << filename << "'" << endmsg;
+    info() << "loading geometry from file:  '" << filename << "'" << endmsg;
     m_dd4hepgeo->fromCompact(filename);
   }
   m_dd4hepgeo->volumeManager();
@@ -87,9 +74,3 @@ StatusCode GeoSvc::buildGeant4Geo() {
 
 G4VUserDetectorConstruction* GeoSvc::getGeant4Geo() { return (m_geant4geo.get()); }
 
-void GeoSvc::handle(const Incident& inc) {
-  error() << "Handling incident '" << inc.type() << "'" << endmsg;
-  if (!inc.type().compare("GeometryFailure")) {
-    m_failureFlag = true;
-  }
-}
