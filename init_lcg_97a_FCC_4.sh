f41c330c3b61531f15d7e9f412a4c4b9c5b683e4
Valentin Volkl <valentin.volkl@cern.ch>
2020-11-04 20:01:56 +0100

    up

--- init_lcg_97a_FCC_4.sh
+++ init_lcg_97a_FCC_4.sh
@@ -5,6 +5,7 @@ source $LCGPATH/bin/thisdd4hep_only.sh
 source $LCGPATH/setup.sh
 
 export PATH=/cvmfs/sft.cern.ch/lcg/releases/CMake/3.17.3-75516/x86_64-centos7-gcc8-opt/bin:$PATH
+export PATH=/cvmfs/sft.cern.ch/lcg/releases/ninja/1.10.0-3d8f6/x86_64-centos7-gcc8-opt/bin:$PATH
 
 export Gaudi_DIR=$(dirname $(readlink -f "$(which gaudirun.py)"))/../
 export CMAKE_PREFIX_PATH=$Gaudi_DIR:$CMAKE_PREFIX_PATH
