b81a3fb27f34c243dadb5cda2a7b4889a28d98ab
Valentin Volkl <valentin.volkl@cern.ch>
2020-11-12 15:22:00 +0100

    update init

--- init_lcg_97a_FCC_4.sh
+++ init_lcg_97a_FCC_4.sh
@@ -1,6 +1,6 @@
 LCGPREFIX=/cvmfs/sft.cern.ch/lcg
 export BINARY_TAG=x86_64-centos7-gcc8-opt
-LCGPATH=$LCGPREFIX/views/LCG_97a_FCC_2/$BINARY_TAG
+LCGPATH=$LCGPREFIX/views/LCG_97a_FCC_4/$BINARY_TAG
 source $LCGPATH/bin/thisdd4hep_only.sh
 source $LCGPATH/setup.sh
 
@@ -8,4 +8,6 @@ source $LCGPATH/setup.sh
 export Gaudi_DIR=$(dirname $(readlink -f "$(which gaudirun.py)"))/../
 export CMAKE_PREFIX_PATH=$Gaudi_DIR:$CMAKE_PREFIX_PATH
 
-source init_fccdependencies_local.sh
+export CMAKE_PREFIX_PATH=/cvmfs/sft.cern.ch/lcg/releases/k4FWCore/00-01-01-8d426/x86_64-centos7-gcc8-opt/:$CMAKE_PREFIX_PATH
+
+#source init_fccdependencies_local.sh
