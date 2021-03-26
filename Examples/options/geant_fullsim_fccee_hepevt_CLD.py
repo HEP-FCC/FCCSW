f86105910cb74394083e4b2813308ccf4aea4973
Valentin Volkl <valentin.volkl@cern.ch>
2021-03-26 08:53:18 +0100

    update cld example file

--- Examples/options/geant_fullsim_fccee_hepevt_CLD.py
+++ Examples/options/geant_fullsim_fccee_hepevt_CLD.py
@@ -11,28 +11,6 @@ from Gaudi.Configuration import *
 # /afs/cern.ch/user/e/eperez/public/FCCee/input_example_GuineaPig.hepevt
 #
 
-###### Parser ################
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('--input', type=str, default=None, help='specify an input File')
-my_args, _ = parser.parse_known_args()
-
-from FWCore.joboptions import parse_standard_job_options
-args = parse_standard_job_options()
-
-
-my_input = ""
-outfilename = "pgun_output.root"
-if args.nevents is not None:
-    nEvents = args.nevents
-
-if args.outputfile is not None:
-    outfilename = args.outputfile
-
-if my_args.input != None:
-    my_input = my_args.input
-################################
-
 
 # Data service
 from Configurables import FCCDataSvc
@@ -41,9 +19,8 @@ podioevent = FCCDataSvc("EventDataSvc")
 # from Configurables import HepMCReader
 from Configurables import HepEVTReader
 
-reader = HepEVTReader("Reader", Filename=my_input)
-reader.genparticles.Path="allGenParticles"
-reader.genvertices.Path="allGenVertices"
+reader = HepEVTReader("Reader", HepEVTFilename='/afs/cern.ch/user/e/eperez/public/FCCee/input_example_GuineaPig.hepevt')
+reader.GenParticles.Path="allGenParticles"
 
 # DD4hep geometry service
 # Parses the given xml file
@@ -64,13 +41,10 @@ from GaudiKernel.SystemOfUnits import mm
 regiontool = SimG4UserLimitRegion("limits", volumeNames=["world"],
                                   maxStep = 2*mm)
 
-physicslisttool = SimG4UserLimitPhysicsList("Physics", fullphysics="SimG4FtfpBert")
+physicslisttool = SimG4UserLimitPhysicsList(fullphysics="SimG4FtfpBert")
 
-actions = SimG4FullSimActions()
-actions.enableHistory=True
 # giving the names of tools will initialize the tools of that type
 geantservice = SimG4Svc("SimG4Svc", detector='SimG4DD4hepDetector', physicslist=physicslisttool, 
-                        actions=actions, 
                         regions=[regiontool],
                         magneticField=field,
 			g4PostInitCommands=['/random/setSeeds 1234500 3456700']
@@ -79,7 +53,7 @@ geantservice = SimG4Svc("SimG4Svc", detector='SimG4DD4hepDetector', physicslist=
 #geantservice.g4PostInitCommands += ["/run/setCut 0.7 mm"]
 #geantservice.g4PostInitCommands +=["/process/eLoss/minKinEnergy 1 MeV"]
 #geantservice.g4PostInitCommands +=["/score/quantity/energyDeposit edep1kev"]
-geantservice.g4PostInitCommands += ["/tracking/storeTrajectory 1"]
+#geantservice.g4PostInitCommands += ["/tracking/storeTrajectory 1"]
 
 
 # Geant4 algorithm
@@ -90,71 +64,46 @@ from Configurables import SimG4Alg, SimG4SaveTrackerHits, SimG4SaveCalHits, SimG
 # and YY is the given name ("saveTrackerHits")
 
 
-savetrajectorytool = SimG4SaveTrajectory("saveTrajectory")
-savetrajectorytool.trajectory.Path = "trajectory"
-savetrajectorytool.trajectoryPoints.Path = "trajectoryPoints"
-
-
-savehisttool = SimG4SaveParticleHistory("saveHistory")
-savehisttool.mcParticles.Path = "simParticles"
-savehisttool.genVertices.Path = "simVertices"
 
 
 savetrackertool = SimG4SaveTrackerHits("saveTrackerHits_Barrel", readoutNames = ["VertexBarrelCollection"])
-savetrackertool.positionedTrackHits.Path = "positionedHits_VXD_barrel"
-savetrackertool.trackHits.Path = "hits_VXD_barrel"
-savetrackertool.digiTrackHits.Path = "digiHits_VXD_barrel"
+savetrackertool.SimTrackHits.Path = "hits_VXD_barrel"
 
 savetrackertool_endcap = SimG4SaveTrackerHits("saveTrackerHits_Endcap", readoutNames = ["VertexEndcapCollection"])
-savetrackertool_endcap.positionedTrackHits.Path = "positionedHits_VXD_endcap"
-savetrackertool_endcap.trackHits.Path = "hits_VXD_endcap"
-savetrackertool_endcap.digiTrackHits.Path = "digiHits_VXD_endcap"
+savetrackertool_endcap.SimTrackHits.Path = "hits_VXD_endcap"
 
 savetrackertool_ITB = SimG4SaveTrackerHits("saveTrackerHits_IT_Barrel", readoutNames = ["InnerTrackerBarrelCollection"])
-savetrackertool_ITB.positionedTrackHits.Path = "positionedHits_IT_barrel"
-savetrackertool_ITB.trackHits.Path = "hits_IT_barrel"
-savetrackertool_ITB.digiTrackHits.Path = "digiHits_IT_barrel"
+savetrackertool_ITB.SimTrackHits.Path = "hits_IT_barrel"
 
 savetrackertool_ITE = SimG4SaveTrackerHits("saveTrackerHits_IT_Endcap", readoutNames = ["InnerTrackerEndcapCollection"])
-savetrackertool_ITE.positionedTrackHits.Path = "positionedHits_IT_endcap"
-savetrackertool_ITE.trackHits.Path = "hits_IT_endcap"
-savetrackertool_ITE.digiTrackHits.Path = "digiHits_IT_endcap"
+savetrackertool_ITE.SimTrackHits.Path = "hits_IT_endcap"
 
 savetrackertool_OTB = SimG4SaveTrackerHits("saveTrackerHits_OT_Barrel", readoutNames = ["OuterTrackerBarrelCollection"])
-savetrackertool_OTB.positionedTrackHits.Path = "positionedHits_OT_barrel"
-savetrackertool_OTB.trackHits.Path = "hits_OT_barrel"
-savetrackertool_OTB.digiTrackHits.Path = "digiHits_OT_barrel"
+savetrackertool_OTB.SimTrackHits.Path = "hits_OT_barrel"
 
 savetrackertool_OTE = SimG4SaveTrackerHits("saveTrackerHits_OT_Endcap", readoutNames = ["OuterTrackerEndcapCollection"])
-savetrackertool_OTE.positionedTrackHits.Path = "positionedHits_OT_endcap"
-savetrackertool_OTE.trackHits.Path = "hits_OT_endcap"
-savetrackertool_OTE.digiTrackHits.Path = "digiHits_OT_endcap"
+savetrackertool_OTE.SimTrackHits.Path = "hits_OT_endcap"
 
 savelumicaltool = SimG4SaveCalHits("saveLumicalHits", readoutNames = ["LumiCalCollection"])
-savelumicaltool.positionedCaloHits.Path = "positionedHits_Lumical"
-savelumicaltool.caloHits.Path = "hits_Lumical"
+savelumicaltool.CaloHits.Path = "hits_Lumical"
 
 saveEcalBarreltool = SimG4SaveCalHits("saveEcalBarrelHits", readoutNames = ["ECalBarrelCollection"])
-saveEcalBarreltool.positionedCaloHits.Path = "positionedHits_EcalBarrel"
-saveEcalBarreltool.caloHits.Path = "hits_EcalBarrel"
+saveEcalBarreltool.CaloHits.Path = "hits_EcalBarrel"
 
 saveEcalEndcaptool = SimG4SaveCalHits("saveEcalEndcapHits", readoutNames = ["ECalEndcapCollection"])
-saveEcalEndcaptool.positionedCaloHits.Path = "positionedHits_EcalEndcap"
-saveEcalEndcaptool.caloHits.Path = "hits_EcalEndcap"
+saveEcalEndcaptool.CaloHits.Path = "hits_EcalEndcap"
 
 saveHcalBarreltool = SimG4SaveCalHits("saveHcalBarrelHits", readoutNames = ["HCalBarrelCollection"])
-saveHcalBarreltool.positionedCaloHits.Path = "positionedHits_HcalBarrel"
-saveHcalBarreltool.caloHits.Path = "hits_HcalBarrel"
+saveHcalBarreltool.CaloHits.Path = "hits_HcalBarrel"
 
 saveHcalEndcaptool = SimG4SaveCalHits("saveHcalEndcapHits", readoutNames = ["HCalEndcapCollection"])
-saveHcalEndcaptool.positionedCaloHits.Path = "positionedHits_HcalEndcap"
-saveHcalEndcaptool.caloHits.Path = "hits_HcalEndcap"
+saveHcalEndcaptool.CaloHits.Path = "hits_HcalEndcap"
 
 
 
 # next, create the G4 algorithm, giving the list of names of tools ("XX/YY")
 particle_converter = SimG4PrimariesFromEdmTool("EdmConverter")
-particle_converter.genParticles.Path = "allGenParticles"
+particle_converter.GenParticles.Path = "allGenParticles"
 
 geantsim = SimG4Alg("SimG4Alg",
                     outputs= [
@@ -178,7 +127,7 @@ geantsim = SimG4Alg("SimG4Alg",
 # PODIO algorithm
 from Configurables import PodioOutput
 out = PodioOutput("out",
-                  filename=outfilename,
+                  filename="fccee_cld_out.py",
                   OutputLevel=DEBUG)
 out.outputCommands = ["keep *"]
 
@@ -189,5 +138,5 @@ ApplicationMgr( TopAlg = [reader, geantsim, out],
                 EvtMax   = 1,
                 # order is important, as GeoSvc is needed by SimG4Svc
                 ExtSvc = [podioevent, geoservice, geantservice],
-                OutputLevel=DEBUG
+                OutputLevel=INFO
                 )
