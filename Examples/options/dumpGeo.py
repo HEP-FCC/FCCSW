e7798c3066c8689bc6d89b6dd61c13f7c7462d0a
Valentin Volkl <valentin.volkl@cern.ch>
2021-06-01 10:24:46 +0200

    update dumpGeo.py options

--- Examples/options/dumpGeo.py
+++ Examples/options/dumpGeo.py
@@ -9,27 +9,29 @@ app.EvtMax = 1
 app.ExtSvc = []
 app.OutputLevel = DEBUG
 
-# order is important, as GeoSvc is needed by SimG4Svc
+# DD4hep geometry service
 from Configurables import GeoSvc
+## parse the given xml file
+path_to_detectors = os.environ.get("FCCDETECTORS", "")
 geoservice = GeoSvc("GeoSvc")
-geoservice.detectors=[
-  'file:../Detector/DetSensitive/tests/compact/Box_simpleTrackerSD.xml',
-  ]
-geoservice.OutputLevel = INFO
-app.ExtSvc += [geoservice]
+geoservice.detectors = [
+                          os.path.join(path_to_detectors, 'Detector/DetFCCeeCLD/compact/FCCee_DectMaster.xml'),
+                       ]
+ApplicationMgr().ExtSvc += [geoservice]
+
 
 from Configurables import SimG4Svc
 geantservice = SimG4Svc("SimG4Svc")
-app.ExtSvc += [geantservice]
+ApplicationMgr().ExtSvc += [geantservice]
 
-export_fname = "TestBox.gdml"
-# check if file exists and delete it:
-if os.path.isfile(export_fname):
-    os.remove(export_fname)
 
 from Configurables import GeoToGdmlDumpSvc
 geodumpservice = GeoToGdmlDumpSvc()
-geodumpservice.gdml=export_fname
-app.ExtSvc += [geodumpservice]
+export_fname = "DetFCCeeCLD.gdml"
+# check if file exists and delete it:
+if os.path.isfile(export_fname):
+    os.remove(export_fname)
+geodumpservice.gdml = export_fname
+ApplicationMgr().ExtSvc += [geodumpservice]
 
 
