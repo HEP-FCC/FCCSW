62adfd2616fdc89a8f0e3b4d1cb6bddc4cccb3ac
Valentin Volkl <valentin.volkl@cern.ch>
2021-06-01 11:03:51 +0200

    reformat read_podio_inpuy.py

--- Examples/options/read_podio_input.py
+++ Examples/options/read_podio_input.py
@@ -1,24 +1,27 @@
 from Gaudi.Configuration import *
 
-from Configurables import ApplicationMgr, FCCDataSvc, PodioOutput
+from Configurables import ApplicationMgr
+ApplicationMgr().EvtSel = 'NONE'
+ApplicationMgr().EvtMax = 3
 
-podioevent   = FCCDataSvc("EventDataSvc", input="tracker_with_field.root")
+from Configurables import FCCDataSvc
+podioevent = FCCDataSvc("EventDataSvc")
+podioevent.input = "tracker_with_field.root"
+ApplicationMgr().ExtSvc += [podioevent]
 
-# reads HepMC text file and write the HepMC::GenEvent to the data service
 from Configurables import PodioInput, ReadTestConsumer
-podioinput = PodioInput("PodioReader", collections=["allGenVertices", "allGenParticles", "hits"], OutputLevel=DEBUG)
+podioinput = PodioInput("PodioReader")
+podioinput.collections = ["allGenVertices", "allGenParticles", "hits"]
+podioinput.OutputLevel = DEBUG
+ApplicationMgr().TopAlg += [podioinput]
+
+from Configurables import ReadTestConsumer
 checker = ReadTestConsumer()
+ApplicationMgr().TopAlg += [checker]
 
-out = PodioOutput("out", filename="out2.root",
-                   OutputLevel=DEBUG)
+out = PodioOutput("out")
+out.filename = "out2.root"
+out.OutputLevel = DEBUG
 out.outputCommands = ["keep *"]
-
-ApplicationMgr(
-    TopAlg = [podioinput, checker,
-              out
-              ],
-    EvtSel = 'NONE',
-    EvtMax   = 3,
-    ExtSvc = [podioevent],
- )
+ApplicationMgr().TopAlg += [out]
 
